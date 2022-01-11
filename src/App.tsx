55c24bb228ef59164e51b46abad56e0eb1fcd75a
Jay <cho2304@naver.com>
2022-01-12 01:08:54 +0900

    feat(card): card style을 정의한다

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card key={'카드다 카드!'} expireData="" number="" owner="" type="" />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,59 @@
+import styles from './Card.module.css'
+
+const CardTypeAccordingToStartsWith = {
+  1: '포코',
+  2: '준',
+  3: '공원',
+  4: '브랜',
+  5: '로이드',
+  6: '도비',
+  7: '콜린',
+  8: '썬',
+} as const
+
+type CardType =
+  typeof CardTypeAccordingToStartsWith[keyof typeof CardTypeAccordingToStartsWith]
+
+interface CardProps {
+  type: CardType | ''
+  number: string
+  expireData: string
+  owner: string
+  cvc: string
+  password: string
+}
+
+const Card = ({
+  type,
+  number,
+  owner,
+  expireData,
+}: Omit<CardProps, 'password' | 'cvc'>) => {
+  return (
+    <div className={styles['card-box']}>
+      <div className={styles['small-card']}>
+        <div className={styles['card-top']}>
+          <span className={styles['card-text']}>{type && `${type}카드`}</span>
+        </div>
+        <div className={styles['card-middle']}>
+          <div className={styles['small-card__chip']}></div>
+        </div>
+        <div className={styles['card-bottom']}>
+          <div className={styles['card-bottom__number']}>
+            <span className={styles['card-text']}>
+              {number && '1111 - 2222 - oooo - oooo'}
+            </span>
+          </div>
+          <div className={styles['card-bottom__info']}>
+            <span className={styles['card-text']}>{owner || 'NAME'}</span>
+            <span className={styles['card-text']}>
+              {expireData || 'MM / YY'}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
