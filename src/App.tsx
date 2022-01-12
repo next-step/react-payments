41301195ec5cf840e16b43782f9c7c15ed3c0219
howdy-mj <hi.minjungkim@gmail.com>
2022-01-12 20:51:34 +0900

    chore: App.tsx 페이지 별 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import OriginCard from 'OriginCardUI';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <h1>React Clean Code Payments CSS example</h1>
-      <h2>카드 추가 페이지 - 원본</h2>
-      <OriginCard />
+      <h2>카드 추가 페이지 라우팅</h2>
+      <div>
+        <Link to="/add">카드 추가</Link> (작업중)
+        <br />
+        <Link to="/add-completed">카드 추가 완료</Link> (시작전)
+        <br />
+        <Link to="/card-list">카드 목록</Link> (시작전)
+      </div>
     </>
   );
 }
