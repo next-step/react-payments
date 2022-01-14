9868caca0a6076301cf2b71e049e0c0e57a25698
Jay <cho2304@naver.com>
2022-01-15 02:14:57 +0900

    feat: 보유 카드 페이지에 빈 카드를 렌더링하고 해당 카드를 누르면 카드 생성 페이지로 이동한다

---
+++ src/css/card.ts
@@ -0,0 +1,13 @@
+import { css } from 'styled-components'
+
+const CardStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+`
+
+export { CardStyle }
--- src/pages/CardList/index.tsx
+++ src/pages/CardList/index.tsx
@@ -1,10 +1,18 @@
+import { Link } from 'react-router-dom'
 import Header from '../../components/Layout/Header'
+import Styled from './index.style'
 
 const CardList = () => {
   return (
     <>
       <Header title="보유카드" />
-      <div>CardList</div>
+      <Styled.CardListContainer>
+        <Styled.CardContainer>
+          <Link to="/create">
+            <Styled.EmptyCard>+</Styled.EmptyCard>
+          </Link>
+        </Styled.CardContainer>
+      </Styled.CardListContainer>
     </>
   )
 }
