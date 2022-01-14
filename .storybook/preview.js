9bd9539ef338c4552acd9981ed3de1d7ec3a4c2c
yoonhe <yhehe228@gmail.com>
2022-01-14 23:16:37 +0900

    Story : Story에 GlobalStyle 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+import { css, Global } from "@emotion/react";
+
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    line-height: 1;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+`;
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +24,12 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Global styles={globalStyle} />
+      <Story />
+    </>
+  ),
+];
