3408f84e1ca159ab5e88a3015bc08902ced61c18
dngwoodo <dngwoodo@gmail.com>
2022-01-15 15:50:53 +0900

    feat: App 컴포넌트 작성

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import App from './App';
 
 describe('App', () => {
   it('renders without crash', () => {
     render(<App />);
+  });
+
+  it('renders card-form title', () => {
+    const { getByTitle, getByRole } = render(<App />);
+
+    fireEvent.click(getByTitle('back'));
+
+    expect(getByRole('heading', { name: '카드 목록' })).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import Header from './components/Header';
+
 function App() {
+  const [step, setStep] = useState('card-form');
+
+  const handleClickGoBack = () => {
+    setStep('card-list');
+  };
+
   return (
     <div className="App">
-      Hello, World!
+      {step === 'card-list' && <h1>카드 목록</h1>}
+      {step === 'card-form' && (
+        <Header onClickGoBack={handleClickGoBack} />
+      )}
     </div>
   );
 }
