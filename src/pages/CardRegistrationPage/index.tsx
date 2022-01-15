dbcf482f45df83470ae0eb56cacb490e9a4e1859
hwano jeon <jho2301@gmail.com>
2022-01-15 15:46:31 +0900

    feat: 카드 번호입력 폼에 라벨을 보여준다

--- src/pages/CardRegistrationPage/index.tsx
+++ src/pages/CardRegistrationPage/index.tsx
@@ -31,10 +31,10 @@ const CardRegistrationPage = () => {
         <div className="input-container">
           <span className="input-title">카드 번호</span>
           <div className="input-box">
-            <input className="input-basic" type="text" />
-            <input className="input-basic" type="text" />
-            <input className="input-basic" type="password" />
-            <input className="input-basic" type="password" />
+            <input className="input-basic" type="tel" placeholder="1234" />
+            <input className="input-basic" type="tel" placeholder="1234" />
+            <input className="input-basic" type="password" placeholder="****" />
+            <input className="input-basic" type="password" placeholder="****" />
           </div>
         </div>
         <div className="input-container">
