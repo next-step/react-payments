17578607c7d24543131ad25f15142b3ea96b6d8f
Jade <e2goon@gmail.com>
2022-01-17 00:36:41 +0900

    initial
    - vite
    - react
    - typescript
    - storybook
    - tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-10 bg-red-100 text-red-600 font-bold text-4xl">
+      Hello?
+    </div>
+  );
+}
+
+export default App;
