b4530d253e86961330cfb1276a0820751d2f34c0
inthewalter <inwalter99@gmail.com>
2022-01-18 02:25:17 +0900

    누락된 props 전달

--- src/pages/AddPage.tsx
+++ src/pages/AddPage.tsx
@@ -24,7 +24,7 @@ function AddPage() {
       <Card
         type="small"
         cardNumber={cardNumber}
-        holderName=""
+        holderName={cardHolderName}
         expireMonth={cardExpireDate.month}
         expireYear={cardExpireDate.year}
         company={{ name: '', color: '' }}
