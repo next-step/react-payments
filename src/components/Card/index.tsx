73b7383bc5e8cf4227a67f9bbeff410ce37b87f9
parksil0 <parksil00@naver.com>
2022-01-17 21:33:46 +0900

    chore : props 설정

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,31 +2,64 @@ import React from 'react';
 import {
   CardBottom,
   CardBottomInfo,
+  CardBottomNumber,
   CardBox,
   CardMiddle,
   CardText,
   CardTop,
   EmptyCard,
   SmallCardChip,
+  CardNumber,
 } from './styles';
 
 interface CardProps {
   type: 'add' | 'alias' | 'list';
+  cardNumber: string[];
+  expirationNumber: string[];
+  owner?: string;
+  cardName?: string;
+  onClick?: () => void;
 }
 
-const index = ({ type }: CardProps) => {
+const index = ({
+  type,
+  cardNumber,
+  expirationNumber,
+  owner,
+  cardName,
+  onClick,
+}: CardProps) => {
+  const setSecurityText = (number: string) => {
+    return number
+      .split('')
+      .map(() => '*')
+      .join('');
+  };
+
   return (
     <>
-      <CardBox>
+      <CardBox onClick={onClick}>
         <EmptyCard>
-          <CardTop></CardTop>
+          <CardTop>
+            <CardText>{cardName}</CardText>
+          </CardTop>
           <CardMiddle>
             <SmallCardChip></SmallCardChip>
           </CardMiddle>
           <CardBottom>
+            <CardBottomNumber>
+              <CardNumber>{cardNumber[0]}</CardNumber>
+              <CardNumber>{cardNumber[1]}</CardNumber>
+              <CardNumber>{setSecurityText(cardNumber[2])}</CardNumber>
+              <CardNumber>{setSecurityText(cardNumber[3])}</CardNumber>
+            </CardBottomNumber>
             <CardBottomInfo>
-              <CardText>NAME</CardText>
-              <CardText>MM / YY</CardText>
+              <CardText>{owner || 'NAME'}</CardText>
+              <CardText>
+                {expirationNumber[0] || 'MM'}
+                &nbsp;/&nbsp;
+                {expirationNumber[1] || 'YY'}
+              </CardText>
             </CardBottomInfo>
           </CardBottom>
         </EmptyCard>
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,45 +1,74 @@
 import React from 'react';
 import { FlexCenter } from '../../common/styles';
-import { Modal, ModalDimmed, ModalItemConatainer, ModalItemDot, ModalItemName } from './styles';
+import {
+  Modal,
+  ModalDimmed,
+  ModalItemButton,
+  ModalItemConatainer,
+  ModalItemDot,
+  ModalItemName,
+} from './styles';
 
-const index = () => {
+interface ModalProps {
+  open: boolean;
+  onClose: () => void;
+  onClickSpace: () => void;
+}
+
+const index = ({ open, onClose, onClickSpace }: ModalProps) => {
   return (
-    <ModalDimmed>
+    <ModalDimmed open={open} onClick={onClickSpace}>
       <Modal>
         <FlexCenter>
           <ModalItemConatainer>
-            <ModalItemDot></ModalItemDot>
-            <ModalItemName>클린 카드</ModalItemName>
+            <ModalItemButton onClick={onClose}>
+              <ModalItemDot color="#e24141"></ModalItemDot>
+              <ModalItemName>포코 카드</ModalItemName>
+            </ModalItemButton>
           </ModalItemConatainer>
           <ModalItemConatainer>
-            <ModalItemDot></ModalItemDot>
-            <ModalItemName>클린 카드</ModalItemName>
+            <ModalItemButton onClick={onClose}>
+              <ModalItemDot color="#547ce4"></ModalItemDot>
+              <ModalItemName>준 카드</ModalItemName>
+            </ModalItemButton>
           </ModalItemConatainer>
           <ModalItemConatainer>
-            <ModalItemDot></ModalItemDot>
-            <ModalItemName>클린 카드</ModalItemName>
+            <ModalItemButton onClick={onClose}>
+              <ModalItemDot color="#73BC6D"></ModalItemDot>
+              <ModalItemName>공원 카드</ModalItemName>
+            </ModalItemButton>
           </ModalItemConatainer>
           <ModalItemConatainer>
-            <ModalItemDot></ModalItemDot>
-            <ModalItemName>클린 카드</ModalItemName>
+            <ModalItemButton onClick={onClose}>
+              <ModalItemDot color="#DE59B9"></ModalItemDot>
+              <ModalItemName>브랜 카드</ModalItemName>
+            </ModalItemButton>
           </ModalItemConatainer>
         </FlexCenter>
         <FlexCenter>
           <ModalItemConatainer>
-            <ModalItemDot></ModalItemDot>
-            <ModalItemName>클린 카드</ModalItemName>
+            <ModalItemButton onClick={onClose}>
+              <ModalItemDot color="#04C09E"></ModalItemDot>
+              <ModalItemName>로이드 카드</ModalItemName>
+            </ModalItemButton>
           </ModalItemConatainer>
           <ModalItemConatainer>
-            <ModalItemDot></ModalItemDot>
-            <ModalItemName>클린 카드</ModalItemName>
+            <ModalItemButton onClick={onClose}>
+              <ModalItemDot color="#E76E9A"></ModalItemDot>
+              <ModalItemName>도비 카드</ModalItemName>
+            </ModalItemButton>
           </ModalItemConatainer>
           <ModalItemConatainer>
-            <ModalItemDot></ModalItemDot>
-            <ModalItemName>클린 카드</ModalItemName>
+            <ModalItemButton onClick={onClose}>
+              <ModalItemDot color="#F37D3B"></ModalItemDot>
+              <ModalItemName>콜린 카드</ModalItemName>
+            </ModalItemButton>
           </ModalItemConatainer>
           <ModalItemConatainer>
-            <ModalItemDot></ModalItemDot>
-            <ModalItemName>클린 카드</ModalItemName>
+            <ModalItemButton onClick={onClose}>
+              <ModalItemDot color="#FBCD58"></ModalItemDot>
+              <ModalItemName>썬 카드</ModalItemName>
+            </ModalItemButton>
           </ModalItemConatainer>
         </FlexCenter>
       </Modal>
