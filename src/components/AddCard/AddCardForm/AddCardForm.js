4ed111b5f313e545df14810d540426ac514f16d9
yoonhe <yhehe228@gmail.com>
2022-01-20 08:48:49 +0900

    AddCardForm의 onChangeSingleInput Props를 넘어온 그대로 자식 컴포넌트에 넘기도록 코드 수정

--- src/components/AddCard/AddCardForm/AddCardForm.js
+++ src/components/AddCard/AddCardForm/AddCardForm.js
@@ -15,59 +15,52 @@ const AddCardForm = ({
   },
   onChangeMultipleInput,
   onChangeSingleInput,
-}) => {
-  const handleChangeSingleInput = ({ name, value }) => {
-    onChangeSingleInput({ name, value });
-  };
-
-  return (
-    <form>
-      <CardNumberInput
-        fields={cardNumbers}
-        onChange={onChangeMultipleInput}
+}) => (
+  <form>
+    <CardNumberInput
+      fields={cardNumbers}
+      onChange={onChangeMultipleInput}
+    />
+    <ExpiryDateInput
+      fields={expiryDate}
+      onChange={onChangeMultipleInput}
+    />
+    <InputStyle.Container>
+      <InputStyle.LabelGroup>
+        <label htmlFor="card-owner">카드 소유자이름(선택)</label>
+        <span>{cardOwner?.length}/30</span>
+      </InputStyle.LabelGroup>
+      <Input
+        field={{
+          id: "card-owner",
+          name: "cardOwner",
+          value: cardOwner,
+          maxLength: 30,
+        }}
+        onChange={onChangeSingleInput}
+        background
       />
-      <ExpiryDateInput
-        fields={expiryDate}
-        onChange={onChangeMultipleInput}
+    </InputStyle.Container>
+    <InputStyle.Container>
+      <InputStyle.Label htmlFor="security-number">
+        보안코드(CVC/CVV)
+      </InputStyle.Label>
+      <Input
+        field={{
+          id: "security-number",
+          type: "password",
+          name: "securityNumber",
+          value: securityNumber,
+          maxLength: 3,
+        }}
+        onChange={onChangeSingleInput}
+        background
       />
-      <InputStyle.Container>
-        <InputStyle.LabelGroup>
-          <label htmlFor="card-owner">카드 소유자이름(선택)</label>
-          <span>{cardOwner?.length}/30</span>
-        </InputStyle.LabelGroup>
-        <Input
-          field={{
-            id: "card-owner",
-            name: "cardOwner",
-            value: cardOwner,
-            maxLength: 30,
-          }}
-          onChange={handleChangeSingleInput}
-          background
-        />
-      </InputStyle.Container>
-      <InputStyle.Container>
-        <InputStyle.Label htmlFor="security-number">
-          보안코드(CVC/CVV)
-        </InputStyle.Label>
-        <Input
-          field={{
-            id: "security-number",
-            type: "password",
-            name: "securityNumber",
-            value: securityNumber,
-            maxLength: 3,
-          }}
-          onChange={handleChangeSingleInput}
-          background
-        />
-      </InputStyle.Container>
-      <CardPasswordInput
-        fields={cardPassword}
-        onChange={onChangeMultipleInput}
-      />
-    </form>
-  );
-};
-
+    </InputStyle.Container>
+    <CardPasswordInput
+      fields={cardPassword}
+      onChange={onChangeMultipleInput}
+    />
+  </form>
+);
 export default AddCardForm;
