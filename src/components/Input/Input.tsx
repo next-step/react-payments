5bd78b670f47c6e1e998cd005f73047f28651757
xid-jade <jade@protopie.io>
2022-01-21 03:42:40 +0900

    add: Input 컴포넌트 추가

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { ForwardedRef, forwardRef, InputHTMLAttributes } from "react";
+import cx from "classnames";
+
+// className="px-6 py-2 bg-neutral-200 border rounded-lg text-lg font-medium border-none focus:bg-neutral-300 focus:outline-none"
+
+interface BaseProps {
+  className?: string;
+  isFullWidth?: boolean;
+  isChecked?: boolean;
+  isDisabled?: boolean;
+  isInvalid?: boolean;
+}
+
+type Props = BaseProps & InputHTMLAttributes<HTMLInputElement>;
+
+const classes = /* @tw */ {
+  container: [
+    "py-3",
+    "px-4",
+    "bg-neutral-200",
+    "border-none",
+    "text-lg",
+    "rounded-lg",
+    "leading-none",
+    "font-bold",
+    "text-[#04c09e]",
+    "placeholder:text-neutral-600",
+    "placeholder:font-normal",
+    "focus:bg-neutral-300",
+    "focus:outline-none",
+  ],
+};
+
+function Input(props: Props, ref: ForwardedRef<HTMLInputElement>) {
+  const { isFullWidth, className, ...rest } = props;
+  return (
+    <input
+      type="text"
+      ref={ref}
+      {...rest}
+      className={cx(isFullWidth && "w-full", classes.container)}
+    />
+  );
+}
+
+export default forwardRef(Input);
