317c40853b1b39c86d6cc6c0451afde4ad5c2a37
xid-jade <jade@protopie.io>
2022-01-21 03:42:20 +0900

    remove: 웹폰트 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [require("@tailwindcss/line-clamp")],
 };
