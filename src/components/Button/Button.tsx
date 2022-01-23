ee932dce329ed087a45f350bc6f9d907c6ec453c
Hyoungnam Kim <kimhyoungnams@gmail.com>
2022-01-24 02:02:31 +0900

    style: 버튼 카드 스타일 위치 수정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,14 @@
-import * as S from './style'
+import styled from 'styled-components'
+import { MINT } from 'styles/colors'
 
 export function Button({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  return <S.Box {...props}>{children}</S.Box>
+  return <Box {...props}>{children}</Box>
 }
+
+export const Box = styled.button`
+  border: 0;
+  padding: 8px 12px;
+  font-size: 16px;
+  color: ${MINT};
+  background: #fff;
+`
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import * as S from './style'
+import styled, { css } from 'styled-components'
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   big?: boolean
 }
 
 export function Card({ children, ...props }: Props) {
-  return <S.Card {...props}>{children}</S.Card>
+  return <Box {...props}>{children}</Box>
 }
+
+export const Box = styled.div<{ big?: boolean }>`
+  margin: 12px 0;
+  width: 208px;
+  min-height: 130px;
+  color: #575757;
+  background: #e5e5e5;
+  border-radius: 5px;
+  ${({ big }) =>
+    big &&
+    css`
+      width: 290px;
+      min-height: 180px;
+    `}
+`
