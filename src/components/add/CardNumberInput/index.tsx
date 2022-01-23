9c8e3a4c229c3b82014b78f2b4da8e805cba3a68
inthewalter <inwalter99@gmail.com>
2022-01-23 11:51:08 +0900

    number 를 하나의 객체로 묶어 관리
    https://github.com/next-step/react-payments/pull/9#discussion_r788407186

--- src/components/add/CardNumberInput/index.tsx
+++ src/components/add/CardNumberInput/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { createRef, useCallback, useEffect, useState } from 'react'
 import { Card } from 'src/types/card'
 
 import { BasicInput, Input, InputBox, InputTitle, InputWarning } from '$components/common/Input'
@@ -10,14 +10,15 @@ interface CardNumberInputProps {
 }
 
 function CardNumberInput({ cardNumber, setCardNumber }: CardNumberInputProps) {
-  const ref1 = useRef<HTMLInputElement>(null)
-  const ref2 = useRef<HTMLInputElement>(null)
-  const ref3 = useRef<HTMLInputElement>(null)
-  const ref4 = useRef<HTMLInputElement>(null)
-  const [number1, setNumber1] = useState('')
-  const [number2, setNumber2] = useState('')
-  const [number3, setNumber3] = useState('')
-  const [number4, setNumber4] = useState('')
+  const refs = Array(4)
+    .fill(null)
+    .map(() => createRef<HTMLInputElement>())
+  const [number, setNumber] = useState<{ 0: string; 1: string; 2: string; 3: string }>({
+    0: cardNumber[0],
+    1: cardNumber[1],
+    2: cardNumber[2],
+    3: cardNumber[3],
+  })
   const [showsWarning, setWarning] = useState(false)
 
   const isValidInput = useCallback((value) => {
@@ -30,59 +31,53 @@ function CardNumberInput({ cardNumber, setCardNumber }: CardNumberInputProps) {
     return false
   }, [])
 
-  const handleNumber1Change = useCallback(
-    ({ target: { value } }) => {
+  const handleNumberChange = useCallback(
+    (index: keyof typeof number, value: string) => {
       if (!isValidInput(value)) return
-      if (value.length === 4) ref2.current?.focus()
-      setNumber1(value)
+      if (value.length === 4 && index < 3) refs[index].current?.focus()
+
+      setNumber((prev) => ({ ...prev, [index]: value }))
     },
-    [isValidInput],
-  )
-  const handleNumber2Change = useCallback(
-    ({ target: { value } }) => {
-      if (!isValidInput(value)) return
-      if (value.length === 4) ref3.current?.focus()
-      setNumber2(value)
-    },
-    [isValidInput],
-  )
-  const handleNumber3Change = useCallback(
-    ({ target: { value } }) => {
-      if (!isValidInput(value)) return
-      if (value.length === 4) ref4.current?.focus()
-      setNumber3(value)
-    },
-    [isValidInput],
-  )
-  const handleNumber4Change = useCallback(
-    ({ target: { value } }) => {
-      if (!isValidInput(value)) return
-      setNumber4(value)
-    },
-    [isValidInput],
+    [isValidInput, refs],
   )
 
   useEffect(() => {
-    setCardNumber([number1, number2, number3, number4])
-  }, [number1, number2, number3, number4, setCardNumber])
-
-  useEffect(() => {
-    const [num1, num2, num3, num4] = cardNumber
-
-    setNumber1(num1)
-    setNumber2(num2)
-    setNumber3(num3)
-    setNumber4(num4)
-  }, [cardNumber])
+    const changedCardNumber = Object.values(number).map((value) => value) as Card['number']
+    setCardNumber(changedCardNumber)
+  }, [number, setCardNumber])
 
   return (
     <Input>
       <InputTitle>카드 번호</InputTitle>
       <InputBox>
-        <BasicInput type="text" value={number1} onChange={handleNumber1Change} ref={ref1} placeholder="0000" />
-        <BasicInput type="text" value={number2} onChange={handleNumber2Change} ref={ref2} placeholder="0000" />
-        <BasicInput type="password" value={number3} onChange={handleNumber3Change} ref={ref3} placeholder="0000" />
-        <BasicInput type="password" value={number4} onChange={handleNumber4Change} ref={ref4} placeholder="0000" />
+        <BasicInput
+          type="text"
+          value={number[0]}
+          onChange={({ target: { value } }) => handleNumberChange(0, value)}
+          ref={refs[0]}
+          placeholder="0000"
+        />
+        <BasicInput
+          type="text"
+          value={number[1]}
+          onChange={({ target: { value } }) => handleNumberChange(1, value)}
+          ref={refs[1]}
+          placeholder="0000"
+        />
+        <BasicInput
+          type="password"
+          value={number[2]}
+          onChange={({ target: { value } }) => handleNumberChange(2, value)}
+          ref={refs[2]}
+          placeholder="0000"
+        />
+        <BasicInput
+          type="password"
+          value={number[3]}
+          onChange={({ target: { value } }) => handleNumberChange(3, value)}
+          ref={refs[3]}
+          placeholder="0000"
+        />
       </InputBox>
       {showsWarning && <InputWarning>숫자만 입력가능해요</InputWarning>}
     </Input>
