b2e9ef3295f2901a0929a741fb6f87643c30525a
howdy-mj <hi.minjungkim@gmail.com>
2022-01-25 21:59:08 +0900

    feat: stories 예시 추가

--- src/components/UI/Typography.stories.tsx
+++ src/components/UI/Typography.stories.tsx
@@ -1,3 +1,4 @@
+import { ButtonText } from 'components/Button/button.style';
 import { PageTitle } from './Typography';
 
 export default {
@@ -5,3 +6,5 @@ export default {
 };
 
 export const Title = () => <PageTitle>페이지 타이틀</PageTitle>;
+
+export const WarnButtonText = () => <ButtonText warn>삭제</ButtonText>;
