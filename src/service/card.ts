3e4571567428e3976ad06a0e40b18e082d133c7a
hwano jeon <jho2301@gmail.com>
2022-01-30 20:36:51 +0900

    feat: formData, 카드 변환시 id 생성

--- src/service/card.ts
+++ src/service/card.ts
@@ -1,9 +1,11 @@
 import { parse } from 'date-fns';
+import { nanoid } from 'nanoid';
 import { Card, CardForm } from '../types';
 import { getLocalStorage, setLocalStorage } from '../utils/storage';
 
-export const convertToCard = (cardForm: CardForm): Card => {
+export const convertFormDataToCard = (cardForm: CardForm): Card => {
   return {
+    id: nanoid(),
     CVC: cardForm.CVC,
     cardNumber: cardForm.cardNumber.join('-'),
     expireDate: parse(cardForm.expireDate.join(''), 'MMyy', new Date()),
