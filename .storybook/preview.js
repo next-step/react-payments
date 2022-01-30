438a5d95f520380ec18ee60a7c4906fe57f2c071
hwano jeon <jho2301@gmail.com>
2022-01-30 16:35:54 +0900

    fix: 스토리북 정상화

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Router } from 'react-router-dom';
+import '../src/styles/index.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story, context) => (
+    <BrowserRouter>
+      <Story />
+    </BrowserRouter>
+  ),
+];
