3cf45fe12047648968634280646479940f30797d
yujo <yujo.dev@gmail.com>
2022-02-02 14:48:59 +0900

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React, { StrictMode } from "react";
+import { render } from "react-dom";
+
+render(
+  <StrictMode>
+    <h1>Hello Parcel Bundler</h1>
+  </StrictMode>,
+  document.getElementById("root")
+);
