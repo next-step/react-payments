991248ae3e7cbf97bda13a7886d907c0f94e5d49
yoonhe <yhehe228@gmail.com>
2022-02-07 20:59:21 +0900

    Story : CardPasswordInput 상호작용 테스트 추가

--- src/components/AddCard/CardPasswordInput/CardPasswordInput.stories.js
+++ src/components/AddCard/CardPasswordInput/CardPasswordInput.stories.js
@@ -1,6 +1,8 @@
+import { within, userEvent } from "@storybook/testing-library";
+
 import PasswordInput from ".";
 
-import useAddCardFormFields from "../AddCardContainer/hooks/useAddCardFormFields";
+import useAddCardFormFields from "../../../page/AddCardPage/hooks/useAddCardFormFields";
 
 export default {
   title: "Component/Input",
@@ -18,5 +20,24 @@ export const cardPassword = () => {
       fields={cardPassword}
       onChange={handleChangeMultipleInput}
     />
+  );
+};
+
+cardPassword.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.type(
+    canvas.getByLabelText("비밀번호 첫번째 자리"),
+    "1",
+    {
+      delay: 100,
+    }
+  );
+  await userEvent.type(
+    canvas.getByLabelText("비밀번호 두번째 자리"),
+    "1",
+    {
+      delay: 100,
+    }
   );
 };
