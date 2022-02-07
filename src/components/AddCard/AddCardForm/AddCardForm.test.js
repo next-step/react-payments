6d6cc39b499d5ab3d8bcdea66adee291db166c05
yoonhe <yhehe228@gmail.com>
2022-02-07 13:51:41 +0900

    Refactor : AddCardForm과 AddCardForm 사이에 중복되는 테스트를 제거합니다
    AddCardForm과 CardOwnerInput 사이에 중복되는 테스트 중
    CardOwnerInput 렌더링을 확인하는 테스트만 남기고 제거합니다

--- src/components/AddCard/AddCardForm/AddCardForm.test.js
+++ src/components/AddCard/AddCardForm/AddCardForm.test.js
@@ -54,32 +54,6 @@ describe("AddCardForm", () => {
     ).toBeInTheDocument();
   });
 
-  it("카드 소유자 Input 값의 길이를 렌더링합니다", () => {
-    const cardOwner = "HEAEUN";
-
-    const { queryByText } = makeAddCardForm({
-      ...initialFields,
-      cardOwner,
-    });
-
-    expect(queryByText(`${cardOwner.length}/30`)).toBeInTheDocument();
-  });
-
-  it("카드 소유자 Input을 값이 변경되면 change 이벤트 핸들러가 실행됩니다", () => {
-    const { getByLabelText } = makeAddCardForm();
-
-    fireEvent.change(getByLabelText("카드 소유자이름(선택)"), {
-      target: {
-        value: 11,
-      },
-    });
-
-    expect(handleChangeSingleInput).toBeCalledWith({
-      name: "cardOwner",
-      value: "11",
-    });
-  });
-
   it("보안코드 Input을 렌더링합니다", () => {
     const { queryByLabelText } = makeAddCardForm();
 
