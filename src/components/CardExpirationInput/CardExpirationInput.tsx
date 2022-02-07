060ed606f1cd7bbb6beb218509156eec30ffcff6
tigger <tigger@channel.io>
2022-02-08 00:19:21 +0900

    fix: 유효월 입력에 유효성 검사 추가

--- src/components/CardExpirationInput/CardExpirationInput.tsx
+++ src/components/CardExpirationInput/CardExpirationInput.tsx
@@ -11,12 +11,25 @@ interface Props {
 const CardExpirationInput = ({ onChange }: Props) => {
   const [expiration, setExpiration] = useState<CardExpiration>({ month: "", year: "" });
 
-  const makeChangeHandlerByKey = (key: keyof CardExpiration) => (value: string) => {
+  const handleChangeMonth = (value: string) => {
+    const valueAsNumber = Number(value);
+    console.log(value);
+
+    if (value && !Number.isInteger(valueAsNumber)) {
+      return;
+    }
+
+    if (value && (valueAsNumber < 1 || valueAsNumber > 12)) {
+      return;
+    }
+
     setExpiration((prev) => ({
       ...prev,
-      [key]: value,
+      month: value,
     }));
   };
+
+  const handleChangeYear = (value: string) => setExpiration((prev) => ({ ...prev, year: value }));
 
   useEffect(() => {
     onChange?.(expiration.month, expiration.year);
@@ -27,14 +40,14 @@ const CardExpirationInput = ({ onChange }: Props) => {
       <IntegerInput
         type="text"
         value={expiration.month}
-        onChange={makeChangeHandlerByKey("month")}
+        onChange={handleChangeMonth}
         maxLength={MAX_CARD_EXPIRATION_VALUE_LENGTH}
       />
       /
       <IntegerInput
         type="text"
         value={expiration.year}
-        onChange={makeChangeHandlerByKey("year")}
+        onChange={handleChangeYear}
         maxLength={MAX_CARD_EXPIRATION_VALUE_LENGTH}
       />
     </Styled.InputWrapper>
