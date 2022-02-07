035e8ed7aedb41e4d9452b631f7b48a8d4359f56
yoonhe <yhehe228@gmail.com>
2022-02-07 20:59:45 +0900

    Story : ExpiryDateInput 상호작용 테스트 추가

--- src/components/AddCard/ExpiryDateInput/ExpiryDateInput.stories.js
+++ src/components/AddCard/ExpiryDateInput/ExpiryDateInput.stories.js
@@ -1,19 +1,90 @@
+import { within, userEvent } from "@storybook/testing-library";
+
 import ExpiryDateInput from ".";
 
-import useAddCardFormFields from "../AddCardContainer/hooks/useAddCardFormFields";
+import { EXPIRY_DATE_LABEL } from "../constants";
+
+import useAddCardFormFields from "../../../page/AddCardPage/hooks/useAddCardFormFields";
 
 export default {
   title: "Component/Input",
   component: ExpiryDateInput,
 };
 
-export const expiryDate = () => {
+export const BasicForm = () => {
   const {
-    fields: { expiryDate },
+    fields: { expiryDate, error },
     handleChangeMultipleInput,
+    handleErrorChange,
   } = useAddCardFormFields();
 
   return (
-    <ExpiryDateInput fields={expiryDate} onChange={handleChangeMultipleInput} />
+    <ExpiryDateInput
+      fields={expiryDate}
+      error={error.expiryDate}
+      onChange={handleChangeMultipleInput}
+      onErrorChange={handleErrorChange}
+    />
   );
 };
+
+export const ErrorForm = () => {
+  const {
+    fields: { expiryDate, error },
+    handleChangeMultipleInput,
+    handleErrorChange,
+  } = useAddCardFormFields();
+
+  return (
+    <ExpiryDateInput
+      fields={expiryDate}
+      error={error.expiryDate}
+      onChange={handleChangeMultipleInput}
+      onErrorChange={handleErrorChange}
+    />
+  );
+};
+
+BasicForm.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.type(
+    canvas.getByLabelText(EXPIRY_DATE_LABEL.month),
+    "12",
+    {
+      delay: 200,
+    }
+  );
+
+  await userEvent.type(
+    canvas.getByLabelText(EXPIRY_DATE_LABEL.year),
+    "30",
+    {
+      delay: 200,
+    }
+  );
+};
+
+BasicForm.storyName = "Expiry Date Input";
+
+ErrorForm.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.type(
+    canvas.getByLabelText(EXPIRY_DATE_LABEL.month),
+    "20",
+    {
+      delay: 200,
+    }
+  );
+
+  await userEvent.type(
+    canvas.getByLabelText(EXPIRY_DATE_LABEL.year),
+    "30",
+    {
+      delay: 200,
+    }
+  );
+};
+
+ErrorForm.storyName = "Expiry Date Input With Error";
