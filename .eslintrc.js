cdc580bd817c9c4b1799989338307550f2a6cc32
yujo <yujo.dev@gmail.com>
2022-02-08 02:48:02 +0900

    chore: sb init

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,18 @@
+// .storybook/preview.js
+
+import "../src/index.css";
+import "../example/styles/index.css";
+
+const customViewports = {
+  Default: {
+    name: "Default",
+    styles: {
+      width: "375px",
+      height: "700px",
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +21,13 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+  previewTabs: {
+    "storybook/docs/panel": { index: -1 },
+  },
+  viewport: {
+    viewports: {
+      ...customViewports,
+    },
+    defaultViewport: "Default",
+  },
+};
