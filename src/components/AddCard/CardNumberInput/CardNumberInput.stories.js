879875d903a3241b34be1b9ee856b6c90bd334cb
yoonhe <yhehe228@gmail.com>
2022-02-07 20:58:28 +0900

    Story : CardNumberInput 상호작용 테스트 추가

--- src/components/AddCard/CardNumberInput/CardNumberInput.stories.js
+++ src/components/AddCard/CardNumberInput/CardNumberInput.stories.js
@@ -1,6 +1,10 @@
+import { within, userEvent } from "@storybook/testing-library";
+
 import CardNumberInput from ".";
 
-import useAddCardFormFields from "../AddCardContainer/hooks/useAddCardFormFields";
+import { CARD_NUMBER_LABEL } from "../constants";
+
+import useAddCardFormFields from "../../../page/AddCardPage/hooks/useAddCardFormFields";
 
 export default {
   title: "Component/Input",
@@ -18,5 +22,41 @@ export const cardNumber = () => {
       fields={cardNumbers}
       onChange={handleChangeMultipleInput}
     />
+  );
+};
+
+cardNumber.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.type(
+    canvas.getByLabelText(CARD_NUMBER_LABEL.first),
+    "1111",
+    {
+      delay: 100,
+    }
+  );
+
+  await userEvent.type(
+    canvas.getByLabelText(CARD_NUMBER_LABEL.second),
+    "1111",
+    {
+      delay: 100,
+    }
+  );
+
+  await userEvent.type(
+    canvas.getByLabelText(CARD_NUMBER_LABEL.third),
+    "1111",
+    {
+      delay: 100,
+    }
+  );
+
+  await userEvent.type(
+    canvas.getByLabelText(CARD_NUMBER_LABEL.fourth),
+    "1111",
+    {
+      delay: 100,
+    }
   );
 };
