a104e1d61163ed16cdf4bba31eb6553b6b69da07
Jay <cho2304@naver.com>
2022-02-15 23:58:53 +0900

    refactor: 에러메세지를 구체화한다

--- src/constants/error/formErrorMessage.ts
+++ src/constants/error/formErrorMessage.ts
@@ -1,7 +1,7 @@
-const CARD_NUMBER_ERROR = '카드번호를 모두 입력해주세요.'
-const CARD_EXPIRE_ERROR = '만료일을 모두 입력해주세요.'
-const CARD_CVC_ERROR = 'CVC를 모두 입력해주세요.'
-const CARD_PASSWORD_ERROR = '비밀번호를 모두 입력해주세요.'
+const CARD_NUMBER_ERROR = '빈 카드번호 입력창이 있습니다.'
+const CARD_EXPIRE_ERROR = '만료일을 YY-MM 형태로 입력해주세요.'
+const CARD_CVC_ERROR = '3자리의 숫자를 넣어주세요.'
+const CARD_PASSWORD_ERROR = '비밀번호가 입력되지 않았습니다.'
 
 export {
   CARD_NUMBER_ERROR,
