a0313397f4a3749191903a78b466eeb5bce0570e
yujo <yujo.dev@gmail.com>
2022-02-19 16:52:16 +0900

    feat: 카드 소유자 이름 입력 시, 상단에 글자 수를 보여주는 기능

--- src/pages/CardAddPage/CardAddPage.tsx
+++ src/pages/CardAddPage/CardAddPage.tsx
@@ -158,11 +158,11 @@ const CardAddPage = (): JSX.Element => {
           />
         </InputContainer>
 
-        <InputContainer title="카드 소유자 이름(선택)">
+        <InputContainer title={`카드 소유자 이름(선택) ${cardName.length}/30`}>
           <Input
             name="cardName"
             placeholder="카드에 표시된 이름과 동일하게 입력하세요."
-            maxLength={15}
+            maxLength={30}
             value={cardName}
             onChange={onChangeCardContextValue}
             className="pl-4"
