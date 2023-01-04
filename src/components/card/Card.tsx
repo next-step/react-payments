8645ee1ffc14600ce5854970bae0c12753f27c1d
junilhwang <junil.h@kakao.com>
2023-01-04 23:29:30 +0900

    feat: Card Component 추가

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface IProps {}
+
+export default function Card(props: IProps) {
+  return (
+    <>
+      <div className="card-box">
+        <div className="small-card">
+          <div className="card-top">
+            <span className="card-text">클린카드</span>
+          </div>
+          <div className="card-middle">
+            <div className="small-card__chip"></div>
+          </div>
+          <div className="card-bottom">
+            <div className="card-bottom__number">
+              <span className="card-text">1111 - 2222 - oooo - oooo</span>
+            </div>
+            <div className="card-bottom__info">
+              <span className="card-text">YUJO</span>
+              <span className="card-text">12 / 23</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <span className="card-nickname">법인카드</span>
+    </>
+  );
+}
--- src/containers/CardList.tsx
+++ src/containers/CardList.tsx
@@ -1,5 +1,6 @@
 import { MouseEventHandler, useCallback } from "react";
 import { Pages, useRouteContext } from "../providers";
+import { Card } from "../components";
 
 interface IProps {}
 
@@ -19,26 +20,7 @@ export default function CardList(props: IProps) {
       <div className="flex-center">
         <h2 className="page-title mb-10">보유 카드</h2>
       </div>
-      <div className="card-box">
-        <div className="small-card">
-          <div className="card-top">
-            <span className="card-text">클린카드</span>
-          </div>
-          <div className="card-middle">
-            <div className="small-card__chip"></div>
-          </div>
-          <div className="card-bottom">
-            <div className="card-bottom__number">
-              <span className="card-text">1111 - 2222 - oooo - oooo</span>
-            </div>
-            <div className="card-bottom__info">
-              <span className="card-text">YUJO</span>
-              <span className="card-text">12 / 23</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <span className="card-nickname">법인카드</span>
+      <Card />
       <div className="card-box">
         <div className="empty-card" onClick={handleClickCardAdd}>
           +
