3cce15ad9250c81e19da1d00fad75142fda3e953
Tene <tene@nextunicorn.kr>
2023-01-08 13:28:48 +0900

    Feat: 모달 UX를 위한 훅 추가

---
+++ src/hooks/useClickAway.ts
@@ -0,0 +1,48 @@
+import { RefObject, useEffect, useRef } from 'react';
+
+const defaultEvents = ['mousedown', 'touchstart'];
+
+function on<T extends Window | Document | HTMLElement | EventTarget>(
+  obj: T | null,
+  ...args: Parameters<T['addEventListener']> | [string, Function | null, ...any]
+): void {
+  if (obj && obj.addEventListener) {
+    obj.addEventListener(...(args as Parameters<HTMLElement['addEventListener']>));
+  }
+}
+
+function off<T extends Window | Document | HTMLElement | EventTarget>(
+  obj: T | null,
+  ...args: Parameters<T['removeEventListener']> | [string, Function | null, ...any]
+): void {
+  if (obj && obj.removeEventListener) {
+    obj.removeEventListener(...(args as Parameters<HTMLElement['removeEventListener']>));
+  }
+}
+
+const useClickAway = <E extends Event = Event>(
+  ref: RefObject<HTMLElement | null>,
+  onClickAway: (event: E) => void,
+  events: string[] = defaultEvents,
+) => {
+  const savedCallback = useRef(onClickAway);
+
+  useEffect(() => {
+    savedCallback.current = onClickAway;
+  }, [onClickAway]);
+
+  useEffect(() => {
+    const handler = (event: any) => {
+      const { current: el } = ref;
+      el && !el.contains(event.target) && savedCallback.current(event);
+    };
+
+    events.forEach((eventName) => on(document, eventName, handler));
+
+    return () => {
+      events.forEach((eventName) => off(document, eventName, handler));
+    };
+  }, [events, ref]);
+};
+
+export default useClickAway;
