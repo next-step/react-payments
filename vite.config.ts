598e2e9f6dcd9b01e95f4b3d7b223faf07635c0a
junilhwang <junil.h@kakao.com>
2023-01-22 19:50:47 +0900

    chore: vitest 추가 및 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-import {defineConfig} from "vite";
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
