3e752a883ceb511fb9ab550e5190af76562e55f7
junilhwang <junil.h@kakao.com>
2023-01-24 21:00:56 +0900

    test: CardBaseForm에 대한 스토리 추가

---
+++ src/pages/card-add/card-form/CardBaseForm.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { CardStateProvider, ModalProvider } from "../providers";
+import { useCardAdd } from "../hooks";
+import CardAddBaseForm from "./CardAddBaseForm";
+
+export default {
+  title: "페이먼츠 미션/Components/CardAddBaseForm",
+  component: CardAddBaseForm,
+  args: {},
+} as ComponentMeta<typeof CardAddBaseForm>;
+
+const Template: ComponentStory<typeof CardAddBaseForm> = () => {
+  const { handleSubmitBaseForm, invalidMessages } = useCardAdd();
+
+  return (
+    <div className="root">
+      <CardStateProvider>
+        <div className="app">
+          <ModalProvider>
+            <CardAddBaseForm
+              onSubmit={handleSubmitBaseForm}
+              invalidMessages={invalidMessages}
+            />
+          </ModalProvider>
+        </div>
+      </CardStateProvider>
+    </div>
+  );
+};
+
+export const example = Template.bind({});
+example.args = {};
