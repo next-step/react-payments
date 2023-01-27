03800976cd8f15127a02899f1df141c5c125cd7b
junilhwang <junil.h@kakao.com>
2023-01-27 22:48:47 +0900

    feat: 카드 목록 정렬 작업 완료

--- src/pages/card-list/CardList.tsx
+++ src/pages/card-list/CardList.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { Card, FlexCenter, PageTitle } from "../../components";
 import { useNavigation } from "../hooks";
 import { useMyCardsContext } from "../../providers/my-cards";
@@ -5,21 +6,24 @@ import { useMyCardsContext } from "../../providers/my-cards";
 export default function CardList() {
   const { handleClickCardAdd } = useNavigation();
   const { myCards } = useMyCardsContext();
+  const sortedCards = useMemo(() => [...myCards].reverse(), [myCards]);
 
   return (
-    <div className="app flex-column-center">
-      <FlexCenter>
-        <PageTitle className="mb-10">보유 카드</PageTitle>
-      </FlexCenter>
+    <div style={{ height: "100%", overflow: "auto" }}>
+      <div className="app flex-column-center">
+        <FlexCenter>
+          <PageTitle className="mb-10">보유 카드</PageTitle>
+        </FlexCenter>
 
-      {myCards.map((card) => (
-        <Card key={card.id} {...card} />
-      ))}
-
-      <div className="card-box">
-        <div className="empty-card" onClick={handleClickCardAdd}>
-          +
+        <div className="card-box">
+          <div className="empty-card" onClick={handleClickCardAdd}>
+            +
+          </div>
         </div>
+
+        {sortedCards.map((card) => (
+          <Card key={card.id} {...card} />
+        ))}
       </div>
     </div>
   );
