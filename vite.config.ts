97f2d7653661d9ab7b7b5c89256f275b68670f06
minhyeok0328 <alsgur7408@gmail.com>
2023-02-14 22:10:57 +0900

    chore: storybook 추가
    혹시 맥에서
    ---
    node:internal/crypto/hash:71 this[kHandle] = new _Hash(algorithm, xofLen);
    
    Error: error:0308010C:digital envelope routines::unsupported
    ---
    
    이런 오류가 난다면 프로젝트 root 경로에서
    export NODE_OPTIONS=--openssl-legacy-provider
    터미널에 요것을 한 번 입력하고 나면 해결된다

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), eslint()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src/'),
-		}
-	}
+  plugins: [react(), eslint()],
 });
