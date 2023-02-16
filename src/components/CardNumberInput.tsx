0e48cf3e9df4f2c6756a6b664a4d24558c461026
hsw824 <ghdtjddn0824@naver.com>
2023-02-16 22:10:20 +0900

    feat: 카드번호 4자리 입력시 다음 input으로 넘어감

--- src/components/CardNumberInput.tsx
+++ src/components/CardNumberInput.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from 'react';
+import { useEffect, useRef, ChangeEvent } from 'react';
 import '../styles/input.css';
 
 type CardNumber = {
@@ -15,13 +15,56 @@ type CardNumberInputProps = {
 const CardNumberInput = ({ numbers, onChange }: CardNumberInputProps) => {
   const { num1, num2, num3, num4 } = numbers;
 
+  const num1InputRef = useRef<HTMLInputElement>(null);
+  const num2InputRef = useRef<HTMLInputElement>(null);
+  const num3InputRef = useRef<HTMLInputElement>(null);
+  const num4InputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    num1InputRef.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    if (num1.length === 4) {
+      num2InputRef.current?.focus();
+    }
+  }, [num1]);
+
+  useEffect(() => {
+    if (num2.length === 4) {
+      num3InputRef.current?.focus();
+    }
+  }, [num2]);
+
+  useEffect(() => {
+    if (num3.length === 4) {
+      num4InputRef.current?.focus();
+    }
+  }, [num3]);
+
   return (
     <div className="input-container">
       <span className="input-title">카드 번호</span>
       <div className="input-box">
-        <input maxLength={4} className="input-basic w-25" type="text" onChange={onChange} value={num1} name="num1" />
+        <input
+          maxLength={4}
+          className="input-basic w-25"
+          type="text"
+          onChange={onChange}
+          value={num1}
+          name="num1"
+          ref={num1InputRef}
+        />
         {num1.length === 4 && <span>-</span>}
-        <input maxLength={4} className="input-basic w-25" type="text" onChange={onChange} value={num2} name="num2" />
+        <input
+          maxLength={4}
+          className="input-basic w-25"
+          type="text"
+          onChange={onChange}
+          value={num2}
+          name="num2"
+          ref={num2InputRef}
+        />
         {num2.length === 4 && <span>-</span>}
 
         <input
@@ -31,6 +74,7 @@ const CardNumberInput = ({ numbers, onChange }: CardNumberInputProps) => {
           onChange={onChange}
           value={num3}
           name="num3"
+          ref={num3InputRef}
         />
         {num3.length === 4 && <span>-</span>}
 
@@ -41,6 +85,7 @@ const CardNumberInput = ({ numbers, onChange }: CardNumberInputProps) => {
           onChange={onChange}
           value={num4}
           name="num4"
+          ref={num4InputRef}
         />
       </div>
     </div>
