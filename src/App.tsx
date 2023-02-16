7730da192e06ee63d41ccbb0d5f9be187051af45
Woo KyungJun <natcho9010@gmail.com>
2023-02-17 00:16:08 +0900

    chore: config lints

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { styled } from '@stitches/react';
+
+const Button = styled('button', {
+  backgroundColor: 'gainsboro',
+  borderRadius: '9999px',
+  fontSize: '13px',
+  padding: '10px 15px',
+  '&:hover': {
+    backgroundColor: 'lightgray',
+  },
+});
 
 function App() {
   return (
     <div className="App">
       app
+      <Button>Button</Button>
     </div>
   );
 }
