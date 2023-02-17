efc03929c54949a10b3ead0c7264202d9d75daac
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-17 21:28:17 +0900

    mod: Card 컴포넌트 업데이트

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,19 +1,37 @@
-function Card() {
+import { CardCompany, ExpiredDate, SerialNumber } from 'types/card';
+
+interface CardProps {
+  company: CardCompany;
+  name: string;
+  serialNumber: SerialNumber;
+  expiredDate: ExpiredDate;
+}
+
+function Card({ company, name, serialNumber, expiredDate }: CardProps) {
+  const { num1, num2, num3, num4 } = serialNumber;
+  const { year, month } = expiredDate;
+
   return (
-    <div className="small-card">
-      <div className="card-top">
-        <span className="card-text">클린카드</span>
-      </div>
-      <div className="card-middle">
-        <div className="small-card__chip"></div>
-      </div>
-      <div className="card-bottom">
-        <div className="card-bottom__number">
-          <span className="card-text">1111 - 2222 - oooo - oooo</span>
+    <div className="card-box">
+      <div className="small-card">
+        <div className="card-top">
+          <span className="card-text">{company}</span>
         </div>
-        <div className="card-bottom__info">
-          <span className="card-text">YUJO</span>
-          <span className="card-text">12 / 23</span>
+        <div className="card-middle">
+          <div className="small-card__chip"></div>
+        </div>
+        <div className="card-bottom">
+          <div className="card-bottom__number">
+            <span className="card-text">
+              {num1} - {num2} - {num3} - {num4}
+            </span>
+          </div>
+          <div className="card-bottom__info">
+            <span className="card-text">{name}</span>
+            <span className="card-text">
+              {month} / {year}
+            </span>
+          </div>
         </div>
       </div>
     </div>
