471959d28c186f577ff8d7a2b7bf6ed36ab4b5b8
Woo KyungJun <natcho9010@gmail.com>
2023-02-18 00:27:25 +0900

    chore: storybook & eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,44 @@
 module.exports = {
   root: true,
-
-  extends: ['airbnb', 'airbnb/hooks'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'prettier', '@typescript-eslint'],
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'react/require-default-props': 0,
+    'react/function-component-definition': 0,
+    'react/no-unescaped-entities': 1,
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
