b498b1c6e65e0bf968def963600bea79f39153c1
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-17 20:57:11 +0900

    feat: 카드 추가 페이지 템플릿 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
-import { Layout, CardList } from 'components';
+import { Layout } from 'components';
+import { AddingCard, HoldingCards } from 'pages';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route index element={<CardList />} />
+      <Route index element={<HoldingCards />} />
+      <Route path="new" element={<AddingCard />} />
     </Route>
   )
 );
---
+++ src/pages/index.ts
@@ -0,0 +1,2 @@
+export { default as HoldingCards } from './HoldingCards';
+export { default as AddingCard } from './AddingCard';
