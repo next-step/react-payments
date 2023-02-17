cbab162f46fb64887573fd0836e6a3d5e34bfcbd
jlee0505 <hj960322@gmail.com>
2023-02-17 12:00:26 +0900

    feat: 데이터 전역관리를 위한 store 생성

--- react-payments-app/src/index.jsx
+++ react-payments-app/src/index.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { CardContext } from "./store/CardContext";
 import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CardContext>
+        <App />
+      </CardContext>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ react-payments-app/src/store/CardContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+export const CardContext = createContext(null);
+
+export const CardProvider = ({ children }) => {
+  const [card, setCard] = useState({});
+
+  //TODO: 닉네임 상태관리
+
+  return (
+    <CardContext.Provider value={{ card, setCard }}>
+      {children}
+    </CardContext.Provider>
+  );
+};
+
+export function useCard() {
+  const context = useContext(CardContext);
+
+  if (!context) {
+    throw Error("Need to be within CardContext to use useCard.");
+  }
+
+  return context;
+}
