4de3a02f4ae460db22462590fc2253fcb225a647
ag502 <ag502@naver.com>
2023-02-18 01:28:22 +0900

    add: Card 컴포넌트 추가

---
+++ src/components/cards/Card/index.tsx
@@ -0,0 +1,59 @@
+import "@/styles/card.css";
+
+import React from "react";
+
+import {
+  CardBottomWrapper,
+  CardChip,
+  CardContainer,
+  CardInfoWrapper,
+  CardMiddleWrapper,
+  CardNumberWrapper,
+  CardText,
+  CardTopWrapper,
+} from "./card.style";
+
+type CardInfo = {
+  cardName: string;
+  cardNumber: [string, string, string, string];
+  cardOwnerName: string;
+  expireDate: string;
+};
+
+export interface CardProps {
+  size: "big" | "small";
+  className?: string;
+  color?: string;
+  cardInfo: CardInfo;
+}
+
+export default function Card({
+  size,
+  color = "#94dacd",
+  cardInfo,
+  className,
+}: CardProps) {
+  const { cardName, cardNumber, cardOwnerName, expireDate } = cardInfo;
+
+  return (
+    <CardContainer className={className} size={size} color={color}>
+      <CardTopWrapper>
+        <CardText>{cardName}</CardText>
+      </CardTopWrapper>
+      <CardMiddleWrapper>
+        <CardChip />
+      </CardMiddleWrapper>
+      <CardBottomWrapper>
+        <CardNumberWrapper>
+          {cardNumber.map((number, idx) => (
+            <CardText key={idx}>{number}</CardText>
+          ))}
+        </CardNumberWrapper>
+        <CardInfoWrapper>
+          <CardText>{cardOwnerName}</CardText>
+          <CardText>{expireDate}</CardText>
+        </CardInfoWrapper>
+      </CardBottomWrapper>
+    </CardContainer>
+  );
+}
--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,3 +1,4 @@
+export { default as Card } from "./Card";
 export { default as CardCvcInputEl } from "./CardCvcInput";
 export { default as CardExpireDateInput } from "./CardExpireDateInput";
 export { default as CardNumberInput } from "./CardNumberInput";
