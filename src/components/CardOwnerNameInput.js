415d3735264c41f847d1730468c4898681332098
realhee <wlsgml5628@gmail.com>
2023-02-18 01:38:53 +0900

    Chore: react-router-dom 설치

--- src/components/CardOwnerNameInput.js
+++ src/components/CardOwnerNameInput.js
@@ -3,6 +3,7 @@ import { CARD_OWNER_NAME } from "../constants/card";
 import Span from "./Span";
 
 // TODO : inputValue 상태명이 명시적인가?
+// TODO : 유효성 검사 함수명이 너무 비슷하진 않은지
 // TODO : 현재 입력 자릿수와 최대 입력 자릿수 위치 조정
 // TODO : state를 컴포넌트 내부로 가져와도 되나? 의미가 있는걸까?
 export default function CardOwnerNameInput({ type, placeholder, maxLength }) {
@@ -41,7 +42,7 @@ export default function CardOwnerNameInput({ type, placeholder, maxLength }) {
         카드 소유자 이름(선택)
         {"    "}
         {inputValue.length} {" / "}
-        {CARD_OWNER_NAME.MAX_LENGTH}{" "}
+        {CARD_OWNER_NAME.MAX_LENGTH}
       </Span>
       <input
         className="input-basic card-owner-name"
