02fc07eca7c0a6e70b835a45a7823db31efa6802
yujeong <yujeong.choung@gmail.com>
2023-02-18 00:07:39 +0900

    feat: payments 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Payments from "./pages/payments";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>React Clean Code Payments CSS example</h1>
+      <Payments />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/payments/index.tsx
@@ -0,0 +1,5 @@
+const index = () => {
+  return <div></div>;
+};
+
+export default index;
