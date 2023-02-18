abaa7949fcb34c693adb54a85798c00396d6d7ef
devsoonote <devsoo0527@gmail.com>
2023-02-18 18:29:55 +0900

    feat: delete type constant and interface, story button example

--- src/constants/ui.constant.ts
+++ src/constants/ui.constant.ts
@@ -1,13 +1,7 @@
 const UI_VARIANT = {
-  FILLED: 'filled',
-  OUTLINED: 'outlined',
+  FILL: 'fill',
+  OUTLINE: 'outline',
   GHOST: 'ghost',
-} as const
-
-const UI_TYPE = {
-  PRIMARY: 'primary',
-  SECONDARY: 'secondary',
-  DANGER: 'danger',
 } as const
 
 const UI_SIZE = {
@@ -16,4 +10,4 @@ const UI_SIZE = {
   LARGE: 'large',
 } as const
 
-export { UI_VARIANT, UI_TYPE, UI_SIZE }
+export { UI_VARIANT, UI_SIZE }
--- src/models/ui.model.ts
+++ src/models/ui.model.ts
@@ -1,5 +1,4 @@
-import { UI_SIZE, UI_TYPE, UI_VARIANT } from 'constants/ui.constant'
+import { UI_SIZE, UI_VARIANT } from 'constants/ui.constant'
 
 export type UiVariant = (typeof UI_VARIANT)[keyof typeof UI_VARIANT]
-export type UiType = (typeof UI_TYPE)[keyof typeof UI_TYPE]
 export type UiSize = (typeof UI_SIZE)[keyof typeof UI_SIZE]
