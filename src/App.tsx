bf199f73ed49032e57bfcadd080b2b3ed194410d
ag502 <ag502@naver.com>
2023-02-18 14:11:08 +0900

    add: 스타일 초기화 추가 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-// import "./styles/index.css";
-
 import React from "react";
 
 import GlobalStyle from "./styles/GlobalStyle";
+import ResetStyle from "./styles/ResetStyle";
 import { AddCard } from "./views/cards";
 
 function App() {
   return (
     <>
+      <ResetStyle />
       <GlobalStyle />
       <div className="App">
         <AddCard />
