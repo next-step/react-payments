0e815cc53174f83925d6758c4f0fdcdd87de63f9
Dongmin Ahn <eamon3481@naver.com>
2023-02-18 11:52:12 +0900

    feat : add PreviewCard component

--- src/pages/CreateCardPage/CreateCardPage.tsx
+++ src/pages/CreateCardPage/CreateCardPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { CardFieldProvider } from './components/CardFieldContext';
 import { CardFields } from './components/CardFields';
+import { PreviewCard } from './components/PreviewCard';
 
 const CreateCardPage = () => {
   return (
     <CardFieldProvider>
+      <PreviewCard />
       <CardFields />
     </CardFieldProvider>
   );
