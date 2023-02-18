7eb80c59cebf726ae006b4e57cf11ff9bde53e5b
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-19 08:06:30 +0900

    feat: InputProgress 컴포넌트 구현 및 스토리북 추가

---
+++ src/stories/InputProgress.stories.tsx
@@ -0,0 +1,12 @@
+import { Story } from '@storybook/react';
+import { InputProgress } from 'components';
+import { InputProgressProps } from 'components/InputProgress';
+
+export default {
+  title: 'InputProgress',
+  component: InputProgress,
+};
+
+export const Template: Story<InputProgressProps> = (args) => <InputProgress {...args} />;
+Template.args = { current: 0, max: 30 };
+Template.storyName = 'Playground';
