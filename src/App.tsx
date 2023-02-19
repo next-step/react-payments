1bcd1b743c49d8d0588b93866399fdb9fcf8c321
seyoungjoy <diddpwl80@gmail.com>
2023-02-19 21:50:28 +0900

    feat : layout 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,15 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path={'/'} element={<Layout />}>
+          <Route
+            path={'/'}
+            element={<Layout className={'flex-column-center'} />}
+          >
             <Route index element={<PaymentCardList />} />
+            <Route path={'/complete'} element={<PaymentCardComplete />} />{' '}
+          </Route>
+          <Route path={'/'} element={<Layout />}>
             <Route path={'/register'} element={<PaymentCardRegister />} />
-            <Route path={'/complete'} element={<PaymentCardComplete />} />
           </Route>
         </Routes>
       </div>
--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
 
-const Layout = () => {
+interface ILayout {
+  className?: string;
+}
+
+const Layout = ({ className }: ILayout) => {
   const S = {
     Root: styled.div`
       background-color: #fff;
@@ -15,11 +19,17 @@ const Layout = () => {
     App: styled.div`
       height: 100%;
       padding: 16px 24px;
+      &.flex-column-center {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
     `,
   };
   return (
     <S.Root>
-      <S.App>
+      <S.App className={className}>
         <Outlet />
       </S.App>
     </S.Root>
