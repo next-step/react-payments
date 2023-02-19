b2e57d547bac7babb1e6282dc4ebc50e4a92cd4e
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-19 12:12:41 +0900

    feat: SecretCodeContainer(보안코드) 컴포넌트 구현

--- src/constants/card.ts
+++ src/constants/card.ts
@@ -2,4 +2,5 @@ export const MAX_LENGTH = {
   CARD_NUMBER: 4,
   EXPIRED_DATE: 2,
   CARD_OWNER: 30,
+  SECRET_CODE: 3,
 };
--- src/pages/AddingCard/index.tsx
+++ src/pages/AddingCard/index.tsx
@@ -7,6 +7,7 @@ import {
 
 import { useCardNumber, useExpiredDate, useCardOwner } from './hooks';
 import { CardCompany } from 'types/card';
+import SecretCodeContainer from 'components/domain/SecretCodeContainer';
 
 function AddingCard() {
   const { cardNumber, handleChangeCardNumber } = useCardNumber();
@@ -32,10 +33,7 @@ function AddingCard() {
         handleChangeExpiredDate={handleChangeExpiredDate}
       />
       <CardOwnerContainer cardOwner={cardOwner} handleChangeCardOwner={handleChangeCardOwner} />
-      <div className="input-container">
-        <span className="input-title">보안코드(CVC/CVV)</span>
-        <input className="input-basic w-25" type="password" />
-      </div>
+      <SecretCodeContainer />
       <div className="input-container">
         <span className="input-title">카드 비밀번호</span>
         <input className="input-basic w-15" type="password" />
