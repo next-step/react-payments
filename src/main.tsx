500804a4f3feb395269f2b36c011a666ed4c2370
minhyeok0328 <alsgur7408@gmail.com>
2023-02-19 15:34:32 +0900

    fix: react router basename 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-payments">
       <App/>
     </BrowserRouter>
   </React.StrictMode>,
