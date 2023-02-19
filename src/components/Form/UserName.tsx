0da7d4f71410fc3ff4cd61abe6281f9bd759e1d4
zestlee1106 <zestlee1106@gmail.com>
2023-02-19 14:52:56 +0900

    [fix] 요구사항 반영

--- src/components/Form/UserName.tsx
+++ src/components/Form/UserName.tsx
@@ -14,6 +14,7 @@ function UserName({ onUserNameChange }: CardNumberProps) {
           placeholder="카드에 표시된 이름과 동일하게 입력하세요."
           onChange={onChange}
           name="username"
+          maxLength={30}
         ></Input>
       </InputBox>
     </InputContainer>
