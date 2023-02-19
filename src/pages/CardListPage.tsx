3845f02d324a27522733f5a1ef1e2a54538cddb4
dahye1013 <alrean@naver.com>
2023-02-19 23:45:01 +0900

    feat: 카드 리스트 페이지 라우팅 수정

--- src/pages/CardListPage.tsx
+++ src/pages/CardListPage.tsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 export const CardListPage = () => {
-    return ((
-        <div className="app flex-column-center">
-            <div className="flex-center">
-                <h2 className="page-title mb-10">보유 카드</h2>
-            </div>
-            <div className="card-box">
-                <Link className="empty-card" to={"/register"}>+</Link>
-            </div>
-        </div>
-    ))
-}
+  return (
+    <div className="app flex-column-center">
+      <div className="flex-center">
+        <h2 className="page-title mb-10">보유 카드</h2>
+      </div>
+      <div className="card-box">
+        <Link className="empty-card" to={'/register'}>
+          +
+        </Link>
+      </div>
+    </div>
+  );
+};
