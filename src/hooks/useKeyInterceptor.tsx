bdddbcfe9a7f1efd7b629b87d4b6ebba9e6af25b
dahye1013 <alrean@naver.com>
2023-02-19 23:36:23 +0900

    feat: number key input 제어 interceptor hook

---
+++ src/hooks/useKeyInterceptor.tsx
@@ -0,0 +1,9 @@
+export const useKeyInterceptor = () => {
+  const keyPressInterceptor = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const { key } = e;
+    if (Number.isNaN(Number(key)) && key !== 'Backspace') {
+      e.preventDefault();
+    }
+  };
+  return keyPressInterceptor;
+};
