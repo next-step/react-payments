e18071d22f426acba095437bc1100ef99be8e6c8
dahye1013 <alrean@naver.com>
2023-02-19 23:46:31 +0900

    feat: 카드 등록 페이지 cardform 컴포넌트 기반의 등록 구현

--- src/pages/CardRegisterPage.tsx
+++ src/pages/CardRegisterPage.tsx
@@ -1,5 +1,98 @@
-import { 카드추가_페이지 } from "../../stories/pages/1.카드추가_페이지"
+import { useCallback, useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { CardForm, CreditCard, TopNavigation } from '@/components';
+import { Button } from '@/components/UI/Button';
+import { getItem, setItem } from '@/storage/storage';
+import { CardKey, CardObj } from '@/types';
+
+import { styled } from '../lib/stitches.config';
+export const initialCardState = {
+  [CardKey.CARD_NUMBERS]: {
+    val: {
+      1: '',
+      2: '',
+      3: '',
+      4: '',
+    },
+    isValid: false,
+  },
+  [CardKey.EXPIRE_DATE]: {
+    val: {
+      month: '',
+      year: '',
+    },
+    isValid: false,
+  },
+  [CardKey.CVC]: {
+    val: '',
+    isValid: false,
+  },
+  [CardKey.PASSWORD]: {
+    val: '',
+    isValid: false,
+  },
+  [CardKey.OWNER_NAME]: {
+    val: '',
+    isValid: false,
+  },
+} as const;
 
 export const CardRegisterPage = () => {
-    return (<카드추가_페이지 />)
-}
+  const navigate = useNavigate();
+  const [cardState, setCardState] = useState(initialCardState);
+  const cardDisplayInfo = useMemo(
+    () => ({
+      [CardKey.CARD_NUMBERS]: cardState[CardKey.CARD_NUMBERS].val,
+      [CardKey.OWNER_NAME]: cardState[CardKey.OWNER_NAME].val,
+      [CardKey.EXPIRE_DATE]: cardState[CardKey.EXPIRE_DATE].val,
+    }),
+    [cardState]
+  );
+
+  const handleCardState = useCallback((key: CardKey, state: CardObj) => {
+    setCardState((prev) => ({
+      ...prev,
+      [key]: { ...state },
+    }));
+  }, []);
+
+  const isAllValid = Object.values(cardState).every((input) => input.isValid);
+
+  const handleSubmit = () => {
+    const newCard = Object.entries(cardState).reduce((acc, cur) => {
+      return { ...acc, [cur[0]]: cur[1].val };
+    }, {});
+
+    setItem('cardList', [...(getItem('cardList') ?? []), newCard]);
+    navigate('/register-confirm');
+  };
+
+  return (
+    <CardRegisterPageLayout>
+      <TopNavigation />
+      <CreditCard cardInfo={cardDisplayInfo} />
+      <CardForm onChangeCardForm={handleCardState} />
+      <Button disabled={!isAllValid} onClick={handleSubmit}>
+        추가하기
+      </Button>
+    </CardRegisterPageLayout>
+  );
+};
+
+const CardRegisterPageLayout = styled('div', {
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'space-around',
+  flexDirection: 'column',
+  alignItems: 'center',
+  placeContent: 'end center',
+  height: '100%',
+  margin: '0 2rem',
+
+  [`& > button`]: {
+    position: 'absolute',
+    bottom: '1rem',
+    right: '0',
+  },
+});
