444f32aa6d9a6f4e735144290c9c311205c5bcd6
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-19 14:29:31 +0900

    mod: 카드 추가 페이지 CardCompanyPicker 추가

--- src/pages/AddingCard/index.tsx
+++ src/pages/AddingCard/index.tsx
@@ -4,41 +4,46 @@ import {
   ExpiredDateContainer,
   CardOwnerContainer,
   CardPasswordContainer,
+  SecretCodeContainer,
+  CardCompanyPicker,
 } from 'components/domain';
-
-import { useCardNumber, useExpiredDate, useCardOwner } from './hooks';
-import { CardCompany } from 'types/card';
-import SecretCodeContainer from 'components/domain/SecretCodeContainer';
+import { useCardNumber, useExpiredDate, useCardOwner, useCompanyPicker } from './hooks';
 
 function AddingCard() {
   const { cardNumber, handleChangeCardNumber } = useCardNumber();
   const { expiredDate, handleChangeExpiredDate } = useExpiredDate();
   const { cardOwner, handleChangeCardOwner } = useCardOwner();
+  const { open, company, updateCompany, openPicker } = useCompanyPicker();
 
   return (
     <div className="app">
       <h2 className="page-title">{`<`} 카드 추가</h2>
       <span className="input-title">카드사 선택</span>
-      <Card
-        cardNumber={cardNumber}
-        name={cardOwner}
-        company={CardCompany.Hana}
-        expiredDate={expiredDate}
-      />
-      <CardNumberContainer
-        cardNumber={cardNumber}
-        handleChangeCardNumber={handleChangeCardNumber}
-      />
-      <ExpiredDateContainer
-        expiredDate={expiredDate}
-        handleChangeExpiredDate={handleChangeExpiredDate}
-      />
-      <CardOwnerContainer cardOwner={cardOwner} handleChangeCardOwner={handleChangeCardOwner} />
-      <SecretCodeContainer />
-      <CardPasswordContainer />
-      <div className="button-box">
-        <span className="button-text">다음</span>
+      <div onClick={openPicker}>
+        <Card
+          cardNumber={cardNumber}
+          name={cardOwner}
+          company={company}
+          expiredDate={expiredDate}
+        />
       </div>
+      <form>
+        <CardNumberContainer
+          cardNumber={cardNumber}
+          handleChangeCardNumber={handleChangeCardNumber}
+        />
+        <ExpiredDateContainer
+          expiredDate={expiredDate}
+          handleChangeExpiredDate={handleChangeExpiredDate}
+        />
+        <CardOwnerContainer cardOwner={cardOwner} handleChangeCardOwner={handleChangeCardOwner} />
+        <SecretCodeContainer />
+        <CardPasswordContainer />
+        <div className="button-box">
+          <span className="button-text">다음</span>
+        </div>
+      </form>
+      {open && <CardCompanyPicker updateCompany={updateCompany} />}
     </div>
   );
 }
