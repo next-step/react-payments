112ad538c7c90d9423f9a7a374635c3c835b55dd
dahye1013 <alrean@naver.com>
2023-02-19 23:37:30 +0900

    feat: local storage setter, getter 함수

---
+++ src/storage/storage.ts
@@ -0,0 +1,13 @@
+export const setItem = (key: string, value: unknown) => {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (err) {
+    throw new Error('Fail to set Item in LocalStorage.');
+  }
+};
+
+export const getItem = (key: string) => {
+  const json = localStorage.getItem(key);
+  if (json === null) return null;
+  return JSON.parse(json);
+};
