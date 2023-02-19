69e448d0ce122e8f45757c8031cf934ac9531f41
dahye1013 <alrean@naver.com>
2023-02-19 23:48:53 +0900

    fix: 누락된 기본 타입 정의

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type CardObj = {
+  val: string | string | { [key: string]: number | string };
+  isValid?: boolean;
+};
+
+export const enum CardKey {
+  CARD_NUMBERS = 'CARD_NUMBERS',
+  EXPIRE_DATE = 'EXPIRE_DATE',
+  CVC = 'CVC',
+  PASSWORD = 'PASSWORD',
+  OWNER_NAME = 'OWNER_NAME',
+}
