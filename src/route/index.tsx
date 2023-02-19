9704e4c0dab342b1601170c0f53f3d79ca6c4e5c
dahye1013 <alrean@naver.com>
2023-02-19 23:43:19 +0900

    chore: router 불필요한 import 구문 제거

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,18 +1,18 @@
-import { Routes as ReactRouters, Route, Outlet, Link, Navigate } from "react-router-dom";
+import { Navigate, Route, Routes as ReactRouters } from 'react-router-dom';
+
 import { CardListPage } from '@/pages/CardListPage';
 import { CardRegisterConfirmPage } from '@/pages/CardRegisterConfirmPage';
 import { CardRegisterPage } from '@/pages/CardRegisterPage';
 
 const Routes = () => {
-    return (
-        <ReactRouters>
-            <Route path="/register" element={<CardRegisterPage />} />
-            <Route path="/register-confirm" element={<CardRegisterConfirmPage />} />
-            <Route path="/list" element={<CardListPage />} />
-            <Route path="*" element={<Navigate replace to="/list" />} />
-        </ReactRouters>
-    );
+  return (
+    <ReactRouters>
+      <Route path="/register" element={<CardRegisterPage />} />
+      <Route path="/register-confirm" element={<CardRegisterConfirmPage />} />
+      <Route path="/list" element={<CardListPage />} />
+      <Route path="*" element={<Navigate replace to="/list" />} />
+    </ReactRouters>
+  );
 };
-
 
 export default Routes;
