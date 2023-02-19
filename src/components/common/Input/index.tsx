95e87f413a5bce25d9eb1f5028bc8ccdd15cca8d
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-19 12:11:59 +0900

    mod: Input size 속성 추가 및 스토리북 수정

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -3,13 +3,25 @@ import { CSSProperties, forwardRef } from 'react';
 
 import type { CombineElementProps } from 'types/utils';
 
+export type ButtonSize = 'small' | 'medium' | 'large' | 'full';
+
+const inputSize = {
+  small: '15%',
+  medium: '25%',
+  large: '50%',
+  full: '100%',
+};
+
 interface InputBaseProps {
   textAlign?: CSSProperties['textAlign'];
+  size?: ButtonSize;
 }
 export type InputProps = CombineElementProps<'input', InputBaseProps>;
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, textAlign = 'center', ...props }, ref) => {
+  ({ className, textAlign = 'center', size = 'full', ...props }, ref) => {
+    const width = inputSize[size];
+
     return (
       <input
         ref={ref}
@@ -18,7 +30,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           css`
             background-color: #ecebf1;
             height: 45px;
-            width: 100%;
+            width: ${width};
             padding: 0 12px;
             outline: 2px solid transparent;
             text-align: ${textAlign};
