8533694f42366e4ec3c453f49314a8bf26b2729f
tooooo1 <wer4272@nate.com>
2023-02-20 20:42:14 +0900

    refactor: useRefs

--- src/hooks/useRefs.ts
+++ src/hooks/useRefs.ts
@@ -3,12 +3,10 @@ import { useMemo, useRef } from 'react';
 const useRefs = <T extends HTMLElement = HTMLElement>(
   length: number,
   defaultValue: T | null = null
-) => {
-  const refsArray = useMemo(() => {
-    return Array.from({ length }, () => useRef<T>(defaultValue));
-  }, [length, defaultValue]);
-
-  return refsArray;
-};
+) =>
+  useMemo(
+    () => Array.from({ length }, () => useRef<T>(defaultValue)),
+    [length, defaultValue]
+  );
 
 export default useRefs;
