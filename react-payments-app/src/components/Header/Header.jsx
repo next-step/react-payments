98542c6bcd9db09669b709ecac4708bc6aadc854
jlee0505 <hj960322@gmail.com>
2023-02-21 01:16:39 +0900

    chore: 불필요한 fragment 삭제

--- react-payments-app/src/components/Header/Header.jsx
+++ react-payments-app/src/components/Header/Header.jsx
@@ -1,21 +1,19 @@
 const Header = ({ pageTitle, headerIcon, onClick }) => {
   return (
-    <>
-      <div id='header' className='page-title mb-10'>
-        {headerIcon && (
-          <div
-            id='icon'
-            className='cursor-pointer'
-            onClick={(e) => {
-              onClick(e);
-            }}
-          >
-            {headerIcon}
-          </div>
-        )}
-        <div className='flex-center ml-10'>{pageTitle}</div>
-      </div>
-    </>
+    <div id='header' className='page-title mb-10'>
+      {headerIcon && (
+        <div
+          id='icon'
+          className='cursor-pointer'
+          onClick={(e) => {
+            onClick(e);
+          }}
+        >
+          {headerIcon}
+        </div>
+      )}
+      <div className='flex-center ml-10'>{pageTitle}</div>
+    </div>
   );
 };
 
