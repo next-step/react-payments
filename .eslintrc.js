3c229519f9a6e3469f1a4b1601ce95cda0bcd243
hsw824 <ghdtjddn0824@naver.com>
2023-02-20 21:46:37 +0900

    feat: 스토리북 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'airbnb',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['airbnb', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
   rules: {
     'react/react-in-jsx-scope': 0,
     'linebreak-style': 0,
@@ -16,11 +10,16 @@ module.exports = {
     'import/extensions': 0,
     'no-use-before-define': 0,
     'import/no-unresolved': 0,
-    'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+    'import/no-extraneous-dependencies': 0,
+    // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
     'no-shadow': 0,
     'react/prop-types': 0,
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }],
     'jsx-a11y/no-noninteractive-element-interactions': 0,
-    'react/function-component-definition': [2, { namedComponents: ['arrow-function', 'function-declaration'] }],
-  },
+    'react/function-component-definition': [2, {
+      namedComponents: ['arrow-function', 'function-declaration']
+    }]
+  }
 };
---
+++ src/components/Input.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, Story } from '@storybook/react';
+import { ChangeEventHandler } from 'react';
+import Input from './Input';
+
+export default {
+  title: 'Component/Input',
+  component: Input,
+  argTypes: {
+    changeHandler: { action: 'onChange' },
+  } as Meta,
+};
+interface ArgsType {
+  changeHandler: () => void;
+}
+const Template: Story<ArgsType> = (args) => <Input {...args} />;
+
+export const Default = Template.bind({});
+
+export const CardNumInput = Template.bind({});
+CardNumInput.args = {
+  state: '',
+};
+
+// export const PWInput = Template.bind({});
+// PWInput.args = {
+//   id: 'password',
+//   type: 'password',
+//   placeholder: '비밀번호를 입력하세요.',
+// };
