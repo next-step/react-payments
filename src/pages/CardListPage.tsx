bcd39c98fa5a3e4658093c687974f9bb339c91e1
areumsheep <areumsheep@gmail.com>
2023-02-21 03:29:11 +0900

    Add: 카드 목록 페이지 퍼블리싱

--- src/pages/CardListPage.tsx
+++ src/pages/CardListPage.tsx
@@ -1,5 +1,40 @@
+import styled from '@emotion/styled';
+
+import { CreditCard } from 'components';
+import { Box, TextField, Button } from 'components/@common';
+import type { CreditCardProps } from 'components/CreditCard/CreditCard.types';
+import useRouter from 'routes/useRouter';
+import { ReactComponent as PlusIcon } from 'assets/PlusIcon.svg';
+
+import { getLocalStorageItem } from 'utils/localStorage';
+
 const CardListPage = () => {
-  return <div>CardListPage</div>;
+  const { push } = useRouter();
+  const item = getLocalStorageItem<CreditCardProps>('CardValues');
+
+  return (
+    <Wrapper display="flex" alignItems="center" justifyContent="center">
+      <CardAddButton
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        onClick={() => push('/card-registration')}
+      >
+        <PlusIcon />
+      </CardAddButton>
+    </Wrapper>
+  );
 };
 
 export default CardListPage;
+
+const Wrapper = styled(Box)`
+  height: 100%;
+`;
+const CardAddButton = styled(Box)`
+  width: 70%;
+  height: 150px;
+  border-radius: 10px;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.color.gray03};
+`;
