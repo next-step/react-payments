396afe14dedf1f3c56517136bed78e3a4b8fa371
areumsheep <areumsheep@gmail.com>
2023-02-21 04:57:54 +0900

    Add: 스토리북에 theme, globalStyle 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,14 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+import { Global, ThemeProvider } from '@emotion/react';
+import { theme } from 'styles/theme';
+import { globalStyles } from 'styles/globalStyles';
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      {Story(context)}
+    </ThemeProvider>
+  );
+};
+
+export const decorators = [withThemeProvider];
