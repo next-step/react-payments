fc68ef46836b172747c9e0367a4522ca7bfb10c8
Leebonggu <leebonggu12@hanmail.net>
2023-02-22 00:42:32 +0900

    refactor: 타입선언 중복제거

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,33 +1,27 @@
-type CardInformation = {
+interface Expiration {
+  month: string;
+  year: string;
+}
+
+interface CardNumber {
   cardNumber1: string;
   cardNumber2: string;
   cardNumber3: string;
   cardNumber4: string;
-  year: string;
-  month: string;
-  cvc: string;
+}
+
+interface Password {
   password1: string;
   password2: string;
-  cardOwner?: string;
-  nickname?: string;
-  cardCompany?: string;
-};
+}
 
-type Expiration = {
-  month: string;
-  year: string;
-};
-
-type CardNumber = {
-  cardNumber1: string;
-  cardNumber2: string;
-  cardNumber3: string;
-  cardNumber4: string;
-};
-
-type Password = {
-  password1: string;
-  password2: string;
-};
+type CardInformation = Expiration &
+  Password &
+  CardNumber & {
+    cvc: string;
+    cardOwner?: string;
+    nickname?: string;
+    cardCompany?: string;
+  };
 
 export type { CardInformation, Expiration, CardNumber, Password };
