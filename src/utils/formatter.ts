4b9b6193b34b98e21d3cd0edf68266d527be98ee
Dongmin Ahn <eamon3481@naver.com>
2023-02-22 02:29:30 +0900

    refactor : addSeparatorsToNumber

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,35 +1,21 @@
-export const SEPARATOR = ' / ';
-// TODO : 추상화 필요 (addDashes 와 비슷한 로직)
-export const addSeparator = (str: string) => {
-  const monthTextLength = 2;
-  const textLength = str.length;
-  if (textLength < monthTextLength) return str;
-  const formatted = str.replace(/(.{2})/g, `$1${SEPARATOR}`);
-  if (textLength === monthTextLength * 2 || textLength === monthTextLength) {
-    return formatted.slice(0, -SEPARATOR.length);
-  }
-  return formatted;
+export const addSeparatorsToNumber = (
+  str: string,
+  separator: string,
+  sectionLength: number
+) => {
+  const chunks = str.match(new RegExp(`.{1,${sectionLength}}`, 'g')) || [];
+  return chunks.join(separator);
 };
+
+export const SEPARATOR = ' / ';
+export const addSeparator = (str: string) =>
+  addSeparatorsToNumber(str, SEPARATOR, 2);
 
 export const DASH = ' - ';
 const CardNumberSectionLength = 4;
 
-// TODO : 카드 넘버 도메인과 많이 의존하는 로직인데 formatter에 있어야 할까?
-//  nested 된 컴포넌트안에서 사용하고 있으면, 찾기 어려워서 빼놓았다.
-export const addCardNumberDashes = (str: string) => {
-  const textLength = str.length;
-  if (textLength < 4) return str;
-  const formatted = str.replace(/(.{4})/g, `$1${DASH}`);
-  if (
-    textLength === CardNumberSectionLength ||
-    textLength === CardNumberSectionLength * 2 ||
-    textLength === CardNumberSectionLength * 3 ||
-    textLength === CardNumberSectionLength * 4
-  ) {
-    return formatted.slice(0, -DASH.length);
-  }
-  return formatted;
-};
+export const addCardNumberDashes = (str: string) =>
+  addSeparatorsToNumber(str, DASH, CardNumberSectionLength);
 
 const replaceCardNumber = (replaceStr: string) => (str: string) => {
   const visibleSectionCount = 2;
