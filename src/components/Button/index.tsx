4b5a46dd4431c8ef90deb9a5106df990daf5513b
tooooo1 <wer4272@nate.com>
2023-02-21 16:31:59 +0900

    refactor: 주석삭제, 옵셔널

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 interface ButtonProp {
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 const Button = ({ children, onClick }: PropsWithChildren<ButtonProp>) => {
