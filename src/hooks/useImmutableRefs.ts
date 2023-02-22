958cbeb9f8a9a98aaab02569b7cd315b7b3076a8
tooooo1 <wer4272@nate.com>
2023-02-22 10:57:58 +0900

    rename: useImmutableRefs -> useRefObjects

--- src/hooks/useImmutableRefs.ts
+++
@@ -1,7 +0,0 @@
-import { createRef, useMemo } from 'react';
-
-const useImmutableRefs = <T extends HTMLElement = HTMLElement>(
-  initialLength: number
-) => useMemo(() => Array.from({ length: initialLength }).map(createRef<T>), []);
-
-export default useImmutableRefs;
