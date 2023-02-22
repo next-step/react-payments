88cea27883a6b0b16b3d3250b1387bc05139e6d1
areumsheep <areumsheep@gmail.com>
2023-02-22 17:55:43 +0900

    Add: Help 스토리북 추가

---
+++ src/components/Help/Help.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta } from '@storybook/react';
+import { PropsWithChildren } from 'react';
+
+import Help from './Help';
+
+export default {
+  title: 'Custom/Help',
+  component: Help,
+  argTypes: {
+    children: {
+      name: 'children',
+      type: 'string',
+    },
+  },
+} as Meta;
+
+export const Primary = ({ children }: PropsWithChildren) => (
+  <Help>
+    <p>{children}</p>
+  </Help>
+);
+
+Primary.args = {
+  children: '신용 카드 앞면의 16자를 입력해주세요.',
+};
--- src/components/Help/Help.tsx
+++ src/components/Help/Help.tsx
@@ -4,11 +4,9 @@ import { ReactComponent as HelpIcon } from 'assets/HelpIcon.svg';
 
 const Help = ({ children }: PropsWithChildren) => {
   return (
-    <>
-      <Tooltip content={children}>
-        <HelpIcon className="mr-5 ml-5" />
-      </Tooltip>
-    </>
+    <Tooltip content={children}>
+      <HelpIcon className="mr-5 ml-5" />
+    </Tooltip>
   );
 };
 
