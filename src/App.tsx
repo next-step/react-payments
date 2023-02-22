5c5fb41d77e5681a30f4ff1f60fbc73133c22c24
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-22 21:55:43 +0900

    mod: router 객체 분리 및 라우팅 경로 상수화

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,6 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
-import { AddingCard, ConfirmCard, HoldingCards } from 'pages';
-import { Layout } from 'components/common';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<HoldingCards />} />
-      <Route path="new" element={<AddingCard />} />
-      <Route path="confirm" element={<ConfirmCard />} />
-    </Route>
-  )
-);
+import router from 'router';
 
 function App() {
   return <RouterProvider router={router} />;
