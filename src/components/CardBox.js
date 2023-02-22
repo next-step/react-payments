7f6d2de0379190adc03693c781f6b966658eb0fd
realhee <wlsgml5628@gmail.com>
2023-02-22 20:16:41 +0900

    Refactor: masking 함수 utils로 분리

--- src/components/CardBox.js
+++ src/components/CardBox.js
@@ -1,17 +1,6 @@
-export default function CardBox({ cardNumber, cardOwnerName, cardExpiration }) {
-  function maskingCardNumber(cardNumber) {
-    const cardNumberArray = Object.values(cardNumber);    
-    return cardNumberArray.map((value, index) => {
-      return (index < 3 && value.length === 4)? value + "-" : value;
-    }).join("");
-  }
+import { maskingCardNumber, maskingCardExpiration } from "../utils/card";
 
-  function maskingCardExpiration(cardExpiration) {
-    const cardExpirationArray = Object.values(cardExpiration);
-    return cardExpirationArray.map((value, index) => {
-      return (index == 0 && value < 13 && value > 0)? value + " / " : value;
-    }).join("");
-  }
+export default function CardBox({ cardNumber, cardOwnerName, cardExpiration }) {  
   return (
     <div className="card-box">
       <div className="empty-card">
