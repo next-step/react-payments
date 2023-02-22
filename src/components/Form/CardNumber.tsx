ee4647d61eabef9b4592662921eabf81c306ebfd
zestlee1106 <zestlee1106@gmail.com>
2023-02-22 18:31:01 +0900

    에러 발생한 채로.. 일단 푸시

--- src/components/Form/CardNumber.tsx
+++ src/components/Form/CardNumber.tsx
@@ -7,8 +7,20 @@ import InputContainer from "../Input/InputContainer";
 const CARD_MAX_LENGTH = 4; // 카드에 들어가는 최대 길이
 const PASSWORD_TYPE_START_INDEX = 2; // 패스워드 타입 시작 인덱스
 
+type CardNumbers = {
+  1: string;
+  2: string;
+  3: string;
+  4: string;
+};
+
 function CardNumber({ onCardNumberChange }: CardNumberProps) {
-  const [cardNumbers, setCardNumbers] = useState([0, 0, 0, 0]);
+  const [cardNumbers, setCardNumbers] = useState<CardNumbers>({
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+  });
 
   const itemsRef = useRef<any>([]);
 
@@ -18,39 +30,52 @@ function CardNumber({ onCardNumberChange }: CardNumberProps) {
 
   const onChange = (
     event: React.ChangeEvent<HTMLInputElement>,
-    order: number
+    type: number
   ) => {
     event.currentTarget.value = remainOnlyNumber(event.currentTarget.value);
 
-    let newArr = [...cardNumbers];
-    newArr[order] = Number(event.currentTarget.value);
+    const numbers: CardNumbers = {
+      ...cardNumbers,
+      [type]: event.currentTarget.value,
+    };
 
-    const isMaxLength = String(newArr[order]).length === CARD_MAX_LENGTH;
-    const isLastCardNumberIndex = order === cardNumbers.length - 1;
+    const isMaxLength = event.currentTarget.value.length === CARD_MAX_LENGTH;
+    const isLastCardNumberIndex = type === 4;
+
     if (isMaxLength && !isLastCardNumberIndex) {
-      itemsRef.current[order + 1].focus();
+      itemsRef.current[type + 1].focus();
     }
 
-    setCardNumbers(newArr);
+    // let newArr = [...cardNumbers];
+    // newArr[order] = Number(event.currentTarget.value);
+
+    // const isMaxLength = String(newArr[order]).length === CARD_MAX_LENGTH;
+    // const isLastCardNumberIndex = order === cardNumbers.length - 1;
+    // if (isMaxLength && !isLastCardNumberIndex) {
+    //   itemsRef.current[order + 1].focus();
+    // }
+
+    setCardNumbers(numbers);
   };
 
   return (
-    <InputContainer label="카드 번호">
-      <InputBox>
-        {cardNumbers.map((cardNumber, index) => (
-          <Input
-            onChange={(event) => onChange(event, index)}
-            maxLength={CARD_MAX_LENGTH}
-            name={`card-${index}`}
-            key={`card-${index}`}
-            type={index < PASSWORD_TYPE_START_INDEX ? "text" : "password"}
-            forwardRef={(el: HTMLInputElement) =>
-              (itemsRef.current[index] = el)
-            }
-          ></Input>
-        ))}
-      </InputBox>
-    </InputContainer>
+    <div></div>
+    // <InputContainer label="카드 번호">
+    //   <InputBox>
+    //     {Object.values(cardNumbers).map((key, index) => (
+    //       <Input
+    //         onChange={(event) => onChange(event, index)}
+    //         maxLength={CARD_MAX_LENGTH}
+    //         name={`card-${index}`}
+    //         key={`card-${index}`}
+    //         type={index < PASSWORD_TYPE_START_INDEX ? "text" : "password"}
+    //         forwardRef={(el: HTMLInputElement) =>
+    //           (itemsRef.current[index] = el)
+    //         }
+    //       ></Input>
+    //     ))}
+    //   </InputBox>
+    // </InputContainer>
   );
 }
 
