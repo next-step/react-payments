a1c6f0a346c67c129b793106e491302957aead09
areumsheep <areumsheep@gmail.com>
2023-02-22 17:56:12 +0900

    Chore: Storybook svg 설정 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,12 +12,39 @@ module.exports = {
     builder: '@storybook/builder-webpack5',
   },
   webpackFinal: async (config) => {
+    // configure for absolute imports
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
       new TsconfigPathsPlugin({
         extensions: config.resolve.extensions,
       }),
     ];
+
+    // disable whatever is already set to load SVGs
+    config.module.rules
+      .filter((rule) => rule.test.test('.svg'))
+      .forEach((rule) => (rule.exclude = /\.svg$/i));
+
+    // add SVGR instead
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+        {
+          loader: 'file-loader',
+          options: {
+            name: 'static/media/[path][name].[ext]',
+          },
+        },
+      ],
+      type: 'javascript/auto',
+      issuer: {
+        and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+      },
+    });
+
     return config;
   },
 };
