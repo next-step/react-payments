25098dd6fb8ffd742505a26f4b3721f6ee96301f
devsoonote <devsoo0527@gmail.com>
2023-02-23 21:02:56 +0900

    feat: update Button storybook

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -3,15 +3,28 @@ import React from 'react'
 import './Button.css'
 
 type ButtonProps = {
+  /** 버튼 사이즈 */
   size: UiSize
+  /** 버튼 종류 */
   variant: UiVariant
+  /** 버튼 컬러 */
   color: string
+  /** 클릭 이벤트 */
   onClick: () => void
+  /** 버튼 내부에 jsx 및 컴포넌트 추가 가능 */
   children: React.ReactNode
+  /** 버튼 클릭 이벤트 설정 유무 */
   isDisabled?: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({ size, variant, onClick, color, children, isDisabled }) => {
+const Button: React.FC<ButtonProps> = ({
+  size,
+  variant = 'fill',
+  onClick,
+  color,
+  children,
+  isDisabled = false,
+}) => {
   const fillStyle = {
     backgroundColor: color,
     color: '#fff',
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
 import { Button } from '../components/atoms/Button'
+import { UI_SIZE, UI_VARIANT } from 'constants/ui.constant'
 
 export default {
   title: 'Button',
@@ -10,11 +12,32 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
-export const Basic = Template.bind({})
-Basic.args = {
-  variant: 'fill',
+export const Fill = Template.bind({})
+Fill.args = {
+  variant: UI_VARIANT.FILL,
   children: 'Button',
-  size: 'small',
+  size: UI_SIZE.SMALL,
   color: '#94dacd',
   isDisabled: false,
+  onClick: action('onClick'),
+}
+
+export const Outline = Template.bind({})
+Outline.args = {
+  variant: UI_VARIANT.OUTLINE,
+  children: 'Button',
+  size: UI_SIZE.SMALL,
+  color: '#94dacd',
+  isDisabled: false,
+  onClick: action('onClick'),
+}
+
+export const Ghost = Template.bind({})
+Ghost.args = {
+  variant: UI_VARIANT.GHOST,
+  children: 'Button',
+  size: UI_SIZE.SMALL,
+  color: '#94dacd',
+  isDisabled: false,
+  onClick: action('onClick'),
 }
