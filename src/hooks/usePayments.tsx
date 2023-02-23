d01e215409a312cfd610f47de3352f2c016006d7
yujeong <yujeong.choung@gmail.com>
2023-02-23 20:06:34 +0900

    feat: default case 명시

--- src/hooks/usePayments.tsx
+++ src/hooks/usePayments.tsx
@@ -58,7 +58,9 @@ export const usePayments = () => {
         }
         return formatNumber({ input, nth: 3, formatter: "/" });
       case CARD_INFO.CVC:
-      case CARD_INFO.EXPIRY:
+      case CARD_INFO.PASSWORD1:
+      case CARD_INFO.PASSWORD2:
+      case CARD_INFO.NICKNAME:
       default:
         return input;
     }
