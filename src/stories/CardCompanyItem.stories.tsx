3439d0e4336128f01d1bffa0fcef7a1a3f4a9527
devsoonote <devsoo0527@gmail.com>
2023-02-23 22:32:33 +0900

    feat" create CardCompanyItem component

---
+++ src/stories/CardCompanyItem.stories.tsx
@@ -0,0 +1,23 @@
+import { action } from '@storybook/addon-actions'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { CardCompanyItem } from 'components/atoms/CardCompanyItem'
+import { CARD_COMPNAYS_CODE } from 'constants/card'
+import React from 'react'
+
+export default {
+  title: 'CardCompanyItem',
+  component: CardCompanyItem,
+  parameters: {
+    componentSubtitle: '카드 회사 컬러 버튼 컴포넌트',
+  },
+} as ComponentMeta<typeof CardCompanyItem>
+
+const Template: ComponentStory<typeof CardCompanyItem> = (args) => (
+  <CardCompanyItem {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  cardCode: CARD_COMPNAYS_CODE.KB,
+  onClick: action('onClick'),
+}
