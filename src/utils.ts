f0ece87a9107c193a8a5261e42d41cf3c83f059e
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-23 23:47:53 +0900

    chore: 오타 수정

--- src/utils.ts
+++ src/utils.ts
@@ -3,8 +3,8 @@ export const isNumber = (value: string) => /^\d+$/.test(value);
 export const pixelOf = (value: string | number) =>
   typeof value === 'number' ? `${value}px` : value;
 
-const MAX_MONTH = 0;
-const MIN_MONTH = 12;
+const MIN_MONTH = 0;
+const MAX_MONTH = 12;
 const LEADING_DIGIT = ['0', '1'];
 export const isOutOfRangeMonth = (month: string) => {
   if (month.length === 1) {
