c43ff5721be5d90f2164a8e6ece7e4416b5cc7a3
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-23 23:16:26 +0900

    chore: .root에 대한 CSS 업데이트

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
 export { default as Dash } from './Dash';
 export { default as Input } from './Input';
-export { default as InputProgress } from './InputProgress';
+export { default as InputProgress } from './Input/InputProgress';
+export { default as Label } from './Input/Label';
 export { default as Header } from './Header';
 export { default as Button } from './Button';
