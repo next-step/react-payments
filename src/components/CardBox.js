242b7fc6c0795be945733bf4ac7557039a971a86
realhee <wlsgml5628@gmail.com>
2023-02-24 20:36:27 +0900

    Refactor: masking 함수 위치를 컴포넌트 외부로 이동

--- src/components/CardBox.js
+++ src/components/CardBox.js
@@ -1,17 +1,22 @@
-export default function CardBox({ cardNumber, cardOwnerName, cardExpiration }) {
-  function maskingCardNumber(cardNumber) {
-    const cardNumberArray = Object.values(cardNumber);    
-    return cardNumberArray.map((value, index) => {
-      return (index < 3 && value.length === 4)? value + "-" : value;
-    }).join("");
-  }
+function maskingCardNumber(cardNumber) {
+  const cardNumberArray = Object.values(cardNumber);
+  return cardNumberArray
+    .map((value, index) => {
+      return index < 3 && value.length === 4 ? value + "-" : value;
+    })
+    .join("");
+}
 
-  function maskingCardExpiration(cardExpiration) {
-    const cardExpirationArray = Object.values(cardExpiration);
-    return cardExpirationArray.map((value, index) => {
-      return (index == 0 && value < 13 && value > 0)? value + " / " : value;
-    }).join("");
-  }
+function maskingCardExpiration(cardExpiration) {
+  const cardExpirationArray = Object.values(cardExpiration);
+  return cardExpirationArray
+    .map((value, index) => {
+      return index == 0 && value < 13 && value > 0 ? value + " / " : value;
+    })
+    .join("");
+}
+
+export default function CardBox({ cardNumber, cardOwnerName, cardExpiration }) {
   return (
     <div className="card-box">
       <div className="empty-card">
@@ -24,10 +29,12 @@ export default function CardBox({ cardNumber, cardOwnerName, cardExpiration }) {
         <div className="card-bottom">
           <div className="card-bottom__number">
             <span className="card-text">{maskingCardNumber(cardNumber)}</span>
-          </div> 
+          </div>
           <div className="card-bottom__info">
             <span className="card-text">{cardOwnerName}</span>
-            <span className="card-text">{maskingCardExpiration(cardExpiration)}</span>
+            <span className="card-text">
+              {maskingCardExpiration(cardExpiration)}
+            </span>
           </div>
         </div>
       </div>
