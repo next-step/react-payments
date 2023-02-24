0510068cb040f3a1b6c61bc9f7d383199826543c
alwayshere <76519867+herekim@users.noreply.github.com>
2023-02-24 17:37:31 +0900

    feat: Add CardExpiredDate storybook

---
+++ src/pages/card-add/card-form/components/CardExpiredDate.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { useCardInfo } from '@/pages/card-add/card-form/hooks'
+
+import CardExpiredDate from './CardExpiredDate'
+
+export default {
+  title: '페이먼츠 미션/Pages/CardAdd/CardForm/Components',
+  component: CardExpiredDate,
+} as ComponentMeta<typeof CardExpiredDate>
+
+const Template: ComponentStory<typeof CardExpiredDate> = (props) => {
+  const { cardInfo, handleExpiredDate } = useCardInfo()
+  return (
+    <div className="root">
+      <div className="app">
+        <CardExpiredDate
+          {...props}
+          expiratedMonth={cardInfo.expiratedMonth}
+          expiratedYear={cardInfo.expiratedYear}
+          handleChange={handleExpiredDate}
+        />
+      </div>
+    </div>
+  )
+}
+
+export const basic = Template.bind({})
