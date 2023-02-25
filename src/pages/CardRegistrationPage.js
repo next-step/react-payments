ee6a2e5b5434e0d5bef28b4fab8f5c9c6bc8a693
realhee <wlsgml5628@gmail.com>
2023-02-26 00:52:33 +0900

    Refactor: cardInfo 상태 통합을 위한 useReducer 적용

--- src/pages/CardRegistrationPage.js
+++ src/pages/CardRegistrationPage.js
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import { Link } from "react-router-dom";
 import CardBox from "../components/CardBox";
 
@@ -9,31 +10,104 @@ import CardExpirationInput from "../components/CardExpirationInput";
 import CardOwnerNameInput from "../components/CardOwnerNameInput";
 import CardSecurityCodeInput from "../components/CardSecurityCodeInput";
 
-import useCardNumber from "../hooks/useCardNumber";
-import useCardExpiration from "../hooks/useCardExpiration";
-import useCardOwnerName from "../hooks/useCardOwnerName";
-import useSecurityCode from "../hooks/useSecurityCode";
-import useCardPassword from "../hooks/useCardPassword";
+import { isNumber } from "../utils/card";
+import { MESSAGE } from "../constants/card";
+import { CARD_OWNER_NAME } from "../constants/card";
 
+// TODO : form형태로 변경
+// TODO : error객체나 {success: boolean, errorMessage?: string} 과 같은 결과 객체를 반환
+function reducer(state, action) {
+  console.log(action, state);
+  switch (action.type) {
+    case "cardNumber": {
+      const { name, value } = action.target;
+      if (!isNumber(value)) {
+        alert(MESSAGE.ALERT_NUMBER);
+        return;
+      }
+      return {
+        ...state,
+        [action.type]: { ...state[action.type], [name]: value },
+      };
+    }
+    case "cardExpiration": {
+      const { name, value } = action.target;
+      if (!isNumber(value)) {
+        alert(MESSAGE.ALERT_NUMBER);
+        return;
+      }
+      if (name == "month") {
+        if (value > 12) {
+          alert(MESSAGE.ALERT_EXP_MONTH);
+          return;
+        }
+      } else {
+        if (value > 31 || value === 0) {
+          alert(MESSAGE.ALERT_EXP_YEAR);
+          return;
+        }
+      }
+      return {
+        ...state,
+        [action.type]: { ...state[action.type], [name]: value },
+      };
+    }
+    case "cardOwnerName": {
+      function isValidOwnerName(value) {
+        if (value.length <= CARD_OWNER_NAME.MAX_LENGTH) {
+          return true;
+        }
+        return false;
+      }
+      const { value } = action.target;
+      if (isValidOwnerName(value)) {
+        return { ...state, [action.type]: value };
+      }
+    }
+    case "cardSecurityCode": {
+      const { value } = action.target;
+      if (!isNumber(value)) {
+        alert(MESSAGE.ALERT_NUMBER);
+        return;
+      }
+      return { ...state, [action.type]: value };
+    }
+    case "cardPassword": {
+      const { name, value } = action.target;
+      if (!isNumber(value)) {
+        alert(MESSAGE.ALERT_NUMBER);
+        return;
+      }
+      return {
+        ...state,
+        [action.type]: { ...state[action.type], [name]: value },
+      };
+    }
+    default:
+      throw new Error();
+  }
+}
 export default function CardRegistrationPage() {
-  // TODO : 모든 input이 제어 컴포넌트로 작성되었는데 form을 제어함에 있어서 언제 렌더가 이뤄져야하는지 고민
-  // TODO : cardInfo는 useReducer & 비제어 컴포넌트로 관리
-  // TODO : form형태로 변경
-  /*
-  const [cardInfo, setCardInfo] = useState({
-   cardNumber: cardNumber,
-   cardExpiration: expiration,
-   cardOwnerName: ownerName,
-   cardSecurityCode: securityCode,
-   cardPassword: password,
-   cardNickName: "클린카드"
+  const [state, dispatch] = useReducer(reducer, {
+    cardNumber: {
+      num0: "",
+      num1: "",
+      num2: "",
+      num3: "",
+    },
+    cardExpiration: { month: "", year: "" },
+    cardOwnerName: "",
+    cardSecurityCode: "",
+    cardPassword: { num0: "", num1: "", num2: "", num3: "" },
   });
-*/
-  const [cardNumber, handleCardNumber] = useCardNumber();
-  const [cardExpiration, handleCardExpiration] = useCardExpiration();
-  const [cardOwnerName, handleCardOwnerName] = useCardOwnerName();
-  const [cardSecurityCode, handleCardSecurityCode] = useSecurityCode();
-  const [cardPassword, handleCardPassword] = useCardPassword();
+
+  const {
+    cardNumber,
+    cardExpiration,
+    cardOwnerName,
+    cardSecurityCode,
+    cardPassword,
+  } = state;
 
   return (
     <>
@@ -47,23 +121,25 @@ export default function CardRegistrationPage() {
       />
       <CardNumberInput
         cardNumber={cardNumber}
-        onChange={handleCardNumber}
+        onChange={(e) => dispatch({ type: "cardNumber", target: e.target })}
       ></CardNumberInput>
       <CardExpirationInput
         cardExpiration={cardExpiration}
-        onChange={handleCardExpiration}
+        onChange={(e) => dispatch({ type: "cardExpiration", target: e.target })}
       ></CardExpirationInput>
       <CardOwnerNameInput
         cardOwnerName={cardOwnerName}
-        onChange={handleCardOwnerName}
+        onChange={(e) => dispatch({ type: "cardOwnerName", target: e.target })}
       ></CardOwnerNameInput>
       <CardSecurityCodeInput
         cardSecurityCode={cardSecurityCode}
-        onChange={handleCardSecurityCode}
+        onChange={(e) =>
+          dispatch({ type: "cardSecurityCode", target: e.target })
+        }
       ></CardSecurityCodeInput>
       <CardPasswordInput
         cardPassword={cardPassword}
-        onChange={handleCardPassword}
+        onChange={(e) => dispatch({ type: "cardPassword", target: e.target })}
       />
 
       <Link className="button-text" to={ROUTE_PATH.CARD_REGISTRATION_COMPLETED}>
