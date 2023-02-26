b838fa5e17144aab2e756a91935e8a43ceb6b671
jlee0505 <hj960322@gmail.com>
2023-02-26 22:54:56 +0900

    refactor: cardPassword component 상태관리 수정

--- react-payments-app/src/components/domain/CardInput/CardPassword/CardPasswordInput.jsx
+++ react-payments-app/src/components/domain/CardInput/CardPassword/CardPasswordInput.jsx
@@ -3,36 +3,28 @@ import { useState, useEffect, useRef } from 'react';
 import Input from '../../../common/Input/Input';
 import InputBox from '../../../common/Input/InputBox';
 import { MAX_INPUT_LENGTH } from '../../../../constants/numbers';
+import { useCard } from '../../../../store/CardContext';
+import { CHANGE_CARD } from '../../../../constants/action';
 
-const CardPasswordInput = ({ onChange }) => {
+const CardPasswordInput = () => {
+  const { changeCardInfo } = useCard();
+
   const [password, setPassword] = useState(['', '']);
-  const [error, setError] = useState('');
-  const nextElement = useRef(null);
-  const [event, setEvent] = useState(null);
 
   const handleCardPasswordChange = (index) => (e) => {
     const updatedPassword = [...password];
     const { value } = e.target;
 
     if (Number.isNaN(+value)) {
-      setError('숫자만 입력해주세요.');
+      changeCardInfo(CHANGE_CARD.ERROR, '숫자만 입력해주세요.');
       return;
     }
-    setError(null);
+    changeCardInfo(CHANGE_CARD.ERROR, null);
 
-    setEvent(e);
     updatedPassword[index] = value;
     setPassword(updatedPassword);
-
-    // // TODO: 스토리북 액션 테스트 실패
-    // if (index === 0) {
-    //   nextElement.current.focus();
-    // }
+    changeCardInfo(CHANGE_CARD.PW, updatedPassword);
   };
-
-  useEffect(() => {
-    onChange(password, error);
-  }, [password, error]);
 
   return (
     <InputBox
@@ -43,16 +35,13 @@ const CardPasswordInput = ({ onChange }) => {
         className='input-basic w-15 mr-10'
         type='password'
         maxLength={MAX_INPUT_LENGTH.PW}
-        value={password[0]}
         onChange={handleCardPasswordChange(0)}
       />
       <Input
         className='input-basic w-15 mr-10'
         type='password'
         maxLength={MAX_INPUT_LENGTH.PW}
-        value={password[1]}
         onChange={handleCardPasswordChange(1)}
-        ref={nextElement}
       />
       <Input
         className='input-basic w-15 mr-10 input-of-pw'
