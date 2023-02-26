767019f83f53607747f9b3339043b8fc1adfbeb4
Woo KyungJun <natcho9010@gmail.com>
2023-02-26 19:55:49 +0900

    style: trim CardList

--- src/pages/CardList/CardList.tsx
+++ src/pages/CardList/CardList.tsx
@@ -5,39 +5,11 @@ import { routes } from '@/routes';
 
 function CardList() {
   return (
-    <>
-      <h2>5️⃣ 카드 목록</h2>
-      <div className="root">
-        <div className="app flex-column-center">
-          <div className="flex-center">
-            <h2 className="page-title mb-10">보유 카드</h2>
-          </div>
-          <div className="card-box">
-            <div className="small-card">
-              <div className="card-top">
-                <span className="card-text">클린카드</span>
-              </div>
-              <div className="card-middle">
-                <div className="small-card__chip" />
-              </div>
-              <div className="card-bottom">
-                <div className="card-bottom__number">
-                  <span className="card-text">1111 - 2222 - oooo - oooo</span>
-                </div>
-                <div className="card-bottom__info">
-                  <span className="card-text">YUJO</span>
-                  <span className="card-text">12 / 23</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <span className="card-nickname">법인카드</span>
-          <Link to={routes.cardCreator} className="card-box">
-            <div className="empty-card">+</div>
-          </Link>
-        </div>
-      </div>
-    </>
+    <div className="app flex-column-center">
+      <Link to={routes.cardCreator} className="card-box">
+        <div className="empty-card">+</div>
+      </Link>
+    </div>
   );
 }
 
