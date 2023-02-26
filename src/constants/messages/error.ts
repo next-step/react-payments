0025829f8dfccf22ccabe04b0eaaaedba5bb749f
ag502 <ag502@naver.com>
2023-02-26 17:11:45 +0900

    add: CardsConText 추가

--- src/constants/messages/error.ts
+++ src/constants/messages/error.ts
@@ -7,3 +7,8 @@ export const CARD_VALIDATION_ERROR_MESSAGES = {
   MONTH_RANGE: `월은 ${MIN_MONTH} ~ ${MAX_MONTH} 사이의 값만 입력할 수 있습니다.`,
   INVALID_OWNER_NAME_LENGTH: `이름은 최대 ${OWNER_NAME_MAX_LENGTH}까지만 입력 가능 합니다.`,
 };
+
+export const CONTEXT_ERROR_MESSAGES = {
+  UNDEFINED_CONTEXT: "컨텍스트가 정의되지 않았습니다.",
+  UNDEFINED_ACTION: "정의되지 않은 액션 입니다.",
+};
