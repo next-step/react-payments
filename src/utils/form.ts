a29170c8c04844581dfdd48f187454ff5df2610a
seyoungjoy <diddpwl80@gmail.com>
2023-02-26 20:02:51 +0900

    refactor :  next focus 함수 수정

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -1,10 +1,8 @@
-const SLASH = '/';
+import { SLASH } from '../constant';
 
 export const validateDigit = (digit: number | string, targetName: string) => {
   const value = String(digit).replace(/\D+/g, '');
-  const length = value.length;
-  const nextId = Number(targetName.split('digit')[1]) + 1;
-  if (length === 4) nextFocus('digit' + nextId);
+  nextFocus(Number(value), targetName, 'digit', 4);
   return value;
 };
 
@@ -37,14 +35,23 @@ const insertSlash = (value: string, length: number) => {
 
 export const validatePassword = (digit: string, targetName: string) => {
   const value = digit.replace(/\D+/g, '');
-  const length = value.length;
-  const nextId = Number(targetName.split('password')[1]) + 1;
-  if (length === 1) nextFocus('password' + nextId);
+
+  nextFocus(Number(value), targetName, 'password', 1);
+
   return digit.replace(/\D+/g, '');
 };
 
-export const nextFocus = (id: string) => {
-  const nextRef = document.getElementById(id);
-  if (!nextRef) return;
-  nextRef.focus();
+const nextFocus = (
+  value: number,
+  targetName: string,
+  targetId: string,
+  maxLength: number
+) => {
+  const length = String(value).length;
+  const nextId = Number(targetName.split(targetId)[1]) + 1;
+  const nextRef = document.getElementById(targetId + nextId);
+  if (length === maxLength) {
+    if (!nextRef) return;
+    nextRef.focus();
+  }
 };
