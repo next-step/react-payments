044e9184fde5d4a77a862315dc977c38db3c96b3
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-27 22:09:52 +0900

    mod: ConfirmCard 컴포넌트 업데이트
    contants/card.ts
    - 카드 별칭 최대 자리 수 상수값 추가
    
    ConfirmCard/index.tsx
    - 카드 별칭를 컨트롤하는 제어 컴포넌트 추가

--- src/pages/ConfirmCard/index.tsx
+++ src/pages/ConfirmCard/index.tsx
@@ -1,37 +1,47 @@
+import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { Card } from 'components/domain';
+import { Button } from 'components/common';
+
+import { PATHS } from 'constants/router';
+import { MAX_LENGTH } from 'constants/card';
+import type { CardBaseProps } from 'types/card';
+
 function ConrimCard() {
+  const [alias, setAlias] = useState('');
+  const navigate = useNavigate();
+  const location = useLocation();
+  const card = location.state as CardBaseProps;
+
+  const handleChangeAlias: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setAlias(e.target.value);
+  };
+
+  const handleClickNextButton = () => {
+    navigate(PATHS.HOME, { replace: true });
+  };
+
   return (
     <div className="app flex-column-center">
       <div className="flex-center">
         <h2 className="page-title mb-10">카드등록이 완료되었습니다.</h2>
       </div>
-      <div className="card-box">
-        <div className="big-card">
-          <div className="card-top">
-            <span className="card-text__big">클린카드</span>
-          </div>
-          <div className="card-middle">
-            <div className="big-card__chip"></div>
-          </div>
-          <div className="card-bottom">
-            <div className="card-bottom__number">
-              <span className="card-text__big">1111 - 2222 - oooo - oooo</span>
-            </div>
-            <div className="card-bottom__info">
-              <span className="card-text__big">YUJO</span>
-              <span className="card-text__big">12 / 23</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Card {...card} />
       <div className="input-container flex-center w-100">
         <input
           className="input-underline w-75"
           type="text"
-          placeholder="카드의 별칭을 입력해주세요."
+          placeholder="카드 별칭 (선택)"
+          value={alias}
+          onChange={handleChangeAlias}
+          maxLength={MAX_LENGTH.ALIAS}
         />
       </div>
       <div className="button-box mt-50">
-        <span className="button-text">다음</span>
+        <Button fontSize={18} onClick={handleClickNextButton}>
+          다음
+        </Button>
       </div>
     </div>
   );
