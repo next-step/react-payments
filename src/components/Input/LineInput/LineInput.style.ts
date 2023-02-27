3381dab6b2fbc0d0fdf959f929a1eca4fd348ada
Dongmin Ahn <eamon3481@naver.com>
2023-02-27 13:18:50 +0900

    feat : add LineInput

---
+++ src/components/Input/LineInput/LineInput.style.ts
@@ -0,0 +1,21 @@
+import { Colors } from '@/styles/colors';
+import styled from '@emotion/styled';
+
+export const StyledLineInput = styled.input<{
+  textAlign: 'left' | 'center' | 'right';
+  fontColor: Colors;
+  lineColor: Colors;
+  width: string;
+}>`
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  width: ${({ width }) => width};
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 21px;
+
+  border-bottom: 1px solid ${({ lineColor, theme }) => theme.colors[lineColor]};
+  text-align: ${({ textAlign }) => textAlign};
+  color: ${({ fontColor, theme }) => theme.colors[fontColor]};
+`;
