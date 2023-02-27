1f816535925c37df3cffcc6290e7700c3c4dc6f4
Woo KyungJun <natcho9010@gmail.com>
2023-02-27 17:12:16 +0900

    style: add EventHandler Props in useInputEventHandler hook

--- src/pages/CardCreator/InputComponents/hooks/useInputEventHandler.ts
+++ src/pages/CardCreator/InputComponents/hooks/useInputEventHandler.ts
@@ -4,36 +4,31 @@ interface UseInputOnChangeProps {
   setState: (newState: any) => void;
 }
 
+export interface OnChangeHandlerProps {
+  props: UseInputOnChangeProps;
+  checkWhetherSetState: (e: ChangeEvent<HTMLInputElement>) => boolean;
+  getNewValue: (e: ChangeEvent<HTMLInputElement>) => string;
+}
+
+export interface OnBlurHandlerProps {
+  props: UseInputOnChangeProps;
+  checkWhetherSetState: (e: FocusEvent<HTMLInputElement, Element>) => boolean;
+  getNewValue: (e: FocusEvent<HTMLInputElement, Element>) => string;
+}
+
 function useInputEventHandler() {
   return {
     createInputChangeHandler:
-      ({
-        props,
-        checkWhetherSetState,
-        getNewValue,
-      }: {
-        props: UseInputOnChangeProps;
-        checkWhetherSetState: (e: ChangeEvent<HTMLInputElement>) => boolean;
-        getNewValue: (e: ChangeEvent<HTMLInputElement>) => string;
-      }) =>
+      ({ props, checkWhetherSetState, getNewValue }: OnChangeHandlerProps) =>
       (e: ChangeEvent<HTMLInputElement>) => {
         const { setState } = props;
         if (checkWhetherSetState(e)) {
           const newValue = getNewValue(e);
-          console.log(newValue);
           setState(newValue);
         }
       },
     createInputBlurHandler:
-      ({
-        props,
-        checkWhetherSetState,
-        getNewValue,
-      }: {
-        props: UseInputOnChangeProps;
-        checkWhetherSetState: (e: FocusEvent<HTMLInputElement, Element>) => boolean;
-        getNewValue: (e: FocusEvent<HTMLInputElement, Element>) => string;
-      }) =>
+      ({ props, checkWhetherSetState, getNewValue }: OnBlurHandlerProps) =>
       (e: FocusEvent<HTMLInputElement, Element>) => {
         const { setState } = props;
         if (checkWhetherSetState(e)) {
