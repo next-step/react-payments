3b599ec72f091d953f77a12c96374e4fc0ea8d13
tooooo1 <wer4272@nate.com>
2023-02-28 18:46:28 +0900

    feat: masking util func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,10 @@
 export const extractNumbers = (value: string) => {
   return value.replace(/[^0-9]/g, '');
 };
+
+export const masking = (value: string) => {
+  const mask = '*';
+  const NumberRegex = /[0-9]/g;
+
+  return value.replace(NumberRegex, mask);
+};
