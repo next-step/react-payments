3142083d23f92511455761a4ac9ff7b4403d42c5
areumsheep <areumsheep@gmail.com>
2023-03-01 03:00:02 +0900

    Add: 카드 별명은 최대 10자까지 입력 가능하도록 설정

--- src/pages/InputCardNamePage.tsx
+++ src/pages/InputCardNamePage.tsx
@@ -6,7 +6,10 @@ import { Box, TextField, Button } from 'components/@common';
 import type { CreditCardProps } from 'components/CreditCard/CreditCard.types';
 import useRouter from 'routes/useRouter';
 
-import { getLocalStorageItem } from 'utils/localStorage';
+import {
+  getLocalStorageItem,
+  removeLocalStorageItem,
+} from 'utils/localStorage';
 import { CardListContext } from 'contexts/CardListProvider';
 
 const InputCardNamePage = () => {
@@ -19,6 +22,7 @@ const InputCardNamePage = () => {
     if (!nameRef.current) return;
     const { value } = nameRef.current;
     addCardInfo({ ...item, nickname: value });
+    removeLocalStorageItem('CardValues');
     push('/card-list');
   };
 
@@ -36,7 +40,7 @@ const InputCardNamePage = () => {
         <TextField
           ref={nameRef}
           placeholder="카드를 저장할 별명을 입력해주세요."
-          maxLength={30}
+          maxLength={10}
           className="w-100"
         />
         <Box display="flex" justifyContent="flex-end" className="mt-20">
