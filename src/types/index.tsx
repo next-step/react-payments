221bfbb09e599a6ec030efe0d35feecf42f1f8b6
TheGiantKiller <ktsdsd94@gmail.com>
2023-02-28 13:40:11 +0900

    fix : Confilct Error3

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,4 +1,4 @@
-export type ColorType = "red" | "blue" | "green" | "pink" | "purple" | "cyon" | "yellow" | "orange" | "";
+export type ColorType = "red" | "blue" | "green" | "pink" | "purple" | "cyon" | "yellow" | "orange" | "" | "primary";
 export type CompanyType =
   | "하나카드"
   | "국민카드"
@@ -9,9 +9,47 @@ export type CompanyType =
   | "카카오카드"
   | "오렌지카드"
   | "";
-export type FontSizeType = "xs" | "s" | "m" | "lg";
+export type FontSizeType = "xs" | "s" | "m" | "lg" | "2x";
 export type FontWeightType = "normal" | "bold";
-export type CardStateType = {
+
+export interface CardFormType {
+  cardNumbers: {
+    text: string;
+    isValid: boolean;
+  };
+  expireDate: {
+    month: {
+      text: string;
+      isValid: boolean;
+    };
+    year: {
+      text: string;
+      isValid: boolean;
+    };
+  };
+  password: {
+    one: string;
+    two: string;
+    isValid: boolean;
+  };
+  cvc: {
+    text: string;
+    isValid: boolean;
+  };
+  ownerName: {
+    text: string;
+    isValid: boolean;
+  };
+  color: {
+    text: ColorType;
+    isValid: boolean;
+  };
+  company: {
+    text: CompanyType;
+    isValid: boolean;
+  };
+}
+export type CardType = {
   cardNumbers: string;
   expireDate: {
     month: string;
@@ -25,4 +63,6 @@ export type CardStateType = {
   ownerName: string;
   color: ColorType;
   company: CompanyType;
+  alias: string;
+  id: string;
 };
