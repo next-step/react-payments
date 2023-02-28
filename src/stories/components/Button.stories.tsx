be12c4a15cb901e2598a67bf419b68c712a8e647
TheGiantKiller <ktsdsd94@gmail.com>
2023-02-28 13:35:15 +0900

    fix :conflict Error

--- src/stories/components/Button.stories.tsx
+++ src/stories/components/Button.stories.tsx
@@ -1,25 +1,17 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import Button from "components/Button/Button";
-import ButtonProps from "components/Button/Button";
-import { withRouter } from "storybook-addon-react-router-v6";
+import Button from "components/Button";
+import ButtonProps from "components/Button";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Button",
   component: Button,
-  decorators: [withRouter],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof ButtonProps>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof ButtonProps> = (args) => <Button {...args} />;
-
-export const XSmall = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-XSmall.args = {
-  fontSize: "xs",
-  label: "Next",
-};
 
 export const Small = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
--- src/stories/components/Card.stories.tsx
+++ src/stories/components/Card.stories.tsx
@@ -1,46 +1,32 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import Card from "components/Card/Card";
-import CardProps from "components/Card/Card";
-import { withRouter } from "storybook-addon-react-router-v6";
+import Card from "components/Card";
+import CardProps from "components/Card";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Card",
   component: Card,
-  decorators: [withRouter],
 } as ComponentMeta<typeof CardProps>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof CardProps> = (args) => <Card {...args} />;
 
-export const SmallPrimary = Template.bind({});
+export const Small = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-SmallPrimary.args = {
+Small.args = {
   size: "small",
-  type: "primary",
-  id: "1",
-  company: "토스카드",
-  color: "blue",
-  expireMonth: "01",
-  expireYear: "23",
-  ownerName: "문준영",
+  company: "국민카드",
+  ownerName: "Name",
+  expirationMonth: "MM",
+  expirationYear: "YY",
 };
 
-export const BigPrimary = Template.bind({});
-BigPrimary.args = {
+export const Big = Template.bind({});
+Big.args = {
   size: "big",
-  type: "primary",
-  id: "1",
-  company: "토스카드",
-  color: "blue",
-  expireMonth: "01",
-  expireYear: "23",
-  ownerName: "문준영",
-};
-
-export const AddCard = Template.bind({});
-AddCard.args = {
-  size: "big",
-  type: "add",
+  company: "국민카드",
+  ownerName: "Name",
+  expirationMonth: "MM",
+  expirationYear: "YY",
 };
