50720814c009ae772ce04b5fbaf7517417b8e055
dahye1013 <alrean@naver.com>
2023-03-02 02:05:34 +0900

    chore: column layout 추가

---
+++ src/components/UI/ColumnLayout/index.tsx
@@ -0,0 +1,10 @@
+import { styled } from '@/lib/stitches.config';
+
+const ColumnLayout = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+export default ColumnLayout;
--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button';
+export { default as ColumnLayout } from './ColumnLayout';
 export { default as CreditCard } from './CreditCard';
 export { default as InputContainer } from './InputContainer';
 export { default as Text } from './Text';
