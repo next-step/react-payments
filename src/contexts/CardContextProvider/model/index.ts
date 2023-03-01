235d6a7c24920ca3eb8efff8741b1c3251ca0567
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-03-01 16:45:07 +0900

    feat: 카드 목록을 관리하는 Context 추가
    CardContextProvider/index.tsx
    - 보유 카드 목록을 value로 갖고 있는 Context 생성
    - 카드 추가와 삭제에 대한 Action 및 reducer 생성
    - dispatch만 사용하는 컴포넌트가 있을 수도 있고 state만 사용하는
      컴포넌트가 있을 수 있기 때문에 dispatch와 state에 대한 Context
      를 각각 생성
    
    CardContextProvider/model/index.ts
    - CardContextProvider 내에서 존재하는 reducer에서 사용될 로직을
      분리하여 model이라는 키워드 아래에 정리

---
+++ src/contexts/CardContextProvider/model/index.ts
@@ -0,0 +1,23 @@
+import type { Card, CardNumber } from 'types/card';
+
+const concatCardNumber = (cardNumber: CardNumber) => Object.values(cardNumber).join('');
+const isMatchedCard = (cardA: Card, cardB: Card) =>
+  concatCardNumber(cardA.cardNumber) === concatCardNumber(cardB.cardNumber);
+
+export const filterCards = (cards: Card[], target: Card) => {
+  const filteredCards = cards.filter((card) => !isMatchedCard(card, target));
+
+  return filteredCards;
+};
+
+export const addOrUpdateCard = (cards: Card[], newCard: Card) => {
+  const existedCard = cards.find((card) => isMatchedCard(card, newCard));
+
+  if (!existedCard) {
+    return [newCard, ...cards];
+  }
+
+  const updatedCards = cards.map((card) => (isMatchedCard(card, newCard) ? newCard : card));
+
+  return updatedCards;
+};
