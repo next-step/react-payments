4da79f854e0076fd465a51afa3ac52f8f98453e9
신형기 <tkaqkeldk@hanatour.com>
2023-03-01 13:30:18 +0900

    feat: modal component 추가 및 뼈대 작업

--- src/components/modal/AddCardModal.tsx
+++ src/components/modal/AddCardModal.tsx
@@ -1,42 +1,21 @@
+import { CARD } from "constant";
+
 const AddCardModal = () => {
+
+  const handleCardCompany = (color: string) => {
+    console.log(color)
+  };
+
   return (
     <div className="modal-dimmed">
       <div className="modal">
-        <div className="flex-center">
-          <div className="modal-item-container">
-            <div className="modal-item-dot"></div>
-            <span className="modal-item-name">클린 카드</span>
-          </div>
-          <div className="modal-item-container">
-            <div className="modal-item-dot"></div>
-            <span className="modal-item-name">클린 카드</span>
-          </div>
-          <div className="modal-item-container">
-            <div className="modal-item-dot"></div>
-            <span className="modal-item-name">클린 카드</span>
-          </div>
-          <div className="modal-item-container">
-            <div className="modal-item-dot"></div>
-            <span className="modal-item-name">클린 카드</span>
-          </div>
-        </div>
-        <div className="flex-center">
-          <div className="modal-item-container">
-            <div className="modal-item-dot"></div>
-            <span className="modal-item-name">클린 카드</span>
-          </div>
-          <div className="modal-item-container">
-            <div className="modal-item-dot"></div>
-            <span className="modal-item-name">클린 카드</span>
-          </div>
-          <div className="modal-item-container">
-            <div className="modal-item-dot"></div>
-            <span className="modal-item-name">클린 카드</span>
-          </div>
-          <div className="modal-item-container">
-            <div className="modal-item-dot"></div>
-            <span className="modal-item-name">클린 카드</span>
-          </div>
+        <div className="grid-template">
+          {CARD.COMPANY.map(({ name, color }: any) => (
+            <div className="modal-item-container" onClick={() => handleCardCompany(color)}>
+              <div className="modal-item-dot" style={{backgroundColor:color}}></div>
+              <span className="modal-item-name">{name}</span>
+            </div>
+          ))}
         </div>
       </div>
     </div>
