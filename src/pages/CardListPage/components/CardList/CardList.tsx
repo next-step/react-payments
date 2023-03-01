d97e96a9aa0a3fc400b3145d976b855c30d486af
Dongmin Ahn <eamon3481@naver.com>
2023-03-01 15:32:01 +0900

    feat : add useCard hook

--- src/pages/CardListPage/components/CardList/CardList.tsx
+++ src/pages/CardListPage/components/CardList/CardList.tsx
@@ -4,9 +4,12 @@ import { useNavigate } from 'react-router-dom';
 import { EmptyCardButton } from '@/components/buttons';
 import { CardListContainer } from './CardList.style';
 import { ROUTE } from '@/constants';
+import { useCardList } from '@/store';
+import { Card } from '@/components';
 
 const CardList = () => {
   const navigate = useNavigate();
+  const cardList = useCardList();
 
   const handleClick = () => {
     navigate(ROUTE.CARD_CREATE);
@@ -15,6 +18,9 @@ const CardList = () => {
   return (
     <CardListContainer>
       <EmptyCardButton onClick={handleClick} />
+      {cardList.map((card) => (
+        <Card key={card.id} card={card} size="small" />
+      ))}
     </CardListContainer>
   );
 };
