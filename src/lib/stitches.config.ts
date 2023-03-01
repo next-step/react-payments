16e131339c1268164260a3aba7cad02f4390c098
dahye1013 <alrean@naver.com>
2023-03-02 00:26:37 +0900

    chore: stiches size token 추가

--- src/lib/stitches.config.ts
+++ src/lib/stitches.config.ts
@@ -14,5 +14,33 @@ export const { styled, css, theme, config } = createStitches({
       shadow0: '#0000001a',
       danger: '#EF6F6F',
     },
+    sizes: {
+      1: '5px',
+      2: '10px',
+      3: '15px',
+      4: '20px',
+      5: '25px',
+      6: '35px',
+      7: '45px',
+      8: '65px',
+      9: '80px',
+      10: '130px',
+    },
+    fontSizes: {
+      1: '12px',
+      2: '13px',
+      3: '15px',
+      4: '17px',
+      5: '19px',
+      6: '21px',
+      7: '27px',
+      8: '35px',
+      9: '59px',
+    },
+  },
+  utils: {
+    mr: (value: Stitches.PropertyValue<'marginRight'>) => ({
+      marginRight: value,
+    }),
   },
 });
