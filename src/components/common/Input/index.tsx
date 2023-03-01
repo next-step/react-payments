9a6aff549f1453e6b4c2563438bbc252c39ccf94
yujeong <yujeong.choung@gmail.com>
2023-03-01 13:37:25 +0900

    feat: 공통 컴포넌트(card, input) 수정

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,26 +1,36 @@
-import { forwardRef } from "react";
-import "./input.css";
+import { forwardRef } from 'react';
+import './input.css';
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  (
-    { className, value, type = "text", id, placeholder, onChange, maxLength },
-    ref
-  ) => {
-    return (
-      <input
-        value={value}
-        ref={ref}
-        className={`input-basic ${className}`}
-        type={type}
-        id={id}
-        placeholder={placeholder}
-        onChange={onChange}
-        maxLength={maxLength}
-      />
-    );
-  }
+	(
+		{
+			className,
+			value,
+			type = 'text',
+			id,
+			placeholder,
+			onChange,
+			maxLength,
+			...props
+		},
+		ref,
+	) => {
+		return (
+			<input
+				value={value}
+				ref={ref}
+				className={`input-basic ${className}`}
+				type={type}
+				id={id}
+				placeholder={placeholder}
+				onChange={onChange}
+				maxLength={maxLength}
+				{...props}
+			/>
+		);
+	},
 );
 
 export default Input;
