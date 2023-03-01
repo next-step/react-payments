4efb8b236fce4c36f2cd938b8c691792670eb88f
tooooo1 <wer4272@nate.com>
2023-03-02 03:11:31 +0900

    feat: card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,41 @@
-const Card = () => {
+import { masking } from '../../utils';
+
+interface CardProps {
+  data: {
+    alias?: string;
+    num1?: string;
+    num2?: string;
+    num3?: string;
+    num4?: string;
+    expiry?: string;
+    holder: string;
+  };
+}
+
+const Card = ({ data }: CardProps) => {
+  const { alias, holder, expiry, num1, num2, num3, num4 } = data;
+
+  const formatCardNumber = () =>
+    [num1, num2, masking(num3), masking(num4)]
+      .filter((lang) => lang !== '')
+      .join('-');
+
   return (
     <div className="card-box">
       <div className="small-card">
         <div className="card-top">
-          <span className="card-text">클린카드</span>
+          <span className="card-text">{alias}</span>
         </div>
         <div className="card-middle">
-          <div className="small-card__chip"></div>
+          <div className="small-card__chip" />
         </div>
         <div className="card-bottom">
           <div className="card-bottom__number">
-            <span className="card-text">1111 - 2222 - oooo - oooo</span>
+            <span className="card-text">{formatCardNumber()}</span>
           </div>
           <div className="card-bottom__info">
-            <span className="card-text">YUJO</span>
-            <span className="card-text">12 / 23</span>
+            <span className="card-text">{holder}</span>
+            <span className="card-text">{expiry}</span>
           </div>
         </div>
       </div>
