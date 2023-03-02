9f41b56831cf664e20132450504fff6afb9e2ad3
yujeong <yujeong.choung@gmail.com>
2023-03-02 11:44:57 +0900

    feat: cardList 페이지에 controlled & uncontrolled input변경

--- src/pages/payments/CardList/index.tsx
+++ src/pages/payments/CardList/index.tsx
@@ -2,46 +2,52 @@ import { Fragment } from "react";
 import { CardListType } from "types/payments";
 import Card from "components/common/Card";
 import {
-  usePaymentsDispatch,
-  usePaymentsState,
+	usePaymentsDispatch,
+	usePaymentsState,
 } from "pages/payments/modules/payments/PaymentsContext";
-import { CLEAR_CARD_INFO } from "pages/payments/modules/payments/PaymentsActionType";
 import { useNavigate } from "react-router";
+import {
+	SelectedCardInfo,
+	SET_SELECTED_CARD,
+} from "pages/payments/modules/payments/PaymentsActionType";
 import { STEP } from "constants/Payments";
 
 const CardList = () => {
-  const { cardList } = usePaymentsState();
-  const dispatch = usePaymentsDispatch();
-  const navigation = useNavigate();
+	const { cardList } = usePaymentsState();
+	const dispatch = usePaymentsDispatch();
+	const navigation = useNavigate();
 
-  const handleCardAddClick = () => {
-    dispatch({ type: CLEAR_CARD_INFO });
-    navigation(STEP.REGISTER_CARD);
-  };
+	const handleCardClick = (selectedCard: SelectedCardInfo) => {
+		dispatch({ type: SET_SELECTED_CARD, selectedCard });
+	};
 
-  return (
-    <>
-      <h2>카드 목록</h2>
-      <div className="root">
-        <div className="app flex-column-center">
-          <div className="flex-center">
-            <h2 className="page-title mb-10">보유 카드</h2>
-          </div>
+	const handleCardAddClick = () => navigation(STEP.REGISTER_CARD);
 
-          {cardList.map((card: CardListType, index) => {
-            return (
-              <Fragment key={index}>
-                <Card input={card} backgroundColor={card?.backgroundColor} />
-                <span className="card-nickname">{card?.nickname}</span>
-              </Fragment>
-            );
-          })}
-
-          <Card onClick={handleCardAddClick} />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<h2>카드 목록</h2>
+			<div className="root">
+				<div className="app flex-column-center">
+					<div className="flex-center">
+						<h2 className="page-title mb-10">보유 카드</h2>
+					</div>
+					{cardList.map((card: CardListType) => {
+						return (
+							<Fragment key={card.id}>
+								<Card
+									input={card}
+									backgroundColor={card?.backgroundColor}
+									onClick={() => handleCardClick(card)}
+								/>
+								<span className="card-nickname">{card?.nickname}</span>
+							</Fragment>
+						);
+					})}
+					<Card onClick={handleCardAddClick} />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default CardList;
