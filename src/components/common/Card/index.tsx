5d0e468e2bc5a88801c5ef094e6653413533e1ec
yujeong <yujeong.choung@gmail.com>
2023-03-02 11:37:05 +0900

    feat: 공통 컴포넌트(card, input) 수정

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -3,59 +3,57 @@ import "./card.css";
 import { CardInput } from "./card.type";
 
 interface CardProps {
-  input?: CardInput;
-  isBigCard?: boolean;
-  backgroundColor?: string;
-  onClick?: () => void;
+	input?: CardInput;
+	isBigCard?: boolean;
+	backgroundColor?: string;
+	onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
 const Card = ({
-  input,
-  isBigCard = false,
-  backgroundColor,
-  onClick,
+	input,
+	isBigCard = false,
+	backgroundColor,
+	onClick,
 }: CardProps) => {
-  const mode =
-    input && Object.values(input).length > 0 ? "small-card" : "empty-card";
-  const newCard = typeof onClick === "function" && "+";
+	const mode =
+		input && Object.values(input).length > 0 ? "small-card" : "empty-card";
+	const newCard = typeof onClick === "function" && !input && "+";
 
-  return (
-    <div className="card-box">
-      <div
-        className={`${isBigCard ? "big-card" : mode} ${
-          newCard && "cursor-pointer"
-        }`}
-        style={{ backgroundColor }}
-        onClick={onClick}
-      >
-        {newCard || (
-          <>
-            <div className="card-top">
-              <span className="card-text">{input?.title}</span>
-            </div>
-            <div className="card-middle">
-              <div className="small-card__chip"></div>
-            </div>
-            <div className="card-bottom">
-              <div className="card-bottom__number">
-                <span className="card-text">
-                  {input?.number
-                    ? displayNumber({ input: input.number, startPoint: 2 })
-                    : input?.number}
-                </span>
-              </div>
-              <div className="card-bottom__info">
-                <span className="card-text">{input?.name || "NAME"}</span>
-                <span className="card-text">
-                  {`${input?.expiry || "MM / YY"}`}
-                </span>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="card-box">
+			<div
+				className={`${isBigCard ? "big-card" : mode} cursor-pointer`}
+				style={{ backgroundColor }}
+				onClick={onClick}
+			>
+				{newCard || (
+					<>
+						<div className="card-top">
+							<span className="card-text">{input?.title}</span>
+						</div>
+						<div className="card-middle">
+							<div className="small-card__chip"></div>
+						</div>
+						<div className="card-bottom">
+							<div className="card-bottom__number">
+								<span className="card-text">
+									{input?.number
+										? displayNumber({ input: input.number, startPoint: 2 })
+										: input?.number}
+								</span>
+							</div>
+							<div className="card-bottom__info">
+								<span className="card-text">{input?.name || "NAME"}</span>
+								<span className="card-text">
+									{`${input?.expiry || "MM / YY"}`}
+								</span>
+							</div>
+						</div>
+					</>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Card;
