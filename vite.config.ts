700d57d13075e9f8ef655137ec903362bf4cd8e3
minhyeok0328 <alsgur7408@gmail.com>
2023-03-02 13:22:00 +0900

    refactor: vite config js 수정

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src/'),
     }
   },
-  base: '/react-payments',
-  build: {
-    outDir: './docs'
-  }
+  base: '/react-payments'
 });
