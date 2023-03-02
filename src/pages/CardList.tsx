75dccf88ffb0d2ae486745f601b6e4cc077d0f34
tooooo1 <wer4272@nate.com>
2023-03-02 13:41:36 +0900

    feat: 카드 목록을 조회할 수 있다.

--- src/pages/CardList.tsx
+++ src/pages/CardList.tsx
@@ -1,32 +1,23 @@
+import { useContext } from 'react';
+
+import Card from '../components/Card';
+import { PaymentsContext } from '../context/PaymentsContext';
+
 const CardAdd = () => {
+  const { cardList } = useContext(PaymentsContext);
+
   return (
     <div className="app flex-column-center">
       <div className="flex-center">
         <h2 className="page-title mb-10">보유 카드</h2>
       </div>
-      <div className="card-box">
-        <div className="small-card">
-          <div className="card-top">
-            <span className="card-text">클린카드</span>
-          </div>
-          <div className="card-middle">
-            <div className="small-card__chip"></div>
-          </div>
-          <div className="card-bottom">
-            <div className="card-bottom__number">
-              <span className="card-text">1111 - 2222 - oooo - oooo</span>
-            </div>
-            <div className="card-bottom__info">
-              <span className="card-text">YUJO</span>
-              <span className="card-text">12 / 23</span>
-            </div>
-          </div>
-        </div>
-      </div>
       <span className="card-nickname">법인카드</span>
       <div className="card-box">
         <div className="empty-card">+</div>
       </div>
+      {cardList.map((card) => (
+        <Card key={card.id.getDate()} data={card} />
+      ))}
     </div>
   );
 };
