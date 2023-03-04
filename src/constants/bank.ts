13d8600ead4583b02660a866946a491b8aae93c1
zestlee1106 <zestlee1106@gmail.com>
2023-03-04 21:52:00 +0900

    [feat] bank 상수에 number 필드 추가

--- src/constants/bank.ts
+++ src/constants/bank.ts
@@ -5,41 +5,49 @@ export const BANKS = [
     NAME: "강준카드",
     COLOR: "#e056fd",
     ID: "gang-jun",
+    NUMBER: "9999",
   },
   {
     NAME: "보검카드",
     COLOR: "#badc58",
     ID: "bo-geom",
+    NUMBER: "7777",
   },
   {
     NAME: "해인카드",
     COLOR: "#ffbe76",
     ID: "hae-in",
+    NUMBER: "8888",
   },
   {
     NAME: "도현카드",
     COLOR: "#eb4d4b",
     ID: "do-hyun",
+    NUMBER: "1111",
   },
   {
     NAME: "도환카드",
     COLOR: "#30336b",
     ID: "do-hwan",
+    NUMBER: "2222",
   },
   {
     NAME: "진구카드",
     COLOR: "#22a6b3",
     ID: "jin-gu",
+    NUMBER: "3333",
   },
   {
     NAME: "수혁카드",
     COLOR: "#535c68",
     ID: "soo-hyuk",
+    NUMBER: "5555",
   },
   {
     NAME: "중기카드",
     COLOR: "#f9ca24",
     ID: "jung-ki",
+    NUMBER: "6666",
   },
 ];
 
