8d34bf41fd136a3a2f16dc2b309ac5d94f502ad5
Kim Hyewon <79592604+hyew-kim@users.noreply.github.com>
2023-03-04 21:58:15 +0900

    style: Card 컴포넌트 big Card 관련 class 적용

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,20 @@
 const Card = ({ cardInfo, size = 'small' }) => (
   <>
     <div className="card-box">
-      <div
-        className={`${size}-card`}
-        style={{ backgroundColor: cardInfo.backgroundColor }}
-      >
+      <div className={`${size}-card`} style={{ backgroundColor: cardInfo.backgroundColor }}>
         <div className="card-top">
-          <span className="card-text">{cardInfo.company}</span>
+          <span className={`card-text__${size}`}>{cardInfo.company}</span>
         </div>
         <div className="card-middle">
-          <div className="small-card__chip"></div>
+          <div className={`${size}-card__chip`}></div>
         </div>
         <div className="card-bottom">
           <div className="card-bottom__number">
-            <span className="card-text">{cardInfo.number}</span>
+            <span className={`card-text__${size}`}>{cardInfo.number}</span>
           </div>
           <div className="card-bottom__info">
-            <span className="card-text">{cardInfo.owner}</span>
-            <span className="card-text">{cardInfo.expiry}</span>
+            <span className={`card-text__${size}`}>{cardInfo.owner}</span>
+            <span className={`card-text__${size}`}>{cardInfo.expiry}</span>
           </div>
         </div>
       </div>
