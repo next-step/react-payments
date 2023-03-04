407a15351a6c9ed28c0437853ae8b465fb09440f
Woo KyungJun <natcho9010@gmail.com>
2023-03-04 21:50:35 +0900

    refactor: add Portal component & refactor useModal

--- src/hooks/useModal/useModal.tsx
+++ src/hooks/useModal/useModal.tsx
@@ -1,7 +1,18 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { memo, MouseEvent, MouseEventHandler, useCallback, useEffect, useState } from 'react';
+
+import { Portal } from '@/components/Portal';
 
 import { ModalBackground, ModalBackgroundProps } from './ModalBackground';
+
+const ModalWithPortal = memo((props: React.PropsWithChildren<ModalBackgroundProps>) => {
+  const { onBackgroundClick } = props;
+
+  return (
+    <Portal>
+      <ModalBackground {...props} onBackgroundClick={onBackgroundClick} />,
+    </Portal>
+  );
+});
 
 function useModal(initialState = false) {
   const [isShow, setIsShow] = useState(false);
@@ -12,36 +23,37 @@ function useModal(initialState = false) {
     }
   }, [initialState]);
 
-  const hideModal = useCallback(() => {
-    setIsShow(false);
-  }, []);
-
   const showModal = useCallback(() => {
     setIsShow(true);
   }, []);
 
-  const clearModalRoot = useCallback(() => {
-    document.getElementById('modal-root')!.innerHTML = '';
+  const hideModal = useCallback(() => {
+    setIsShow(false);
   }, []);
+
+  const handleModalBackgroundClick = useCallback(
+    (onBackgroundClick?: MouseEventHandler<Element>) => (e: MouseEvent<Element>) => {
+      hideModal();
+      onBackgroundClick?.(e);
+    },
+    [hideModal]
+  );
 
   const Modal = useCallback(
     (props: React.PropsWithChildren<ModalBackgroundProps>) => {
       if (!isShow) {
         return null;
       }
-      return ReactDOM.createPortal(
-        <ModalBackground {...props} onBackgroundClick={props.onBackgroundClick || hideModal} />,
-        document.getElementById('modal-root')!
-      );
+
+      return <ModalWithPortal {...props} onBackgroundClick={handleModalBackgroundClick(props.onBackgroundClick)} />;
     },
-    [isShow, hideModal]
+    [isShow, handleModalBackgroundClick]
   );
 
   return {
     Modal,
     hideModal,
     showModal,
-    clearModalRoot,
   };
 }
 
