46a90ca0207f2a80c00577ca946c8e265aad425e
yanggak12 <yanggak12@naver.com>
2023-03-05 00:58:47 +0900

    feat : add botton sheet & modal context

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './styles.css';
 import App from './App';
 import NewCardProvider from './contexts/NewCardContext';
+import ModalProvider from './contexts/ModalContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <NewCardProvider>
-      <App />
-    </NewCardProvider>
+    <ModalProvider>
+      <NewCardProvider>
+        <App />
+      </NewCardProvider>
+    </ModalProvider>
   </React.StrictMode>,
 );
