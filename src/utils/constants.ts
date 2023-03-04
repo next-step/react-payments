46a65d0038a25028cde11078a3b8c1b0f6c49f9c
yanggak12 <yanggak12@naver.com>
2023-03-05 02:07:20 +0900

    feat : add style & bank infos

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import { INewCardBank } from './types';
+
+export const bankList: INewCardBank[] = [
+  { title: '포코 카드', bgColor: '#E24141' },
+  { title: '준 카드', bgColor: '#547CE4' },
+  { title: '현석 카드', bgColor: '#73BC6D' },
+  { title: '윤호 카드', bgColor: '#DE59B9' },
+  { title: '환오 카드', bgColor: '#04C09E' },
+  { title: '태은 카드', bgColor: '#E76E9A' },
+  { title: '준일 카드', bgColor: '#F37D3B' },
+  { title: '은규 카드', bgColor: '#FBCD58' },
+];
