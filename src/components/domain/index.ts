db5c81d497dfc85a0ec58da0d4e73ce26f27e3a7
dahye1013 <alrean@naver.com>
2023-03-05 23:18:30 +0900

    remove: 미사용 emptyCard 컴포넌트 제거

--- src/components/domain/index.ts
+++ src/components/domain/index.ts
@@ -5,5 +5,4 @@ export { default as CardNickNameForm } from './CardNickNameForm';
 export { default as CardNumberInput } from './CardNumberInput';
 export { default as CardOwnerInput } from './CardOwnerInput';
 export { default as CardPwdInput } from './CardPwdInput';
-export { default as EmptyCard } from './EmptyCard';
 export { default as ExpiredDateInput } from './ExpiredDateInput';
