c80c42a5e735a2719a4ec60922423ce42b01ccb4
dahye1013 <alrean@naver.com>
2023-03-05 23:18:09 +0900

    refactor: uid에 매칭되는 카드 미존재시 메시지 노출 후 리다이렉션

--- src/pages/CardDetail/index.tsx
+++ src/pages/CardDetail/index.tsx
@@ -1,31 +1,38 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { CardNickNameForm, EmptyCard } from '@/components/domain';
+import { CardNickNameForm } from '@/components/domain';
 import { CreditCard, Layout, TopNavigation } from '@/components/UI';
+import { useRouter } from '@/hooks/useRouter';
 import { getCards } from '@/storage/service';
 import { CardData } from '@/types';
 
 export const CardDetailPage = () => {
   const params = useParams();
+  const { go } = useRouter();
 
   const [card, setCard] = useState<CardData>();
 
   useEffect(() => {
     const cards = getCards();
     const selectedCard = cards.find((card) => card.UID == params.cardId);
+    if (!selectedCard) {
+      alert('해당하는 카드가 존재하지 않습니다.');
+      go('/list');
+    }
+
     setCard(selectedCard);
   }, []);
 
-  if (!card) {
-    return <EmptyCard />;
-  }
-
   return (
-    <Layout variant="column">
-      <TopNavigation>카드 별칭 수정하기</TopNavigation>
-      <CreditCard size="small" cardInfo={card} />
-      <CardNickNameForm card={card} />
+    <Layout variant="column" justify="center">
+      {card && (
+        <>
+          <TopNavigation>카드 별칭 수정하기</TopNavigation>
+          <CreditCard size="small" cardInfo={card} />
+          <CardNickNameForm card={card} />
+        </>
+      )}
     </Layout>
   );
 };
--- src/pages/CardRegisterConfirmPage.tsx
+++ src/pages/CardRegisterConfirmPage.tsx
@@ -1,47 +1,39 @@
 import { useEffect, useState } from 'react';
 
-import { CardNickNameForm, EmptyCard } from '@/components/domain';
-import { Button, CreditCard } from '@/components/UI';
+import { CardNickNameForm } from '@/components/domain';
+import { Button, CreditCard, Layout } from '@/components/UI';
 import { useRouter } from '@/hooks/useRouter';
-import { styled } from '@/lib/stitches.config';
 import { getLatestCard } from '@/storage/service';
 import { CardData } from '@/types';
 
 export const CardRegisterConfirmPage = () => {
-  const [card, setCard] = useState<CardData>();
+  const [card, setCard] = useState<CardData | null>(null);
   const { go } = useRouter();
 
   useEffect(() => {
     const newCard = getLatestCard();
+    if (!newCard) {
+      alert('해당하는 카드가 존재하지 않습니다.');
+      go('/list');
+    }
     setCard(newCard);
   }, []);
 
-  if (!card) {
-    return <EmptyCard />;
-  }
-
   return (
-    <CardRegisterConfirmPageLayout>
-      <CreditCard size="small" cardInfo={card} />
-      <h2>카드 등록이 완료되었습니다.</h2>
-      <CardNickNameForm card={card} />
-      <Button
-        css={{ position: 'absolute', bottom: '$5', width: '$11' }}
-        onClick={() => go('/list')}
-      >
-        카드 목록
-      </Button>
-    </CardRegisterConfirmPageLayout>
+    <Layout variant="column" justify="center">
+      {card && (
+        <>
+          <CreditCard size="small" cardInfo={card} />
+          <h2>카드 등록이 완료되었습니다.</h2>
+          <CardNickNameForm card={card} />
+          <Button
+            css={{ position: 'absolute', bottom: '$5', width: '$11' }}
+            onClick={() => go('/list')}
+          >
+            카드 목록
+          </Button>
+        </>
+      )}
+    </Layout>
   );
 };
-
-const CardRegisterConfirmPageLayout = styled('div', {
-  position: 'relative',
-  height: '100%',
-  margin: '0 2rem',
-  display: 'flex',
-  justifyContent: 'space-around',
-  alignItems: 'center',
-  flexDirection: 'column',
-  placeContent: 'end center',
-});
