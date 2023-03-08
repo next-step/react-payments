4501504215038733c3a3ca7e1e4455a885ef51df
Woo KyungJun <natcho9010@gmail.com>
2023-03-08 18:21:42 +0900

    feat: CardList

--- src/pages/CardList/CardList.tsx
+++ src/pages/CardList/CardList.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { Card } from '@/components/Card';
 import { routes } from '@/routes';
+import { CardInfoProvider } from '@/stores/CardCreatorContext';
+
+import { useCardListWithLocalStorage } from '../CardNickname/hooks/useCardListWithLocalStorage';
 
 function CardList() {
+  const { cardList } = useCardListWithLocalStorage();
+
   return (
     <div className="app flex-column-center">
       <Link to={routes.cardCreator} className="card-box">
         <div className="empty-card">+</div>
       </Link>
+      {Object.entries(cardList).map(([key, val]) => (
+        <CardInfoProvider key={key} value={val}>
+          <Card cardId={key} />
+        </CardInfoProvider>
+      ))}
     </div>
   );
 }
