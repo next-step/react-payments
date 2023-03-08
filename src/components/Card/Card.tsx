ff2fb31a897045d98a34aa8a9966d52aa1153a49
Woo KyungJun <natcho9010@gmail.com>
2023-03-08 19:36:54 +0900

    style: import order

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,13 @@
 import React, { memo, MouseEvent } from 'react';
 
+import { ThemeSetter } from '@/components/ThemeSetter';
+import { useSelectCardCompany } from '@/stores/CardCreatorContext';
+
 import { CardNumbers } from './CardNumbers';
 import { CardOwnerName } from './CardOwnerName';
 import { CardExpireDate } from './CardExpireDate';
-import { CardWrapper } from './Card.styled';
-
-import { ThemeSetter } from '@/components/ThemeSetter';
-import { useSelectCardCompany } from '@/stores/CardCreatorContext';
 import { CardNickname } from './CardNickname';
+import { CardWrapper } from './Card.styled';
 
 interface CardProps {
   cardId?: string;
@@ -16,6 +16,7 @@ interface CardProps {
 }
 
 // TODO: 카드컴퍼니 에러가 있을 경우 테두리 깜박이기 및 문구 추가.
+// TODO: onclick이 있으면 click cursor 적용
 const Card = memo(({ cardId, disableNickname, onCardClick }: CardProps) => {
   const cardCompany = useSelectCardCompany();
 
