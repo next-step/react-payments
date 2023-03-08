1e5851c04b977a8c6a43f6567a52c822f1b33a67
seyoungjoy <diddpwl80@gmail.com>
2023-03-08 23:52:21 +0900

    refactor : 중복 선언 수정

--- src/context/CardContext.tsx
+++ src/context/CardContext.tsx
@@ -1,12 +1,12 @@
-import React, { useContext, useReducer } from 'react';
+import React, { createContext, ReactNode, useContext, useReducer } from 'react';
 import { CardDispatchType, CardInfoType } from '../type/card';
 import { reducer } from '../hook/cardReducer';
 import { initCardInfo } from '../data/init';
 
-const CardStateContext = React.createContext<CardInfoType | null>(null);
-const CardDispatchContext = React.createContext<CardDispatchType | null>(null);
+const CardStateContext = createContext<CardInfoType | null>(null);
+const CardDispatchContext = createContext<CardDispatchType | null>(null);
 
-export const CardProvider = ({ children }: { children: React.ReactNode }) => {
+export const CardProvider = ({ children }: { children: ReactNode }) => {
   const [state, dispatch] = useReducer(reducer, initCardInfo);
 
   return (
