f44267a769d6337af781e2aa22f3a7f613a7f4ca
seyoungjoy <diddpwl80@gmail.com>
2023-03-08 23:51:47 +0900

    refactor : browserRouter 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import PaymentCardList from './pages/PaymentCardList';
 import PaymentCardRegister from './pages/PaymentCardRegister';
 import Layout from './components/common/Layout';
@@ -11,19 +11,17 @@ import { ROUTE } from './constant/route';
 
 function App() {
   return (
-    <BrowserRouter>
-      <CardProvider>
-        <CardListProvider>
-          <Routes>
-            <Route path={ROUTE.HOME} element={<Layout />}>
-              <Route index element={<PaymentCardList />} />
-              <Route path={ROUTE.REGISTER} element={<PaymentCardRegister />} />
-              <Route path={ROUTE.COMPLETE} element={<PaymentCardComplete />} />
-            </Route>
-          </Routes>
-        </CardListProvider>
-      </CardProvider>
-    </BrowserRouter>
+    <CardProvider>
+      <CardListProvider>
+        <Routes>
+          <Route path={ROUTE.HOME} element={<Layout />}>
+            <Route index element={<PaymentCardList />} />
+            <Route path={ROUTE.REGISTER} element={<PaymentCardRegister />} />
+            <Route path={ROUTE.COMPLETE} element={<PaymentCardComplete />} />
+          </Route>
+        </Routes>
+      </CardListProvider>
+    </CardProvider>
   );
 }
 
