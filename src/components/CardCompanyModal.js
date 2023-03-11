6bdbcda1e61bff170837caa7bb27d20f8ff51409
realhee <wlsgml5628@gmail.com>
2023-03-12 00:10:43 +0900

    Fix: CardCompanyModal 반복문 key 추가

--- src/components/CardCompanyModal.js
+++ src/components/CardCompanyModal.js
@@ -18,9 +18,13 @@ export default function CardCompanyModal({ handleModalClick }) {
     <div className="modal-dimmed">
       <div className="modal">
         <div className="flex-center">
-          {CompanyNames[0].map((value) => {
+          {CompanyNames[0].map((value, index) => {
             return (
-              <div className="modal-item-container" onClick={handleModalClick}>
+              <div
+                className="modal-item-container"
+                onClick={handleModalClick}
+                key={index}
+              >
                 <div className={"modal-item-dot dot" + value["index"]}></div>
                 <span className="modal-item-name">{value["name"]} 카드</span>
               </div>
@@ -28,9 +32,13 @@ export default function CardCompanyModal({ handleModalClick }) {
           })}
         </div>
         <div className="flex-center">
-          {CompanyNames[1].map((value) => {
+          {CompanyNames[1].map((value, index) => {
             return (
-              <div className="modal-item-container" onClick={handleModalClick}>
+              <div
+                className="modal-item-container"
+                onClick={handleModalClick}
+                key={index}
+              >
                 <div className={"modal-item-dot dot" + value["index"]}></div>
                 <span className="modal-item-name">{value["name"]} 카드</span>
               </div>
