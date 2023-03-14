70b16cfa55c47b683a8d32a3428eb2a37adc534d
Woo KyungJun <natcho9010@gmail.com>
2023-03-14 14:11:08 +0900

    refactor: add useSeqeuntialAutoFocus

--- src/pages/CardCreator/CardCreator.tsx
+++ src/pages/CardCreator/CardCreator.tsx
@@ -1,11 +1,10 @@
-import React, { MouseEvent, useCallback, useEffect } from 'react';
+import React, { MouseEvent, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
 import { routes } from '@/routes';
 import { Card } from '@/components/Card';
 import { ThemeSetter } from '@/components/ThemeSetter';
 import { useCardContextApiSelector, useCardInfoSelector } from '@/stores/CardCreatorContext';
-import { checkIsArrayLast } from '@/utils';
 
 import { CardNumbersInputListPure } from './InputComponents/CardNumbersInputList';
 import { ExpireDatesInputListPure } from './InputComponents/ExpireDatesInputList';
@@ -14,6 +13,7 @@ import { SecurityCodesInputListPure } from './InputComponents/SecurityCodesInput
 import { PasswordsInputListPure } from './InputComponents/PasswordsInputList';
 import { CardCompanyModel, useCardCompanySelectModal } from './hooks/useCardCompanySelectModal';
 import { SubmitButton } from './SubmitButton';
+import { useSequentialAutoFocus } from './hooks/useSequentialAutoFocus';
 
 export function CardCreator() {
   const cardInfo = useCardInfoSelector();
@@ -37,45 +37,7 @@ export function CardCreator() {
     [apis, hideModal]
   );
 
-  // TODO: cardInfo가 바뀔때마다 useEffect로 invalid체크 -> invalid 주목시키기 + 다음 ref focus하기
-  useEffect(() => {
-    if (!cardInfo) return;
-
-    const { cardCompany, cardNumbers, expireDates, cardOwners, passwords, securityCodes } = cardInfo;
-    const inputLists = [[cardCompany], cardNumbers, expireDates, cardOwners, securityCodes, passwords];
-
-    inputLists.every((inputList, inputListIndex) => {
-      const isActiveElement = inputList?.some((input) => {
-        return document.activeElement === input.ref;
-      });
-
-      if (!isActiveElement) return true;
-
-      // @ts-ignore
-      const isEveryInputListValid = inputList?.every((input, i) => {
-        // TODO: 이 로직을 추상화해서 선언적으로 보이도록 클린코드 필요.
-
-        const isFinished = input?.checkIsInputFinished();
-
-        if (!isFinished) {
-          input?.ref?.focus();
-          return isFinished;
-        }
-
-        if (checkIsArrayLast(inputList, i)) {
-          inputList?.[i + 1]?.ref?.focus();
-        }
-
-        return isFinished;
-      });
-
-      if (isEveryInputListValid && !checkIsArrayLast(inputLists, inputListIndex)) {
-        inputLists?.[inputListIndex + 1][0]?.ref?.focus();
-      }
-
-      return isEveryInputListValid;
-    });
-  }, [cardInfo]);
+  useSequentialAutoFocus(cardInfo);
 
   return (
     <ThemeSetter className="app" theme={cardInfo?.cardCompany.value?.theme}>
---
+++ src/pages/CardCreator/hooks/useSequentialAutoFocus.ts
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+
+import { checkIsArrayLast } from '@/utils';
+import type { useCardInfoSelector } from '@/stores/CardCreatorContext';
+
+export function useSequentialAutoFocus(cardInfo: ReturnType<typeof useCardInfoSelector>) {
+  useEffect(() => {
+    if (!cardInfo) return;
+
+    const { cardCompany, cardNumbers, expireDates, cardOwners, passwords, securityCodes } = cardInfo;
+    const inputLists = [[cardCompany], cardNumbers, expireDates, cardOwners, securityCodes, passwords];
+
+    inputLists.every((inputList, inputListIndex) => {
+      const isActiveList = inputList?.some((input) => checkIsActiveElement(input.ref));
+      if (!isActiveList) return true;
+
+      // @ts-ignore
+      const isEveryInputFinished = inputList?.every((input, i) => {
+        const isFinished = input?.checkIsInputFinished();
+
+        if (isFinished && checkIsActiveElement(input.ref) && !checkIsArrayLast(inputList, i)) {
+          inputList?.[i + 1]?.ref?.focus();
+        }
+
+        return isFinished;
+      });
+
+      if (isEveryInputFinished && !checkIsArrayLast(inputLists, inputListIndex)) {
+        inputLists?.[inputListIndex + 1][0]?.ref?.focus();
+      }
+
+      return isEveryInputFinished;
+    });
+  }, [cardInfo]);
+}
+
+function checkIsActiveElement(ref?: HTMLElement | null) {
+  return document.activeElement === ref;
+}
