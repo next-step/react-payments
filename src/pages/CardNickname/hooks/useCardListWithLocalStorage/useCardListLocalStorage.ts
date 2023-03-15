dfecde004c04db53508cef341e98ef5043e766e8
Woo KyungJun <natcho9010@gmail.com>
2023-03-16 00:28:16 +0900

    fix: bug of loading cardLoading from localstorage

--- src/pages/CardNickname/hooks/useCardListWithLocalStorage/useCardListLocalStorage.ts
+++ src/pages/CardNickname/hooks/useCardListWithLocalStorage/useCardListLocalStorage.ts
@@ -1,18 +1,20 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
-import { useLocalStorage } from '@/hooks/useLocalStorage';
 
+import { useLocalStorage } from '@/hooks/useLocalStorage';
 import type { CardStore } from '@/stores/CardCreatorContext/cardStore';
 import { useCardContextApiSelector } from '@/stores/CardCreatorContext';
 
 const LOCAL_STORAGE_CARD_LIST_KEY = 'cardList';
 
-type PropertyType = { theme?: any; value?: any; name?: any };
-type ObjectPropertyWithPrimitiveProperty = { [key: string]: PropertyType | null | PropertyType[] };
-type CardStoreJSON = Record<keyof CardStore, ObjectPropertyWithPrimitiveProperty>;
+type CardStatePOJO = { theme?: any; value?: any; name?: any };
+type CardStateValuePOJO = CardStatePOJO[] | CardStatePOJO | null;
+type CardStoreJSON = Record<keyof CardStore, CardStateValuePOJO>;
 
+// FIXME: 클린코드 필요!
+// FIXME: LocalStorage 의존성 제거 -> useGetCard라고 이름 바꿔서 범용성 늘리기
 export function useCardListWithLocalStorage() {
   const { storageValue, setValueInLocalStorage } = useLocalStorage(LOCAL_STORAGE_CARD_LIST_KEY);
-  const [cardList, setCardList] = useState<{ [key: string]: any }>({});
+  const [cardList, setCardList] = useState<{ [createdAt: string]: any } | null>(null);
 
   const cardContextApis = useCardContextApiSelector();
 
@@ -24,6 +26,8 @@ export function useCardListWithLocalStorage() {
 
   const getCardFromStorage = useCallback(
     (cardId: string, fallback?: () => void): void => {
+      if (typeof storageValue === undefined || cardList === null) return;
+
       const card = cardList[cardId] as CardStoreJSON | undefined;
       if (!card) {
         fallback && fallback();
@@ -44,13 +48,15 @@ export function useCardListWithLocalStorage() {
         cardContextApis?.dispatch({ type, payload: { index: 0, ...cardInfo } });
       });
     },
-    [cardList, cardContextApis]
+    [storageValue, cardList, cardContextApis]
   );
 
   const setCardInStorage = useCallback(
     (cardId: string | number, card: CardStore) => {
+      if (cardList === null) return;
+
       cardList[cardId] = card;
-      const stringifyAbleObject = formatToStringifyAbleObject(card) as CardStoreJSON;
+      const stringifyAbleObject = changeValueToPOJO(card);
       setValueInLocalStorage(
         LOCAL_STORAGE_CARD_LIST_KEY,
         JSON.stringify({ ...cardList, [cardId]: stringifyAbleObject })
@@ -70,7 +76,7 @@ export function useCardListWithLocalStorage() {
 
   return useMemo(
     () => ({
-      cardList,
+      cardList: cardList || {},
       getCardFromStorage,
       setCardInStorage,
       deleteCard,
@@ -79,15 +85,17 @@ export function useCardListWithLocalStorage() {
   );
 }
 
-function formatToStringifyAbleObject(object: ObjectPropertyWithPrimitiveProperty): ObjectPropertyWithPrimitiveProperty {
-  return Object.entries(object).reduce<ObjectPropertyWithPrimitiveProperty>((prev, [key, val]) => {
+function changeValueToPOJO(object: CardStore): CardStoreJSON {
+  return Object.entries(object).reduce<CardStoreJSON>((prev, [key, val]) => {
     if (Array.isArray(val)) {
-      const stringifyAbleObject = val.map(({ theme, value, name }) => ({ theme, value, name }));
+      const stringifyAbleObject = val.map(({ value }) => ({ value }));
+      // @ts-ignore
       prev[key] = stringifyAbleObject;
       return prev;
     }
 
+    // @ts-ignore
     prev[key] = { theme: val?.theme, value: val?.value, name: val?.name };
     return prev;
-  }, {});
+  }, {} as CardStoreJSON);
 }
