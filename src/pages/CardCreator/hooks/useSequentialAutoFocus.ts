79f1495f5b613d3c0edcb27c7a0f627bee6bd48c
Woo KyungJun <natcho9010@gmail.com>
2023-03-16 23:44:29 +0900

    style: clean code in useSequentialAutoFocus

--- src/pages/CardCreator/hooks/useSequentialAutoFocus.ts
+++ src/pages/CardCreator/hooks/useSequentialAutoFocus.ts
@@ -3,36 +3,69 @@ import { useEffect } from 'react';
 import { checkIsArrayLast } from '@/utils';
 import type { useCardSelector } from '@/stores/CardContext';
 
-// FIXME: 클린코드
 export function useSequentialAutoFocus(cardInfo: ReturnType<typeof useCardSelector>) {
   useEffect(() => {
     if (!cardInfo) return;
 
     const { cardCompany, cardNumbers, expireDates, cardOwners, passwords, securityCodes } = cardInfo;
-    const inputLists = [[cardCompany], cardNumbers, expireDates, cardOwners, securityCodes, passwords];
+    const inputRowList = [[cardCompany], cardNumbers, expireDates, cardOwners, securityCodes, passwords];
 
-    inputLists.every((inputList, inputListIndex) => {
-      const isActiveList = inputList?.some((input) => checkIsActiveElement(input.ref));
-      if (!isActiveList) return true;
-
-      // @ts-ignore
-      const isEveryInputFinished = inputList?.every((input, i) => {
-        const isFinished = input?.checkIsInputFinished();
-
-        if (isFinished && checkIsActiveElement(input.ref) && !checkIsArrayLast(inputList, i)) {
-          inputList?.[i + 1]?.ref?.focus();
-        }
-
-        return isFinished;
-      });
-
-      if (isEveryInputFinished && !checkIsArrayLast(inputLists, inputListIndex)) {
-        inputLists?.[inputListIndex + 1][0]?.ref?.focus();
-      }
-
-      return isEveryInputFinished;
-    });
+    // @ts-ignore
+    inputRowList.every(checkIsEveryRowFinished(inputRowList));
   }, [cardInfo]);
+}
+
+function checkIsEveryRowFinished(inputRowList: { ref: HTMLInputElement }[][]) {
+  return function everyCallback(inputRow: { ref: HTMLInputElement }[], inputRowListIndex: number) {
+    const isActiveRow = inputRow?.some((input) => checkIsActiveElement(input.ref));
+    if (!isActiveRow) return true;
+
+    // @ts-ignore
+    const isEveryRowFinished = inputRow?.every(checkIsEveryInputFinished(inputRow));
+    if (isAllowToFocusNextInputRow({ isEveryRowFinished, currentIndex: inputRowListIndex, inputRowList })) {
+      inputRowList?.[inputRowListIndex + 1][0]?.ref?.focus();
+    }
+
+    return isEveryRowFinished;
+  };
+}
+
+function isAllowToFocusNextInputRow({
+  isEveryRowFinished,
+  inputRowList,
+  currentIndex,
+}: {
+  isEveryRowFinished: boolean;
+  inputRowList: any[];
+  currentIndex: number;
+}) {
+  return isEveryRowFinished && !checkIsArrayLast(inputRowList, currentIndex);
+}
+
+function checkIsEveryInputFinished(inputRow: { ref: HTMLInputElement }[]) {
+  return function everyCallback(input: { checkIsInputFinished: () => boolean; ref: HTMLInputElement }, i: number) {
+    const isInputFinished = input?.checkIsInputFinished();
+
+    if (isAllowToFocusNextInput({ isInputFinished, inputRef: input.ref, inputRow, currentIndex: i })) {
+      inputRow?.[i + 1]?.ref?.focus();
+    }
+
+    return isInputFinished;
+  };
+}
+
+function isAllowToFocusNextInput({
+  isInputFinished,
+  inputRef,
+  inputRow,
+  currentIndex,
+}: {
+  isInputFinished: boolean;
+  inputRef: HTMLInputElement;
+  inputRow: any[];
+  currentIndex: number;
+}) {
+  return isInputFinished && checkIsActiveElement(inputRef) && !checkIsArrayLast(inputRow, currentIndex);
 }
 
 function checkIsActiveElement(ref?: HTMLElement | null) {
