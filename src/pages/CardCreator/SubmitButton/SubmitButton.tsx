a3c7225d6a62fa2cca766ecdf8b4348299d184c0
Woo KyungJun <natcho9010@gmail.com>
2023-03-19 00:56:47 +0900

    refactor: divide submit logic in SubmitButton

--- src/pages/CardCreator/SubmitButton/SubmitButton.tsx
+++ src/pages/CardCreator/SubmitButton/SubmitButton.tsx
@@ -1,26 +1,14 @@
-import React, { MouseEvent } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { useGetInvalidCardInputState } from '@/hooks';
 import { routes } from '@/routes';
-import { useErrorContextApiSelector } from '@/stores/ErrorContext';
+
+import { useCardSubmitEvent } from '../hooks';
 
 interface SubmitButtonProps {}
 
 export function SubmitButton(_: SubmitButtonProps) {
-  const errorApis = useErrorContextApiSelector();
-
-  const invalidElement = useGetInvalidCardInputState();
-
-  // select된 inputState가 변하면 아래 함수도 새로 만들어져야 하므로, useCallback은 적용하지 않음.
-  const handleSubmitButtonClick = (e: MouseEvent<HTMLAnchorElement>) => {
-    if (invalidElement) {
-      e.preventDefault();
-
-      errorApis?.dispatch({ type: invalidElement.key, message: invalidElement.getInvalidMessage() });
-      invalidElement.ref?.focus();
-    }
-  };
+  const handleSubmitButtonClick = useCardSubmitEvent();
 
   return (
     <div className="button-box">
