dcddd26f5ae1cec6ba24f35ab38b91b4ffb9b978
Woo KyungJun <natcho9010@gmail.com>
2023-03-19 00:57:05 +0900

    style: ellipsis overflow text

--- src/components/Card/CardOwnerName.tsx
+++ src/components/Card/CardOwnerName.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { styled } from '@/stitches.config';
 
 import { TCardOwnerNameProp } from './types';
+
+const StyledCardOwnerName = styled('span', {
+  maxWidth: '75%',
+  overflowX: 'hidden',
+  textOverflow: 'ellipsis',
+});
 
 interface CardOwnerNameProps {
   ownerName?: TCardOwnerNameProp;
 }
 
 export function CardOwnerName({ ownerName }: CardOwnerNameProps) {
-  return <span className="card-text card-name-spacing">{ownerName || 'NAME'}</span>;
+  return <StyledCardOwnerName className="card-text card-name-spacing">{ownerName || 'NAME'}</StyledCardOwnerName>;
 }
