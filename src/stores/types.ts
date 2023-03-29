fc57e451ea148683e75a79cd991d23ec6bd08cdd
Woo KyungJun <natcho9010@gmail.com>
2023-03-29 11:31:36 +0900

    chore: clean comments

--- src/stores/types.ts
+++ src/stores/types.ts
@@ -9,7 +9,7 @@ export type DispatchContext<T> = { dispatch: T } | null;
 export interface IInputState<T = string> {
   value?: T;
 }
-// TODO: 당연한 method는 상속받도록
+
 // 하나의 InputElement에 해당하는 타입
 export interface IInputElement<T = string> extends IInputState<T> {
   errorMessage?: string;
