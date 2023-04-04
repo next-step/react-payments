af7f6ac23682c9c62697253fad7d882362b189b2
Hojoong Choi <inegg.apps@gmail.com>
2023-04-05 08:38:47 +0900

    chore: 스토리북 빌드 설정 변경

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,19 @@
+import { StepContextProvider } from '../src/context/StepContext';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <StepContextProvider>
+      <Story />
+    </StepContextProvider>
+  ),
+];
--- src/pages/card-edit/CardEditPage.stories.tsx
+++ src/pages/card-edit/CardEditPage.stories.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { withReactContext } from 'storybook-react-context';
 import CardEdit from './CardEdit';
 
 export default {
   title: 'Payments/Pages/CardEdit',
   component: CardEdit,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof CardEdit>;
 
 const Template: ComponentStory<typeof CardEdit> = () => {
