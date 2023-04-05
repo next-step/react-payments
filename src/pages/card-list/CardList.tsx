5b5af505b3072071dc99bd8b8f3ba96b505040c6
Hojoong Choi <inegg.apps@gmail.com>
2023-04-05 14:04:33 +0900

    feat: 카드 목록에서 등록된 카드 조회하기

--- src/pages/card-list/CardList.tsx
+++ src/pages/card-list/CardList.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Card } from '../../components';
 import { Frame } from '../../components/Frame';
 import { PAYMENTS_STEP, useStepContext } from '../../context/StepContext';
+import { getEnrolledCards } from '../../domain/payments/cardStorage';
+import { ICard } from '../../domain/payments/types';
 import './CardList.css';
 
 function CardList() {
   const { setStep } = useStepContext();
+
+  const enrolledCards: ICard[] = getEnrolledCards();
 
   const handleClick = () => {
     setStep && setStep(PAYMENTS_STEP.ADD);
@@ -18,6 +23,19 @@ function CardList() {
             +
           </div>
         </li>
+        {enrolledCards.map(({ cardName, owner, numbers, expiredMonth, expiredYear, alias, cvc }) => (
+          <li key={numbers.join('')}>
+            <Card
+              cardName={cardName}
+              owner={owner}
+              numbers={numbers}
+              expiredMonth={expiredMonth}
+              expiredYear={expiredYear}
+              alias={alias}
+              cvc={cvc}
+            />
+          </li>
+        ))}
       </ul>
     </Frame>
   );
