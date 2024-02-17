77f57026e1b56c5bc10f033f6490abd6f28fdf7d
Chung-il Jung <jungchungil7@gmail.com>
2024-02-17 22:10:31 +0900

    [클린코드 리액트 2기 정충일] 페이먼츠 미션 Step 3 (#99)
    * init: payments project
    
    * feat: 뒤로가기 버튼 클릭 시 카드 목록 페이지로 이동
    
    * feat: useRefs 구현
    
    * refactor: useRefs
    
    * remove: stories dummy
    
    * fix: useRefs (임새해결)
    
    * feat: button component
    
    * refactor: 주석삭제, 옵셔널
    
    * rename: useRefs -> useImmutableRefs
    
    * rename: useImmutableRefs -> useRefObjects
    
    * refactor: cardNumberInput
    
    * refactor: index.ts
    
    * feat: masking util func
    
    * feat: card component
    
    * refactor: formatCardNumber
    
    * rename: alias -> bank
    
    * feat: Context API를 활용해 전역 상태 관리 및 계층 재구성
    
    * refactor: Card depth 제거 및 formatCardNumber 의존성 제거
    
    * refactor: prevState 활용
    
    * rename: cardData -> card
    
    * rename: cardAdded -> addCardToDatabase
    
    * refactor: card interface extends
    
    * refactor: component index.ts
    
    * rename: add -> update Alias
    
    * feat: 카드를 클릭하면, 카드 별칭 수정(카드 추가 완료 페이지)로 이동한다.
    
    * remove: console.log
    
    * refactor: Date -> number 타입 변경
    
    * feat: eslint 규칙 추가(eqeqeq, no console)
    
    * feat: 카드사가 선택되고 유효한 카드 번호 16자리를 모두 입력하면, 자동으로 만료일로 focus된다.
    
    * feat: 자동으로 다음 input으로 focus
    
    * refactor: CardProps type
    
    * feat: isValidExpiryDate
    
    * feat: 유효성 메세지 css
    
    * refactor: type num1, num2 .. -> numbers
    
    * feat: 유효성 검증
    
    * feat: validateCard 예외처리
    
    * refactor: handleRefInputMaxLength
    
    * refactor: validateCard
    
    * feat: 보안코드 툴팁
    
    * feat: 가상 키보드 숫자
    
    * feat: 숫자 랜덤 유틸 함수

--- src/pages/AddCard.tsx
+++ src/pages/AddCard.tsx
@@ -1,43 +1,81 @@
-import { ChangeEvent, RefObject, useContext, useRef, useState } from 'react';
+import { ChangeEvent, useContext, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Button, Card } from '../components';
+import { CardNumber } from '../components/Card/Card';
 import { CreditCard, PaymentsContext } from '../context/PaymentsContext';
 import useRefObjects from '../hooks/useRefObjects';
-import { extractNumbers } from '../utils';
+import {
+  areAllRefsMaxLength,
+  extractNumbers,
+  handleRefInputMaxLength,
+  isValidExpiryDate,
+} from '../utils';
 
 const AddCard = () => {
   const { addCard } = useContext(PaymentsContext);
   const navigate = useNavigate();
   const cardRefs = useRefObjects<HTMLInputElement>(4);
   const expirationDateRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
   const securityCodeRef = useRef<HTMLInputElement>(null);
   const cardPasswordRefs = useRefObjects<HTMLInputElement>(2);
-  const [name, setName] = useState('');
+  const [holder, setHolder] = useState('');
+  const [errorMessages, setErrorMessages] = useState({
+    cardNumber: '',
+    CVC: '',
+    expiry: '',
+    password: '',
+  });
+  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
 
-  const handleInputChange = (
-    index: number,
-    refs: RefObject<HTMLInputElement>[],
-    maxLength: number
-  ) => {
-    const currentInput = refs[index].current;
-    if (!currentInput) return;
+  const validateCard = () => {
+    const errors = {
+      cardNumber: '',
+      CVC: '',
+      password: '',
+      expiry: '',
+    };
 
-    const value = extractNumbers(currentInput.value);
-    currentInput.value = value;
+    const isCardNumberValid = areAllRefsMaxLength(cardRefs);
+    const isPasswordValid = areAllRefsMaxLength(cardPasswordRefs);
+    const isCvcValid = securityCodeRef.current?.value.length === 3;
+    const isExpiryValid = isValidExpiryDate(
+      expirationDateRef.current?.value || ''
+    );
 
-    if (value.length === maxLength) {
-      if (index < refs.length - 1) {
-        refs[index + 1].current?.focus();
-      }
+    if (!isCardNumberValid) {
+      errors.cardNumber = '카드 번호는 모두 4자리여야 합니다.';
     }
+    if (!isPasswordValid) {
+      errors.password = '카드 비밀번호는 모두 1자리여야 합니다.';
+    }
+    if (!isCvcValid) {
+      errors.CVC = 'CVC는 3자리여야 합니다.';
+    }
+    if (!isExpiryValid) {
+      errors.expiry = '유효한 만료일을 입력해주세요. (MM/YY)';
+    }
+
+    setErrorMessages(errors);
+    return isCardNumberValid && isPasswordValid && isCvcValid && isExpiryValid;
   };
 
   const handleCardNumber = (index: number) =>
-    handleInputChange(index, cardRefs, 4);
+    handleRefInputMaxLength(
+      index,
+      cardRefs,
+      index < cardRefs.length - 1 ? cardRefs[index + 1] : expirationDateRef
+    );
 
   const handleCardPassword = (index: number) =>
-    handleInputChange(index, cardPasswordRefs, 1);
+    handleRefInputMaxLength(
+      index,
+      cardPasswordRefs,
+      index < cardPasswordRefs.length - 1
+        ? cardPasswordRefs[index + 1]
+        : undefined
+    );
 
   const checkExpiration = () => {
     if (!expirationDateRef.current) return;
@@ -46,15 +84,14 @@ const AddCard = () => {
     if (currentValue === '') return;
 
     const month = currentValue.slice(0, 2);
-    if (month < JANUARY || month > DECEMBER) {
-      alert(MONTH_VALIDITY_MESSAGE);
-      expirationDateRef.current.value = '';
-      return;
-    }
 
     const date =
       month + (currentValue.length > 2 ? '/' + currentValue.slice(2, 4) : '');
     expirationDateRef.current.value = date;
+
+    if (currentValue.length === 4) {
+      nameRef.current?.focus();
+    }
   };
 
   const handleChangeExpirationDate = (e: ChangeEvent<HTMLInputElement>) => {
@@ -76,18 +113,31 @@ const AddCard = () => {
     securityCodeRef.current.value = extractNumbers(
       securityCodeRef.current.value
     );
+
+    if (securityCodeRef.current.value.length === 3) {
+      cardPasswordRefs[0].current?.focus();
+    }
   };
 
+  const cardNumber: CardNumber = [
+    cardRefs[0].current?.value,
+    cardRefs[1].current?.value,
+    cardRefs[2].current?.value,
+    cardRefs[3].current?.value,
+  ];
+
   const card = {
-    num1: cardRefs[0].current?.value,
-    num2: cardRefs[1].current?.value,
-    num3: cardRefs[2].current?.value,
-    num4: cardRefs[3].current?.value,
-    holder: name,
+    cardNumber,
+    CVC: securityCodeRef.current?.value,
+    holder,
+    password1: cardPasswordRefs[0].current?.value,
+    password2: cardPasswordRefs[1].current?.value,
     expiry: expirationDateRef.current?.value,
   };
 
-  const addCardToDatabase = () => {
+  const handleSubmitCard = () => {
+    if (!validateCard()) return;
+
     const newCard: CreditCard = { ...card, id: new Date().getTime() };
     addCard(newCard);
     navigate('/card-added', { state: newCard });
@@ -116,6 +166,7 @@ const AddCard = () => {
             />
           ))}
         </div>
+        <p className="error-message">{errorMessages.cardNumber}</p>
       </div>
       <div className="input-container">
         <span className="input-title">만료일</span>
@@ -129,31 +180,66 @@ const AddCard = () => {
             onChange={handleChangeExpirationDate}
           />
         </div>
+        <p className="error-message">{errorMessages.expiry}</p>
       </div>
       <div className="input-container">
         <span className="input-title">카드 소유자 이름(선택)</span>
         <span className="input-title">
-          {name.length} / {NAME_MAX_LENGTH}
+          {holder.length} / {NAME_MAX_LENGTH}
         </span>
         <input
           type="text"
           className="input-basic"
           placeholder="카드에 표시된 이름과 동일하게 입력하세요."
           maxLength={NAME_MAX_LENGTH}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={holder}
+          ref={nameRef}
+          onChange={(e) => setHolder(e.target.value)}
         />
       </div>
       <div className="input-container">
         <span className="input-title">보안코드(CVC/CVV)</span>
-        <input
-          className="input-basic w-25"
-          type="password"
-          maxLength={3}
-          ref={securityCodeRef}
-          onChange={handleSecurityCode}
-        />
+        <div className="cvc">
+          <input
+            className="input-basic w-25"
+            type="password"
+            maxLength={3}
+            ref={securityCodeRef}
+            onChange={handleSecurityCode}
+          />
+          <button
+            className="cvc-info"
+            onClick={() => setIsTooltipOpen(true)}
+            onBlur={() => setIsTooltipOpen(false)}
+          >
+            <svg
+              width="27"
+              height="27"
+              viewBox="0 0 27 27"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="13.5"
+                cy="13.5"
+                r="13"
+                fill="white"
+                stroke="#BABABA"
+              />
+              <path
+                d="M12.5547 16.8203C12.5547 15.9544 12.6621 15.2643 12.877 14.75C13.0918 14.2357 13.515 13.6725 14.1465 13.0605C14.7845 12.4421 15.1882 12.0026 15.3574 11.7422C15.6178 11.3451 15.748 10.9154 15.748 10.4531C15.748 9.84115 15.5951 9.37565 15.2891 9.05664C14.9896 8.73112 14.5469 8.56836 13.9609 8.56836C13.401 8.56836 12.9486 8.72786 12.6035 9.04688C12.265 9.35938 12.0957 9.78581 12.0957 10.3262H9.72266C9.73568 9.17383 10.1263 8.26237 10.8945 7.5918C11.6693 6.92122 12.6914 6.58594 13.9609 6.58594C15.2695 6.58594 16.2884 6.91797 17.0176 7.58203C17.7533 8.24609 18.1211 9.17383 18.1211 10.3652C18.1211 11.4264 17.6263 12.4714 16.6367 13.5L15.4355 14.6816C15.0059 15.1699 14.7845 15.8828 14.7715 16.8203H12.5547ZM12.3887 19.8574C12.3887 19.4733 12.5091 19.1641 12.75 18.9297C12.9909 18.6888 13.3164 18.5684 13.7266 18.5684C14.1432 18.5684 14.472 18.6921 14.7129 18.9395C14.9538 19.1803 15.0742 19.4863 15.0742 19.8574C15.0742 20.2155 14.957 20.515 14.7227 20.7559C14.4883 20.9967 14.1562 21.1172 13.7266 21.1172C13.2969 21.1172 12.9648 20.9967 12.7305 20.7559C12.5026 20.515 12.3887 20.2155 12.3887 19.8574Z"
+                fill="#969696"
+              />
+            </svg>
+          </button>
+        </div>
+        {isTooltipOpen && (
+          <div className="cvc-tooltip">
+            보안코드는 카드 뒷면의 마지막 3자리 숫자입니다.
+          </div>
+        )}
       </div>
+      <p className="error-message">{errorMessages.CVC}</p>
       <div className="input-container">
         <span className="input-title">카드 비밀번호</span>
         <input
@@ -183,8 +269,9 @@ const AddCard = () => {
           disabled
         />
       </div>
+      <p className="error-message">{errorMessages.password}</p>
       <div className="button-box">
-        <Button onClick={addCardToDatabase}>다음</Button>
+        <Button onClick={handleSubmitCard}>다음</Button>
       </div>
     </section>
   );
@@ -210,9 +297,6 @@ const PrevIcon = () => {
   );
 };
 
-const JANUARY = '01';
-const DECEMBER = '12';
-const MONTH_VALIDITY_MESSAGE = '유효하지 않아요';
 const NAME_MAX_LENGTH = 30;
 
 const cardNumberInputTypes = ['text', 'text', 'password', 'password'];
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,66 @@
+import { RefObject } from 'react';
+
 export const extractNumbers = (value: string) => {
   return value.replace(/[^0-9]/g, '');
 };
 
 export const masking = (value: string | undefined) => {
-  if (value == null) return;
+  if (value === null || value === undefined) return;
 
   const mask = '*';
   const NumberRegex = /[0-9]/g;
 
   return value.replace(NumberRegex, mask);
+};
+
+export const areAllRefsMaxLength = (refs: RefObject<HTMLInputElement>[]) => {
+  return refs.every(
+    (ref) => ref.current?.value.length === ref.current?.maxLength
+  );
+};
+
+const JANUARY = 1;
+const DECEMBER = 12;
+export const isValidExpiryDate = (expiry: string) => {
+  if (expiry.length !== 5) return false;
+
+  const [month, year] = expiry.split('/');
+
+  const inputYear = parseInt(`20${year}`, 10);
+  const inputMonth = parseInt(month, 10);
+
+  if (inputMonth < JANUARY || inputMonth > DECEMBER) {
+    return false;
+  }
+
+  const now = new Date();
+  if (
+    inputYear < now.getFullYear() ||
+    (inputYear === now.getFullYear() && inputMonth < now.getMonth() + 1)
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+export const handleRefInputMaxLength = (
+  index: number,
+  refs: RefObject<HTMLInputElement>[],
+  nextRef?: RefObject<HTMLInputElement>
+) => {
+  const currentInput = refs[index].current;
+  if (!currentInput) return;
+
+  const value = extractNumbers(currentInput.value);
+  currentInput.value = value;
+
+  if (value.length === refs[0].current?.maxLength && nextRef) {
+    nextRef.current?.focus();
+  }
+};
+
+export const shuffle = (arr: number[]) => {
+  const shuffled = arr.map((value) => ({ value, sort: Math.random() }));
+  return shuffled.sort((a, b) => a.sort - b.sort).map(({ value }) => value);
 };
