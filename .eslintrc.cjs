d8d89506eb1cd39cc69ea311560636c81e3342a1
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-02-24 19:45:48 +0900

    Vite 기반의 React 프로젝트 생성

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'xo',
+	],
+	overrides: [
+		{
+			extends: [
+				'xo-typescript',
+			],
+			files: [
+				'*.ts',
+				'*.tsx',
+			],
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+	],
+	rules: {
+	},
+};
