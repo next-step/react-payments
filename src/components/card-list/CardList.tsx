f36906ae5872d20c099faf6182e8e46871b84210
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-02-26 01:44:44 +0900

    카드가 등록되어 있지 않은 리스트로 HTML 변경

--- src/components/card-list/CardList.tsx
+++ src/components/card-list/CardList.tsx
@@ -6,26 +6,6 @@ export default function CardList() {
 					<h2 className='page-title mb-10'>보유 카드</h2>
 				</div>
 				<div className='card-box'>
-					<div className='small-card'>
-						<div className='card-top'>
-							<span className='card-text'>클린카드</span>
-						</div>
-						<div className='card-middle'>
-							<div className='small-card__chip'></div>
-						</div>
-						<div className='card-bottom'>
-							<div className='card-bottom__number'>
-								<span className='card-text'>1111 - 2222 - oooo - oooo</span>
-							</div>
-							<div className='card-bottom__info'>
-								<span className='card-text'>YUJO</span>
-								<span className='card-text'>12 / 23</span>
-							</div>
-						</div>
-					</div>
-				</div>
-				<span className='card-nickname'>법인카드</span>
-				<div className='card-box'>
 					<div className='empty-card'>+</div>
 				</div>
 			</div>
