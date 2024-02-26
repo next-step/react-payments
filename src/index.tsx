39a908c2cdd4af91e309bf9c2dc290e7fe8e493c
bxxmi <bmi6638@gmail.com>
2024-02-26 21:30:51 +0900

    [config] 기본 환경 구성

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+import "./styles/index.css";
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(<App />);
