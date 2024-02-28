a49c7f111160c64422a6b0b5facd80b44e830c78
Han-D-Peter <secret_wings@naver.com>
2024-02-28 14:02:34 +0900

    chore: storybook test env

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,20 +1,20 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from './Button';
+import { Button } from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 } satisfies Meta<typeof Button>;
 
@@ -25,26 +25,47 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Button',
+    label: "Button",
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    label: "Button",
   },
 };
 
 export const Large: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    size: "large",
+    label: "Button",
   },
 };
 
 export const Small: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    size: "small",
+    label: "Button",
   },
+};
+
+import { expect } from "@storybook/jest";
+import { waitFor, userEvent, within } from "@storybook/testing-library";
+
+// ...
+
+// Default 스토리에 인터랙션 스토리를 재생하는 속성을 추가한다.
+Large.play = async ({ canvasElement }) => {
+  // 직접 screen API를 쓸 수도 있지만 스토리북에서는 within(canvasElement) 로 캔버스를 가져올 것을 권장한다.
+  const canvas = within(canvasElement);
+
+  await waitFor(() => {
+    expect(canvas.queryByText("You have no tasks")).toBe(null);
+  });
+
+  const getTask = () => canvas.getByRole("listitem", { name: "Export logo" });
+
+  const pinButton = within(getTask()).getByRole("button", { name: "pin" });
+
+  userEvent.click(pinButton);
 };
