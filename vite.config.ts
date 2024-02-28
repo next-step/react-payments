e441ed2cea36b2bf83c43f182258bd935e806b0e
Jay Lee <roof2586@naver.com>
2024-02-28 21:14:36 +0900

    project kick-off: vite + react + ts

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@assets', replacement: '/src/assets' },
+    ],
+  },
+});
