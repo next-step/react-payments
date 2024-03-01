c8e0ef531400e4a1c5a4b65d04824c0c70e14c0c
gywjd <72644229+gywjd@users.noreply.github.com>
2024-03-01 15:01:31 +0900

    feat: 초기 화면 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import '../styles/index.css';
+
 function App() {
-  return <>App</>;
+  return (
+    <div className="root">
+      <div className="app">App</div>
+    </div>
+  );
 }
 
 export default App;
