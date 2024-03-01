dfc2736c6ecb32ec784b01014c2c2392ad61d6a1
jiyeon <ejy1105@gmail.com>
2024-03-02 04:02:49 +0900

    feat: 날짜 validation ing

--- src/hooks/useDefaultInput.ts
+++ src/hooks/useDefaultInput.ts
@@ -1,9 +1,11 @@
-import { useContext, useEffect, useState } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
 import { UpdateCardInfoContext } from '../context/paymentContext'
 
 const useDefaultInput = () => {
-  const [state, setState] = useState('')
   const updateCardInfo = useContext(UpdateCardInfoContext)
+  const [state, setState] = useState('')
+  const [date, setDate] = useState('')
+
   const handleChange = (param: string) => {
     setState(param)
   }
@@ -12,7 +14,27 @@ const useDefaultInput = () => {
     updateCardInfo({ name: state })
   }, [state])
 
-  return [state, handleChange] as const
+  const matchMonthReg = new RegExp(/(0[1-9]|1[0-2])/g)
+  const matchYearReg = new RegExp(/(2[4-9])/g)
+
+  let month = ''
+  let year = ''
+  const handleValidity = useCallback((text: string) => {
+    // const matchNum = text.match(matchMonthReg)?.join('/') || text
+    // const matchYear = text.match(matchYearReg)?.join('/') || text
+    // if (!month && text.match(matchMonthReg)) {
+    //   month += text.match(matchMonthReg)
+    //   month += '/'
+    //   setDate(month)
+    // } else if (month && text.match(matchYearReg)) {
+    //   year += text.match(matchYearReg)
+    //   setDate(month + matchYear)
+    // } else {
+    //   setDate(matchYear + '')
+    // }
+  }, [])
+
+  return [state, date, handleChange, handleValidity] as const
 }
 
 export default useDefaultInput
