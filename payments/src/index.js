2235e5b743def6a27e5c8a1551809bcf3b55a87d
tjdls111 <tjdls111@naver.com>
2024-03-01 15:53:41 +0900

    feat: 페이지 분리, react router dom 설치

--- payments/src/index.js
+++ payments/src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import CardList from './pages/CardList.tsx';
+import AddCard from './pages/AddCard/AddCard.tsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CardList/>,
+  },
+  {
+    path:'/create',
+    element:<AddCard/>
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} >
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 );
 
