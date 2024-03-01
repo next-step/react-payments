50832f9177fad5de96b7acaf725caa6e3345478e
jiyeon <ejy1105@gmail.com>
2024-03-02 03:59:05 +0900

    fix: 주석 제거

--- src/hooks/useMasking.ts
+++ src/hooks/useMasking.ts
@@ -1,21 +1,18 @@
-import { useCallback, useContext, useEffect, useState } from 'react'
-import { TInputProps } from '../components/common/MaskingPartInput'
-import { CardInfoContext, UpdateCardInfoContext } from '../context/paymentContext'
+import { useCallback, useState } from 'react'
+import { TInputProps } from '../components'
 
 const useMasking = (props: Partial<TInputProps>) => {
   const [masking, setMasking] = useState<Array<String>>([])
   const [originValue, setOriginValue] = useState('')
-  // const updateCardInfo = useContext(UpdateCardInfoContext)
 
   let maskingVal = ''
   const regx = new RegExp(`[0-9\*]{1,${props.number}}`, 'g')
 
   const checkPartMasking = useCallback((text: string) => {
-    // if (props.totalText && props.totalText < text.length) return
-    const matchNum = text.match(regx)?.join('-') || text
+    if (!text.match(regx)) return
 
+    const matchNum = text.match(regx)?.join('-') || text
     const splitNum = matchNum.split('-')
-    //cvc
 
     const masking = splitNum.map((value, idx) => {
       if (props.divider && props.divider <= idx) {
@@ -26,13 +23,11 @@ const useMasking = (props: Partial<TInputProps>) => {
       return value
     })
     setMasking(masking)
-
-    console.log('3. masking', masking)
   }, [])
 
   //cvc,password
   const checkAllMasking = (text: string) => {
-    // if (props.totalText && props.totalText < text.length) return
+    if (!text.match(regx)) return
     const masking = [text].map((value, idx) => {
       return (maskingVal = value.replace(value, '*'.repeat(value.length)))
     })
