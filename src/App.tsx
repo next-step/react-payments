baef1c6a749e61aac1403dc98f50260caceb6895
Pengoose <pengoose_dev@naver.com>
2024-03-01 14:43:04 +0900

    chore: 불필요한 CSS 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Payments } from '@/pages/Payments/Payments';
-import '../assets/mission/index.css'; // FIXME: 추후 컴포넌트 분리 후 삭제
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@/styles/index.css';
 import App from './App.tsx';
-import '@/styles/index.module.css';
 import { GlobalLayout } from './layout/global/Layout.tsx';
 import { LAYOUT } from './layout/global/layout.constant.ts';
 
