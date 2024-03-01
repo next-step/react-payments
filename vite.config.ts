c5461e5793831ef94ed022d7f60c09a6496d7671
sasha1107 <sasha1107@naver.com>
2024-03-02 03:58:53 +0900

    chore: svgr 셋팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin(), svgr()],
   base: '/react-payments/',
   resolve: {
     alias: {
