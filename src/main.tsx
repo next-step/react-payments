59af1f501a1ff806a25b9e0bde04dafd438afbac
sasha1107 <sasha1107@naver.com>
2024-03-02 00:27:07 +0900

    chore: vanilla extract css setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from '@/router';
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   base: '/react-payments/',
   resolve: {
     alias: {
