8ddfca3af719037f0785b49c7fcb785584ad72a9
lomen-SeoJaeWan <sjw7324@lomen.kr>
2024-03-01 22:55:42 +0900

    feat: Box 코드 작성

--- src/components/atoms/box/index.jsx
+++ src/components/atoms/box/index.jsx
@@ -1,5 +1,21 @@
-const Box = () => {
-  return <></>;
+import { createElement } from "react";
+import PropTypes from "prop-types";
+
+const Box = (props) => {
+  const { className, as, children } = props;
+
+  const classes = Array.isArray(className) ? className.join(" ") : className;
+
+  return createElement(as, { className: classes }, children);
+};
+
+Box.propTypes = {
+  as: PropTypes.oneOf(["div", "label"]),
+  className: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]),
+  children: PropTypes.node,
 };
 
 export default Box;
