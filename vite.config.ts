dcbb94e0c7f51c3d00eb8f093e3d2b708912fdb6
Yangseungchan <uvzone@naver.com>
2024-03-01 14:07:48 +0900

    feat: storybook, vanilla-extract 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
   test: {
     globals: true,
     setupFiles: ['./src/tests/setup-tests.ts'],
