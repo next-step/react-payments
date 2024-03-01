9e1e712e6d9e617bfe8dfcf946a5cdb94e304d41
Yangseungchan <uvzone@naver.com>
2024-03-01 17:57:14 +0900

    chore: story 공통 arg type 정의

---
+++ src/stories/arg-types.ts
@@ -0,0 +1,31 @@
+export const layoutArgTypes = [
+  'margin',
+  'marginX',
+  'marginY',
+  'marginLeft',
+  'marginRight',
+  'marginTop',
+  'marginBottom',
+  'padding',
+  'paddingX',
+  'paddingY',
+  'paddingLeft',
+  'paddingRight',
+  'paddingTop',
+  'paddingBottom',
+  'width',
+  'height',
+  'minWidth',
+  'maxWidth',
+  'minHeight',
+  'maxHeight',
+].reduce((acc, prop) => {
+  return {
+    ...acc,
+    [prop]: {
+      table: {
+        category: 'Layout Props',
+      },
+    },
+  }
+}, {})
