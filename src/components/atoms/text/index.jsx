ef9869f8969c56dae143fb325d711107d1b789e4
lomen-SeoJaeWan <sjw7324@lomen.kr>
2024-03-01 23:08:40 +0900

    feat: Text 코드 작성

---
+++ src/components/atoms/text/index.jsx
@@ -0,0 +1,27 @@
+import { createElement } from "react";
+import PropTypes from "prop-types";
+
+const Text = (props) => {
+  const { as = "p", className, children } = props;
+
+  const classes = Array.isArray(className) ? className.join(" ") : className;
+
+  return createElement(
+    as,
+    {
+      className: classes,
+    },
+    children
+  );
+};
+
+Text.propTypes = {
+  as: PropTypes.oneOf(["p", "span", "h1", "h2", "h3", "h4", "h5", "h6"]),
+  className: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]),
+  children: PropTypes.node,
+};
+
+export default Text;
