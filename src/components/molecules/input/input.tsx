a9b2f4ffa39607987ed69b455ebee9ba62f05525
Yangseungchan <uvzone@naver.com>
2024-03-02 15:15:53 +0900

    feat: Input 컴포넌트 추가

---
+++ src/components/molecules/input/input.tsx
@@ -0,0 +1,49 @@
+import { forwardRef } from 'react'
+import { Flex, FlexElements, FlexProps } from '@/components/atom/flex'
+import { Box, BoxProps } from '@/components/atom/box'
+import { Text, TextProps } from '@/components/atom/text'
+import { PolymorphicRef } from '@/types'
+
+/* -------------------------------------------------------------------------------------------------
+ * InputLabel
+ * -----------------------------------------------------------------------------------------------*/
+
+export type InputLabelProps = Omit<TextProps<'label'>, 'ref'>
+export const InputLabel = forwardRef((props: InputLabelProps, ref: PolymorphicRef<'label'>) => {
+  return <Text as="label" ref={ref} {...props} />
+})
+
+/* -------------------------------------------------------------------------------------------------
+ * InputWrapper
+ * -----------------------------------------------------------------------------------------------*/
+
+export type InputWrapperProps<C extends FlexElements> = Omit<FlexProps<C>, 'ref'>
+
+export const InputWrapper = forwardRef(
+  <C extends FlexElements = 'div'>(
+    { children, ...rest }: InputWrapperProps<C>,
+    ref: PolymorphicRef<C>,
+  ) => {
+    return (
+      <Flex direction="column" ref={ref} {...rest}>
+        {children}
+      </Flex>
+    )
+  },
+)
+
+/* -------------------------------------------------------------------------------------------------
+ * InputContent
+ * -----------------------------------------------------------------------------------------------*/
+
+export type InputContentProps = Omit<BoxProps<'input'>, 'ref'>
+
+export const InputContent = forwardRef((props: InputContentProps, ref: PolymorphicRef<'input'>) => {
+  return <Box as="input" ref={ref} {...props} />
+})
+
+export const Input = {
+  Label: InputLabel,
+  Wrapper: InputWrapper,
+  Content: InputContent,
+}
