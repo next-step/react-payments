c0adfe2d489e188068c695cd6a3ef17557ec79c7
saewookkang <pjy9665@kakao.com>
2024-03-02 20:53:24 +0900

    feat(Input::stroies): stroy 추가 및 클릭 테스트 작성

--- src/components/primitive/Input/index.stories.tsx
+++ src/components/primitive/Input/index.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { expect, fn, userEvent, within } from '@storybook/test'
 
 import { Input } from '.'
 
@@ -6,10 +7,38 @@ const meta = {
   title: 'Primitive/Input',
   component: Input,
   tags: ['autodocs'],
+  args: {
+    onClick: fn(),
+  },
 } satisfies Meta<typeof Input>
 
 type Story = StoryObj<typeof meta>
 
 export const Basic: Story = {}
+
+export const WithText: Story = {
+  args: {
+    value: '텍스트 입니다.',
+  },
+}
+
+export const PlaceHolder: Story = {
+  args: {
+    placeholder: 'placeholder 입니다.',
+  },
+}
+
+export const ClickTest: Story = {
+  args: {
+    role: 'text',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    await userEvent.click(canvas.getByRole('text'))
+
+    await expect(canvas.getByRole('text')).toHaveFocus()
+  },
+}
 
 export default meta
