6342431c390e96adec7f0e05d1a29fb58f984454
Yangseungchan <uvzone@naver.com>
2024-03-02 22:38:41 +0900

    refactor: input-error 공통 요소 추가

--- src/components/molecule/input/input.tsx
+++ src/components/molecule/input/input.tsx
@@ -1,8 +1,9 @@
-import { forwardRef } from 'react'
+import { forwardRef, ReactElement } from 'react'
 import { Flex, FlexElements, FlexProps } from '@/components/atom/flex'
 import { Box, BoxProps } from '@/components/atom/box'
 import { Text, TextProps } from '@/components/atom/text'
-import { PolymorphicRef } from '@/types'
+import { LayoutProps, PolymorphicRef } from '@/types'
+import { isNil } from '@/utils'
 
 /* -------------------------------------------------------------------------------------------------
  * InputLabel
@@ -12,6 +13,29 @@ export type InputLabelProps = Omit<TextProps<'label'>, 'ref'>
 export const InputLabel = forwardRef((props: InputLabelProps, ref: PolymorphicRef<'label'>) => {
   return <Text as="label" ref={ref} {...props} />
 })
+
+/* -------------------------------------------------------------------------------------------------
+ * InputError
+ * -----------------------------------------------------------------------------------------------*/
+
+export interface InputErrorProps extends LayoutProps {
+  /** 표시하고자 하는 에러 메시지  */
+  error?: string
+  /** 에러 요소 커스텀 렌더 함수 */
+  errorRender?: (error: string) => ReactElement
+}
+
+export const InputError = ({ error, errorRender, ...layoutProps }: InputErrorProps) => {
+  const isCustomError = !isNil(error) && !isNil(errorRender)
+
+  return isCustomError ? (
+    errorRender?.(error as string)
+  ) : (
+    <Text variant="caption1" color="red" {...layoutProps}>
+      {error}
+    </Text>
+  )
+}
 
 /* -------------------------------------------------------------------------------------------------
  * InputWrapper
@@ -49,4 +73,5 @@ export const Input = {
   Label: InputLabel,
   Wrapper: InputWrapper,
   Content: InputContent,
+  Error: InputError,
 }
