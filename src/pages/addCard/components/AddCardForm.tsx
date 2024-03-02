0b6fd1878ffbfd42b2ddece7def0f3d128718f88
gaeun <knospe1@naver.com>
2024-03-02 23:55:38 +0900

    feat: add SecurityCodeInput

--- src/pages/addCard/components/AddCardForm.tsx
+++ src/pages/addCard/components/AddCardForm.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import CardNumberInput from "./CardNumberInput";
 import ExpiredDateInput from "./ExpiredDateInput";
+import SecurityCodeInput from "./SecurityCodeInput";
 
 export default function AddCardForm() {
   const [cardNumber, setCardNumber] = useState<string>("");
   const [expiredDate, setExpiredDate] = useState<string>("");
+  const securityCodeRef = useRef<HTMLInputElement>(null);
 
   return (
     <form>
@@ -13,6 +15,15 @@ export default function AddCardForm() {
         expiredDate={expiredDate}
         setExpiredDate={setExpiredDate}
       />
+      <SecurityCodeInput securityCodeRef={securityCodeRef} />
+      <button
+        type="button"
+        onClick={() => {
+          console.log("securityCodeRef", securityCodeRef.current?.value);
+        }}
+      >
+        버튼
+      </button>
     </form>
   );
 }
