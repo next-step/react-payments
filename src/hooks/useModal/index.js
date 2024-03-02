33b55473506e847e8e5509438c39e9ca723bc603
lomen-SeoJaeWan <sjw7324@lomen.kr>
2024-03-02 15:37:40 +0900

    feat: useModal 코드 작성

--- src/hooks/useModal/index.js
+++ src/hooks/useModal/index.js
@@ -1,3 +1,17 @@
-const useModal = () => {};
+import { cloneElement, useState } from "react";
+
+const useModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
+  const Modal = ({ children }) => {
+    return isModalOpen && cloneElement(children, { toggleModal });
+  };
+
+  return { Modal, toggleModal };
+};
 
 export default useModal;
--- src/hooks/useModal/useModal.test.jsx
+++ src/hooks/useModal/useModal.test.jsx
@@ -1,19 +1,41 @@
-import { act, render, renderHook, waitFor } from "@testing-library/react";
+import {
+  act,
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import { describe, expect, it } from "vitest";
+import PropTypes from "prop-types";
 import useModal from ".";
+
+const ChildModal = (props) => {
+  const { toggleModal } = props;
+
+  return (
+    <div>
+      <button onClick={toggleModal}>닫기</button>
+    </div>
+  );
+};
+
+ChildModal.propTypes = {
+  toggleModal: PropTypes.func,
+};
 
 describe("useModal 테스트", () => {
   it("Modal 표시", () => {
     const { result: Result } = renderHook(() => useModal());
 
     render(
-      <Result.Modal>
+      <Result.current.Modal>
         <div>모달</div>
-      </Result.Modal>
+      </Result.current.Modal>
     );
 
-    const text = screen.getByText("모달");
-    expect(!text).toBeTruthy();
+    const textBeforeToggle = screen.queryByText("모달");
+    expect(textBeforeToggle).toBeNull();
 
     act(() => {
       Result.current.toggleModal();
@@ -22,6 +44,30 @@ describe("useModal 테스트", () => {
     waitFor(() => {
       const text = screen.getByText("모달");
       expect(text).toBeTruthy();
+    });
+  });
+
+  it("Modal toggle 전달 확인", () => {
+    const { result: Result } = renderHook(() => useModal());
+
+    render(
+      <Result.current.Modal>
+        <ChildModal />
+      </Result.current.Modal>
+    );
+
+    act(() => {
+      Result.current.toggleModal();
+    });
+
+    waitFor(() => {
+      const text = screen.getByText("닫기");
+      fireEvent.click(text);
+    });
+
+    waitFor(() => {
+      const textAfterToggle = screen.queryByText("닫기");
+      expect(textAfterToggle).toBeNull();
     });
   });
 });
