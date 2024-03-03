1516cf23aae21095bafc1eff2993a5bd90cd15f7
saewookkang <pjy9665@kakao.com>
2024-03-03 19:37:55 +0900

    design(CardPassword.tsx): 카드 비밀번호 UI 피그마에 맞게 수정

--- src/pages/payments/cards/new/components/Step1/Components/CardPassword/index.tsx
+++ src/pages/payments/cards/new/components/Step1/Components/CardPassword/index.tsx
@@ -2,10 +2,23 @@ export const CardPassword = () => {
   return (
     <div className="input-container">
       <span className="input-title">카드 비밀번호</span>
-      <input className="input-basic w-15" type="password" />
-      <input className="input-basic w-15" type="password" />
-      <input className="input-basic w-15" type="password" />
-      <input className="input-basic w-15" type="password" />
+
+      <div style={{ display: 'grid', gridTemplateColumns: '43px 43px 43px 43px', gap: '10px' }}>
+        <input className="input-basic w-15" type="password" />
+        <input className="input-basic w-15" type="password" />
+        <input
+          value={1}
+          className="input-basic w-15"
+          type="password"
+          style={{ backgroundColor: 'transparent' }}
+        />
+        <input
+          value={1}
+          className="input-basic w-15"
+          type="password"
+          style={{ backgroundColor: 'transparent' }}
+        />
+      </div>
     </div>
   )
 }
