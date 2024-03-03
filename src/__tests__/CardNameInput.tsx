cabee3533242ce0ed55fd44c45c2b1008db443f9
Gn0lee <jinho605@gmail.com>
2024-03-03 22:34:21 +0900

    test : 카드 소유자 입력 컴포넌트 테스트 코드 추가

---
+++ src/__tests__/CardNameInput.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import CardOwnerNameInput from 'src/components/CardOwnerNameInput.tsx';
+import useCardOwnerNameInput from 'src/hooks/useCardOwnerNameInput.ts';
+
+export function TestContainer() {
+	const { handleCardOwnerNameChange, cardOwnerName, maxLength } = useCardOwnerNameInput();
+
+	return (
+		<CardOwnerNameInput
+			value={cardOwnerName}
+			onChange={handleCardOwnerNameChange}
+			maxLength={maxLength}
+			id="card-owener-name"
+			aria-label="card-owener--name"
+		/>
+	);
+}
+
+const setup = () => {
+	const utils = render(<TestContainer />);
+
+	const cardOwnerNameInput = screen.getByLabelText<HTMLInputElement>('card-name', { selector: 'input' });
+
+	return {
+		cardOwnerNameInput,
+		...utils,
+	};
+};
+
+describe('카드 소유자 입력', () => {
+	it('"카드에 표시된 이름과 동일하게 입력하세요." 라는 placeholder가 표시된다.', async () => {
+		const { cardOwnerNameInput } = setup();
+
+		expect(cardOwnerNameInput.placeholder).toBe('카드에 표시된 이름과 동일하게 입력하세요.');
+	});
+
+	it('최대 입력 길이 보다 긴 입력값은 무시된다.', async () => {
+		const { cardOwnerNameInput } = setup();
+
+		await userEvent.type(cardOwnerNameInput, '1'.repeat(cardOwnerNameInput.maxLength + 3));
+
+		expect(cardOwnerNameInput.value).toBe('1'.repeat(cardOwnerNameInput.maxLength));
+	});
+
+	it('입력한 값의 길이가 상단 라벨에 표시된다.', async () => {
+		const { cardOwnerNameInput } = setup();
+
+		await userEvent.type(cardOwnerNameInput, '1234');
+
+		const label = await screen.findByText(`${cardOwnerNameInput.value.length}/${cardOwnerNameInput.maxLength}`);
+
+		expect(label).not.toBeNull();
+	});
+});
