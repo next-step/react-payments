b4155c6d2624844a045bc8811a57854ec1ff34eb
sonsurim <surim014@naver.com>
2024-03-03 13:23:23 +0900

    style: apply the emotion css

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import {
+  resetCSS,
+  globalCSS,
+  utilCSS,
+  buttonCSS,
+  cardCSS,
+  inputCSS,
+  modalCSS
+} from '@/styles'
+import { css } from '@emotion/css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div
+      className={css([
+        resetCSS,
+        globalCSS,
+        utilCSS,
+        buttonCSS,
+        cardCSS,
+        inputCSS,
+        modalCSS
+      ])}>
+      <App />
+    </div>
   </React.StrictMode>
 )
