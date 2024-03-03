5b6539b0cd60315e3d0e67f3bb223eb97923cbd3
saewookkang <pjy9665@kakao.com>
2024-03-03 17:22:24 +0900

    refactor(InputWithMask.tsx): onChange props 호환성 문제로 타입 변경

--- src/components/InputWithMask/index.tsx
+++ src/components/InputWithMask/index.tsx
@@ -5,25 +5,24 @@ import { ChangeEvent, ComponentPropsWithoutRef, forwardRef, useState } from 'rea
 import { Input } from '../Input'
 import { applyMask, getOnlyNumbers } from './index.utils'
 
-interface InputWithMaskProps extends Omit<ComponentPropsWithoutRef<'input'>, 'value' | 'onChange'> {
+interface InputWithMaskProps extends ComponentPropsWithoutRef<'input'> {
   /** 입력 받을 숫자를 0으로 작성해주세요. */
   mask: string
-  onChange?: (v: string) => void
 }
 
-export const InputWithMask = forwardRef<HTMLInputElement, InputWithMaskProps>((props, ref) => {
-  const [value, setValue] = useState('')
+export const InputWithMask = forwardRef<HTMLInputElement, InputWithMaskProps>(
+  ({ mask, onChange, ...props }, ref) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value
+      const numbers = getOnlyNumbers(newValue)
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newValue = e.target.value
-    const numbers = getOnlyNumbers(newValue)
+      const maskedValue = applyMask(mask, numbers)
 
-    const maskedValue = applyMask(props.mask, numbers)
+      e.target.value = maskedValue
 
-    setValue(maskedValue)
+      onChange?.(e)
+    }
 
-    props.onChange?.(maskedValue)
+    return <Input onChange={handleChange} ref={ref} {...props} />
   }
-
-  return <Input placeholder={props.placeholder} value={value} onChange={handleChange} ref={ref} />
-})
+)
