a072fa0397fe9389344a758a0b7f10a79b9c2db3
Pengoose <pengoose_dev@naver.com>
2024-03-03 11:42:22 +0900

    chore: 불필요한 추상화 제거

--- src/hooks/useInputFields.ts
+++ src/hooks/useInputFields.ts
@@ -17,7 +17,6 @@ interface FieldConfig {
   fieldType: InputFieldType;
   maxLength?: number;
   validation?: (fields: string) => boolean;
-  onFieldChangeCallback?: (fields: string[]) => void;
 }
 
 export const useInputFields = (fieldConfigs: FieldConfig[]) => {
@@ -35,6 +34,7 @@ export const useInputFields = (fieldConfigs: FieldConfig[]) => {
     );
   }, [fieldAmount]);
 
+  // FIXME: 값을 평가하고 특정 조건이 아니라면 입력을 막아내는 외부로 분리해낼 수 있을듯. validation과 다르게 생각해야 함.
   const getRegex = (inputType: InputFieldType) => {
     return inputType === INPUT_FIELDS.NUMBER.TYPE
       ? INPUT_FIELDS.NUMBER.REGEX
@@ -46,14 +46,14 @@ export const useInputFields = (fieldConfigs: FieldConfig[]) => {
     index: number
   ) => {
     const newFields = [...fields];
-    const { maxLength, fieldType, onFieldChangeCallback, validation } =
-      fieldConfigs[index];
+    const { maxLength, fieldType, validation } = fieldConfigs[index];
+
+    // FIXME: InputGurad : validation과 달리 값 평가 후 true인 경우에만 state를 변경하는 로직으로 바꾸면 더 좋을듯
     const changedValue = target.value.replace(getRegex(fieldType), '');
 
     if (!maxLength || (maxLength && changedValue.length <= maxLength)) {
       newFields[index] = changedValue;
       setFields(newFields);
-      onFieldChangeCallback?.(newFields);
     }
 
     if (validation && validation(changedValue) && index < fieldAmount - 1) {
