72ac6d6caf61ddf949854a9ba1a1e9989ff117e5
bytrustu <bytrustu@gmail.com>
2024-03-03 23:26:17 +0900

    feat: Funnel 기반 페이지 이동되도록 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { AppLayout } from 'src/components/AppLayout';
+import { Funnel } from '@/components';
+import { CardListPage, CardAddPage, CardCompletePage } from '@/pages';
 
 const App = () => (
-  <AppLayout>
-    <div>Hello, React Payments</div>
-  </AppLayout>
+  <Funnel.Root>
+    <Funnel.Step index={0}>
+      <CardListPage />
+    </Funnel.Step>
+    <Funnel.Step index={1}>
+      <CardAddPage />
+    </Funnel.Step>
+    <Funnel.Step index={2}>
+      <CardCompletePage />
+    </Funnel.Step>
+  </Funnel.Root>
 );
-
 export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './CardAddPage';
+export * from './CardCompletePage';
+export * from './CardListPage';
