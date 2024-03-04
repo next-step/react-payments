fa203c011bfc0484c3a569e5bd3aa092dba10f2d
ww8007 <wshmin1234@gmail.com>
2024-03-05 01:17:57 +0900

    feat: GlobalStyle 및 IconLoader 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "../styles/index.css";
 import App from "./App.tsx";
+import GlobalStyle from "./common/ui/GlobalStyle.tsx";
+import IconLoader from "./common/ui/assets/IconLoader.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
+		<GlobalStyle />
+		<IconLoader />
 		<App />
 	</React.StrictMode>
 );
