7dfde1bb011d92d7163d243b95e77b90948fe018
ingdaeho <ingdaeho@swingby.us>
2024-03-04 21:13:44 +0900

    feat: localStorage

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,18 @@
+export const getLocalStorageItem = ({ key }: { key: string }) => {
+  const storedData = localStorage.getItem(key);
+  if (!storedData) return [];
+
+  return JSON.parse(storedData);
+};
+
+export const setLocalStorageItem = ({
+  key,
+  item,
+}: {
+  key: string;
+  item: unknown;
+}): void => {
+  const data = JSON.stringify(item);
+
+  localStorage.setItem(key, data);
+};
