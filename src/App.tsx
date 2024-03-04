fe7d8e6d942813c5691f24040d9f08117fe5423a
jaehvvan <zeze@linercorp.com>
2024-03-04 22:40:10 +0900

    [feature] routes 전역 변수 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { CardInventoryPage } from './pages/card-inventory';
 import { AddCompletedPage } from './pages/add-completed';
 import { AddCardPage } from './pages/add-card';
+import { PATH } from './constants/routes';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CardInventoryPage />} />
-        <Route path="/card-inventory" element={<CardInventoryPage />} />
-        <Route path="/add-card" element={<AddCardPage />} />
-        <Route path="/add-completed" element={<AddCompletedPage />} />
+        <Route path={PATH.root} element={<CardInventoryPage />} />
+        <Route path={PATH.cardInventory} element={<CardInventoryPage />} />
+        <Route path={PATH.addCard} element={<AddCardPage />} />
+        <Route path={PATH.addCompleted} element={<AddCompletedPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/constants/routes.ts
@@ -0,0 +1,6 @@
+export const PATH = {
+  root: '/',
+  cardInventory: '/card-inventory',
+  addCard: '/add-card',
+  addCompleted: '/add-completed',
+};
