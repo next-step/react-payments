47f7b4ac98fc76579688bc019f796b718e385711
ww8007 <wshmin1234@gmail.com>
2024-03-05 00:45:46 +0900

    feat: Width, TextAlign의 타입과 variable 정보 선언

---
+++ src/common/utils/style.ts
@@ -0,0 +1,7 @@
+export const widthVariable = [100, 75, 50, 25, 15] as const;
+
+export type Width = (typeof widthVariable)[number];
+
+export const textAlignVariable = ["left", "center", "right"] as const;
+
+export type TextAlign = (typeof textAlignVariable)[number];
