5dfbf4cc679c33d0df5efb25423b701b678bdacf
ww8007 <wshmin1234@gmail.com>
2024-03-05 00:57:48 +0900

    feat: svg 아이콘을 사용하기 위한 Icon 및 IconLoader 추가

---
+++ src/common/ui/assets/IconLoader.tsx
@@ -0,0 +1,21 @@
+const IconLoader = () => (
+	<svg
+		id='sprite-svgs'
+		version='1.1'
+		xmlns='http://www.w3.org/2000/svg'
+		xmlnsXlink='http://www.w3.org/1999/xlink'
+		style={{ display: "none" }}
+		data-testid='icon'
+	>
+		<defs id='defs'>
+			<symbol id='arrow-left' fill='none' viewBox='0 0 12 20'>
+				<path
+					fill='#000'
+					d='M11.67 1.87 9.9.1 0 10l9.9 9.9 1.77-1.77L3.54 10l8.13-8.13Z'
+				/>
+			</symbol>
+		</defs>
+	</svg>
+);
+
+export default IconLoader;
