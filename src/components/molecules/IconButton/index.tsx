4786731143beaa8e904af3781aa17e1f8e93ff2b
Jay Lee <roof2586@naver.com>
2024-03-04 20:34:30 +0900

    feat: pass html button type as rest props

--- src/components/molecules/IconButton/index.tsx
+++ src/components/molecules/IconButton/index.tsx
@@ -8,9 +8,17 @@ interface IconButtonProps extends ComponentProps<'button'> {
   iconProps?: ComponentProps<'img'>;
 }
 
-export default function IconButton({ onClick, className, size, src, alt = '', iconProps }: IconButtonProps) {
+export default function IconButton({
+  onClick,
+  className,
+  size,
+  src,
+  alt = '',
+  iconProps,
+  ...restProps
+}: IconButtonProps) {
   return (
-    <button type="button" onClick={onClick} className={className} aria-label={alt}>
+    <button type="button" onClick={onClick} className={className} aria-label={alt} {...restProps}>
       <Icon src={src} size={size} {...iconProps} />
     </button>
   );
