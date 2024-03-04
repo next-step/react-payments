ca68c962ecdd2e5160363c4246e483f59a93c27f
ww8007 <wshmin1234@gmail.com>
2024-03-05 01:14:17 +0900

    setting: emotion 및 alias 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react({
+			jsxImportSource: "@emotion/react",
+			babel: {
+				plugins: ["@emotion/babel-plugin"]
+			}
+		})
+	],
+	resolve: {
+		alias: [{ find: "@", replacement: "/src" }]
+	}
+});
