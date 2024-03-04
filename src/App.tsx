d8ae0923fc6e403f97a4d685d7dadaf25047b476
ww8007 <wshmin1234@gmail.com>
2024-03-05 01:19:59 +0900

    feat: Router 추가 및 CardFunnel 렌더링 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import navigationPath from "./common/navigation/navigationPath";
+import CardFunnel from "./pages/CardFunnel";
+
 function App() {
-	return <>Payments</>;
+	return (
+		<div id='app'>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path={navigationPath.ADD_CARD_FUNNEL["CARD_LIST"]}
+						element={<CardFunnel />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
