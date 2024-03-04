a880e85f1f54c5bf7104ce25bb4bedaa8e96521a
Jay Lee <roof2586@naver.com>
2024-03-04 19:36:17 +0900

    refactor: remove unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<CreateCardForm />} />
-        <Route path="/credit-card" element={<CreateCardForm />} />
-        <Route path="/credit-card/create" element={<CreateCardForm />} />
       </Routes>
     </Router>
   );
