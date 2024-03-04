19b2264ca591d62ae5442cdcb8cdd0f80b33b246
ww8007 <wshmin1234@gmail.com>
2024-03-05 00:55:29 +0900

    feat: 합성 컴포넌트 형식의 FlexibleInput 컴포넌트 추가

---
+++ src/common/ui/FlexibleInput/FlexibleInput.tsx
@@ -0,0 +1,59 @@
+import styled from "@emotion/styled";
+import { ReactNode } from "react";
+import { Width } from "../../utils";
+import {
+	findComponentFromChildren,
+	findRestFromChildren
+} from "../../utils/component";
+import Count from "./core/Count";
+import Input from "./core/Input";
+import InputBox from "./core/InputBox";
+import Separator from "./core/Separator";
+import Title from "./core/Title";
+import { FlexibleInputProvider } from "./provider/FlexibleInputProvider";
+
+export interface FlexibleInputProps {
+	children: ReactNode;
+	width?: Width;
+}
+
+const FlexibleInput = ({ children, width = 100 }: FlexibleInputProps) => {
+	const titleComponent = findComponentFromChildren(children, Title);
+	const countComponent = findComponentFromChildren(children, Count);
+	const restComponents = findRestFromChildren(children, [Title, Count]);
+
+	return (
+		<FlexibleInputProvider>
+			<InputContainer width={width}>
+				<InputTop>
+					{titleComponent}
+					{countComponent}
+				</InputTop>
+				{restComponents}
+			</InputContainer>
+		</FlexibleInputProvider>
+	);
+};
+
+FlexibleInput.Title = Title;
+FlexibleInput.Count = Count;
+FlexibleInput.InputBox = InputBox;
+FlexibleInput.Input = Input;
+FlexibleInput.Separator = Separator;
+
+export default FlexibleInput;
+
+interface InputContainerProps {
+	width: Width;
+}
+
+const InputContainer = styled.label<InputContainerProps>`
+	margin: 8px 0;
+	width: ${({ width }) => width}%;
+`;
+
+const InputTop = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
