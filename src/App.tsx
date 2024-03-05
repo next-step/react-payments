adb4fbf35bbe468d62e4505b49fdcbdac2fbc1db
Han-D-Peter <secret_wings@naver.com>
2024-03-05 19:29:50 +0900

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import MyCards from "./domains/MyCards/MyCards";
 import CardRegister from "./domains/cardRegister/CardRegister";
 import CardResult from "./domains/CardResult/CardResult";
@@ -7,6 +7,14 @@ function App() {
   return (
     <div>
       <Routes>
+        <Route
+          index={true}
+          element={
+            <Link to="/mycards">
+              <button>my cards</button>
+            </Link>
+          }
+        ></Route>
         <Route path="mycards">
           <Route index element={<MyCards />} />
           <Route path="register">
