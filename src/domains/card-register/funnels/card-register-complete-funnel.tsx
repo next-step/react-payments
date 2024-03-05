f1a7e315956355d3266dff81b5054fc59a2368b7
Yangseungchan <uvzone@naver.com>
2024-03-05 09:11:52 +0900

    feat: 카드 목록 페이지 구성 완료

--- src/domains/card-register/funnels/card-register-complete-funnel.tsx
+++ src/domains/card-register/funnels/card-register-complete-funnel.tsx
@@ -1,5 +1,25 @@
-import { Text } from '@/components'
+import { Card, Flex, Text, UnderlineInput } from '@/components'
+import { useCardInputContext } from '@/domains/card-register/contexts/card-input-context'
+import { useNavigate } from 'react-router-dom'
 
 export const CardRegisterCompleteFunnel = () => {
-  return <Text variant="title1">카드등록이 완료되었습니다.</Text>
+  const { cardInput } = useCardInputContext()
+  const navigate = useNavigate()
+  return (
+    <Flex width="100%" height="100vh" direction="column">
+      <Flex direction="column" justifyContent="center" alignItems="center" marginTop="48px">
+        <Text as="h1" variant="title1" marginBottom="48px">
+          카드등록이 완료되었습니다.
+        </Text>
+        <Card cardSize="lg" {...cardInput} marginBottom="16px" />
+        <UnderlineInput width="240px" placeholder="카드 별명" contentAlign="center" />
+      </Flex>
+
+      <Flex justifyContent="flex-end" paddingX="24px" paddingY="32px" marginTop="auto">
+        <Text as="button" variant="body1" color="aqua" onClick={() => navigate('/card-list')}>
+          확인
+        </Text>
+      </Flex>
+    </Flex>
+  )
 }
