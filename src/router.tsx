ef0ce07e0be1a27981918a9517e7b935669a28f7
saewookkang <pjy9665@kakao.com>
2024-03-06 23:30:45 +0900

    chore: 카드 추가 페이지 폴더명 변경(기존 new는 예약어)

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { Home } from './pages/home/Home'
 import { CardsPage } from './pages/payments/cards'
-import { AddingCard } from './pages/payments/cards/new'
+import { AddingCard } from './pages/payments/cards/registration'
 
 export const router = createBrowserRouter([
   {
