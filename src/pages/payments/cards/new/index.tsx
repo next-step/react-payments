8c4ded12aeea564524d0f84693fe48ba82eae8bb
saewookkang <pjy9665@kakao.com>
2024-03-06 23:10:58 +0900

    chore: 변수명 구체화

--- src/pages/payments/cards/new/index.tsx
+++ src/pages/payments/cards/new/index.tsx
@@ -9,19 +9,19 @@ import { Step1 } from './components/Step1'
 import { Step1Validate } from './service/validations'
 
 export const AddingCard = () => {
-  const { Funnel, changeStep } = useFunnel<'a' | 'b'>('a')
+  const { Funnel, changeStep } = useFunnel<'add-card' | 'complete-card'>('add-card')
 
   const stepValue = useRef<{ step1: FormType }>({ step1: formInitialValues })
 
   return (
     <Funnel>
-      <Funnel.Step name="a">
+      <Funnel.Step name="add-card">
         <Form
           initialValues={stepValue.current.step1}
           onSubmit={(values) => {
             stepValue.current.step1 = values
 
-            changeStep('b')
+            changeStep('complete-card')
           }}
           validate={Step1Validate}
         >
@@ -29,8 +29,8 @@ export const AddingCard = () => {
         </Form>
       </Funnel.Step>
 
-      <Funnel.Step name="b">
-        <button onClick={() => changeStep('a')}> 이전</button>
+      <Funnel.Step name="complete-card">
+        <button onClick={() => changeStep('add-card')}> 이전</button>
         <div>등록 완료 페이지 입니다.</div>
       </Funnel.Step>
     </Funnel>
