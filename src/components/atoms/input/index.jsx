d8fd5919bd9e2819a5783d10f3e3f682f056afc5
lomen-SeoJaeWan <sjw7324@lomen.kr>
2024-03-07 14:54:43 +0900

    refactor: 숫자 입력 수정

--- src/components/atoms/input/index.jsx
+++ src/components/atoms/input/index.jsx
@@ -33,7 +33,7 @@ const Input = (props) => {
 
   const handleKeyPress = (e) => {
     if (type === "number") {
-      if ((e.which !== 8 && e.which !== 0 && e.which < 48) || e.which > 57) {
+      if (e.which < 48 || e.which > 57) {
         e.preventDefault();
       }
     }
