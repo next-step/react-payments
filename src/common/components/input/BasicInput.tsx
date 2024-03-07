d22fbec21b55f4332d11c10f0d5c59615c73a788
bxxmi <bmi6638@gmail.com>
2024-03-07 20:22:44 +0900

    [feat] eslintrc 변경

--- src/common/components/input/BasicInput.tsx
+++ src/common/components/input/BasicInput.tsx
@@ -3,9 +3,18 @@ import { InputHTMLAttributes, forwardRef } from "react";
 interface BasicInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 const BasicInput = forwardRef<HTMLInputElement, BasicInputProps>(
-  ({ type, value, className, onChange, ...props }, ref) => (
-    <input type={type} value={value} className="input-basic" onChange={onChange} ref={ref} {...props} />
+  ({ type, value, onChange, ...props }, ref) => (
+    <input
+      type={type}
+      value={value}
+      className="input-basic"
+      onChange={onChange}
+      ref={ref}
+      {...props}
+    />
   )
 );
+
+BasicInput.displayName = "BasicInput";
 
 export default BasicInput;
