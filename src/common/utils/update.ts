86a9c50f989892b4e831cdc0efc7317fe08895fe
ww8007 <wshmin1234@gmail.com>
2024-03-07 23:55:48 +0900

    refactor: 불변성을 지키며 업데이트를 할 수 있도록 하고 지정된 형식이 아닌 값을 집어 넣을시 에러가 나도록 코드 변경

--- src/common/utils/update.ts
+++ src/common/utils/update.ts
@@ -2,16 +2,38 @@
 export const updateNestedState = <T extends Record<string, any>>(
 	prevState: T,
 	path: string,
-	value: any
+	newValue: any
 ): T => {
 	const keys = path.split(".");
-	const lastKey = keys.pop();
-	const lastObj = keys.reduce<Record<string, any>>(
-		(acc, key) => (acc[key] = acc[key] || {}),
-		prevState
-	);
-	if (lastKey) {
-		lastObj[lastKey] = value;
+	if (keys.length === 0) {
+		throw new Error("path가 제공되지 않았습니다.");
 	}
-	return { ...prevState };
+
+	const updateRecursively = (
+		state: Record<string, any>,
+		keyPath: string[]
+	): Record<string, any> => {
+		const [currentKey, ...restKeys] = keyPath;
+
+		if (!currentKey) {
+			throw new Error("path가 유효하지 않습니다: 빈 key가 있습니다.");
+		}
+
+		if (restKeys.length === 0) {
+			return { ...state, [currentKey]: newValue };
+		}
+
+		if (state[currentKey] !== null && typeof state[currentKey] !== "object") {
+			throw new Error(
+				`path가 유효하지 않습니다: '${currentKey}'에 해당하는 값이 객체가 아닙니다.`
+			);
+		}
+
+		return {
+			...state,
+			[currentKey]: updateRecursively(state[currentKey] ?? {}, restKeys)
+		};
+	};
+
+	return updateRecursively(prevState, keys) as T;
 };
