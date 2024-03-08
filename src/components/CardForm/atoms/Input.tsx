2a7ab117eb78ebd2834b53dfcc0e228e767c84c9
osdoonhyun <skw1996@naver.com>
2024-03-08 21:32:27 +0900

    feat: Input atom FieldProps 추가

--- src/components/CardForm/atoms/Input.tsx
+++ src/components/CardForm/atoms/Input.tsx
@@ -1,6 +1,14 @@
-type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
-  className?: string;
+type FieldProps = {
+  name?: string;
+  value?: unknown;
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
+  FieldProps & {
+    className?: string;
+  };
 
 export default function Input({ className, ...rest }: InputProps) {
   return <input className={`input-basic ${className}`} {...rest} />;
