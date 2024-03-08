0131991d533938cf758c2b53f1acda793d5f0b54
sonsurim <surim014@naver.com>
2024-03-09 00:47:16 +0900

    feat: implement the input component

---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,19 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
+type InputProps = ComponentPropsWithoutRef<'input'> & {
+  theme?: 'basic' | 'underline'
+}
+
+export const Input = forwardRef<ElementRef<'input'>, InputProps>(
+  ({ className, theme = 'basic', ...props }, forwardedRef) => {
+    return (
+      <input
+        type="text"
+        ref={forwardedRef}
+        className={`input-${theme} ${className}`}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = 'Input'
