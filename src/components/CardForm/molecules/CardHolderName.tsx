72db676dd4ffd815e12ec6511ac7254c41fae941
osdoonhyun <skw1996@naver.com>
2024-03-08 21:45:15 +0900

    feat: 카드 소유자 이름 최대 입력 자릿수를 실시간으로 보여주는 기능 추가

--- src/components/CardForm/molecules/CardHolderName.tsx
+++ src/components/CardForm/molecules/CardHolderName.tsx
@@ -3,17 +3,26 @@ import Input from '../atoms/Input';
 import InputContainer from '../atoms/InputContainer';
 import InputTitle from '../atoms/InputTitle';
 import { useFormContext } from '@/context/Form';
+import { CARD_HOLDER_NAEM_MAX_LENGTH } from '@/utils/cardValidations';
 
 export default function CardHolderName() {
-  const { getFieldProps, errors, touched } = useFormContext<FormType>();
+  const {
+    getFieldProps,
+    errors,
+    touched,
+    values: { cardHolderName },
+  } = useFormContext<FormType>();
 
   return (
     <>
       <InputContainer>
-        <InputTitle>카드 소유자 이름(선택)</InputTitle>
+        <div className='input-space-between'>
+          <InputTitle>카드 소유자 이름(선택)</InputTitle>
+          <InputTitle>{`${cardHolderName.length}/${CARD_HOLDER_NAEM_MAX_LENGTH}`}</InputTitle>
+        </div>
         <Input
           type='text'
-          maxLength={30}
+          maxLength={CARD_HOLDER_NAEM_MAX_LENGTH}
           placeholder='카드에 표시된 이름과 동일하게 입력하세요.'
           {...getFieldProps('cardHolderName')}
         />
