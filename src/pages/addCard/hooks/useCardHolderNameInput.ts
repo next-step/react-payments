564a30415d98e5de193986939a89861b33544d06
gaeun <knospe1@naver.com>
2024-03-09 23:00:50 +0900

    refactor: remove useCardHolderNameInput

--- src/pages/addCard/hooks/useCardHolderNameInput.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from "react";
-
-export default function useCardHolderNameInput() {
-  const [cardHolderName, setCardHolderName] = useState("");
-  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setCardHolderName(e.target.value);
-  }, []);
-  return { cardHolderName, handleChange };
-}
