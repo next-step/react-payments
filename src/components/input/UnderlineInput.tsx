0bb38b6db76b9ffc4fe246a5fbd3d08b26930e5f
gaeun <knospe1@naver.com>
2024-03-09 12:37:28 +0900

    refactor: remove UnderlineInput

--- src/components/input/UnderlineInput.tsx
+++
@@ -1,9 +0,0 @@
-import Input, { BaseInputProps } from "./Input";
-
-export default function UnderlineInput({ value, onChange }: BaseInputProps) {
-  return (
-    <div className="input-container">
-      <Input style="underline" value={value} onChange={onChange} />
-    </div>
-  );
-}
