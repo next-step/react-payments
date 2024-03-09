404d3efea18b5fde378eb65be73e9ad55dc31a96
gywjd <72644229+gywjd@users.noreply.github.com>
2024-03-09 12:44:08 +0900

    fix: onClick 마크업 수정

--- src/components/atoms/Text.jsx
+++ src/components/atoms/Text.jsx
@@ -2,8 +2,14 @@ function Text(props) {
   const { className, text, onClick } = props;
 
   return (
-    <span className={className} onClick={onClick}>
-      {text}
+    <span className={className}>
+      {onClick !== undefined ? (
+        <button type="button" className="button-as-link" onClick={onClick}>
+          {text}
+        </button>
+      ) : (
+        text
+      )}
     </span>
   );
 }
--- src/components/atoms/Title.jsx
+++ src/components/atoms/Title.jsx
@@ -2,8 +2,14 @@ function Title(props) {
   const { extraClassName = '', title, onClick } = props;
 
   return (
-    <h2 className={`page-title ${extraClassName}`} onClick={onClick}>
-      {title}
+    <h2 className={`page-title ${extraClassName}`}>
+      {onClick !== undefined ? (
+        <button type="button" className="button-as-link" onClick={onClick}>
+          {title}
+        </button>
+      ) : (
+        title
+      )}
     </h2>
   );
 }
