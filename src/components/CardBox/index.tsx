073d6e443d66349925446c5c50716b614e912cfc
jiyeon <ejy1105@gmail.com>
2024-03-10 01:08:38 +0900

    fix: cardInfo 컨텍스트 정보 가져와서 카드박스에 그려주기

--- src/components/CardBox/index.tsx
+++ src/components/CardBox/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { CardInfoContext } from '../../context/paymentContext'
 import ui from '../../styles/index.module.css'
+import { checkAllMasking } from '../../utils/check'
 
 export const CardBox = () => {
   const cardInfo = useContext(CardInfoContext)
@@ -11,13 +12,17 @@ export const CardBox = () => {
         <div className={ui['card-top']}>{cardInfo.cardType}</div>
         <div className={ui['card-middle']}>
           <div className={ui['small-card__chip']}></div>
-          <p>{cardInfo.cardNumber}</p>
+          <p>{` ${cardInfo.cardNumber?.first ?? ''} ${cardInfo.cardNumber?.second ?? ''} ${cardInfo.cardNumber?.third ? checkAllMasking(cardInfo.cardNumber.third || '', cardInfo.cardNumber.third?.length) : ''} ${
+            cardInfo.cardNumber?.fourth ? checkAllMasking(cardInfo.cardNumber.fourth || '', cardInfo.cardNumber?.fourth.length) : ''
+          }`}</p>
         </div>
         <div className={ui['card-bottom']}>
           <div className={ui['card-bottom__info']}>
             <div>
               <span className={ui['card-text']}>{cardInfo.name || 'NAME'}</span>
-              <span className={ui['card-text']}>MM / YY</span>
+              <span className={ui['card-text']}>
+                {cardInfo.month || 'MM'} / {cardInfo.year || 'YY'}
+              </span>
             </div>
           </div>
         </div>
