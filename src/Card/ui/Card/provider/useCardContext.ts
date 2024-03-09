dfcf5f7657f6ede6a49d51f43508392616d13ef8
ww8007 <wshmin1234@gmail.com>
2024-03-09 22:03:48 +0900

    refactor: useContextHook을 통해 context 사용 내용 추상화

--- src/Card/ui/Card/provider/useCardContext.ts
+++ src/Card/ui/Card/provider/useCardContext.ts
@@ -1,12 +1,8 @@
-import { useContext } from "react";
 import { CardContext } from "./CardProvider";
+import useContextHook from "@/common/hooks/useContextHook";
 
 const useCardContext = () => {
-	const context = useContext(CardContext);
-
-	if (!context)
-		throw new Error("useCardContext는 CardContext 내부에서만 사용 가능합니다.");
-	return context;
+	return useContextHook(CardContext, "CardContext");
 };
 
 export default useCardContext;
--- src/common/ui/FlexibleInput/provider/useFlexibleInputContext.ts
+++ src/common/ui/FlexibleInput/provider/useFlexibleInputContext.ts
@@ -1,14 +1,8 @@
-import { useContext } from "react";
 import { FlexibleInputContext } from "./FlexibleInputProvider";
+import useContextHook from "@/common/hooks/useContextHook";
 
 const useFlexibleInputContext = () => {
-	const context = useContext(FlexibleInputContext);
-
-	if (!context)
-		throw new Error(
-			"useFlexibleInputContext는 FlexibleInputContext 내부에서만 사용 가능합니다."
-		);
-	return context;
+	return useContextHook(FlexibleInputContext, "FlexibleInputContext");
 };
 
 export default useFlexibleInputContext;
