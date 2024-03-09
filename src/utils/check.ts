e7980603fd520504f99746de4f26a4e5c67ad47d
jiyeon <ejy1105@gmail.com>
2024-03-10 01:09:05 +0900

    fix: useMasking util 함수로 분리하고 정리하기

---
+++ src/utils/check.ts
@@ -0,0 +1,11 @@
+export const checkAllMasking = (text: string, number?: number) => {
+  if (!number) return
+  let maskingVal = ''
+  const regx = new RegExp(`[0-9\*]{1,${number}}`, 'g')
+  if (!text.match(regx)) return
+
+  const masking = [text].map((value, _) => {
+    return (maskingVal = value.replace(value, '*'.repeat(value.length)))
+  })
+  return masking
+}
