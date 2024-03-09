cde60163f19ccde2ebf87c59852cbd6dc403da2f
Yangseungchan <uvzone@naver.com>
2024-03-09 15:47:54 +0900

    feat: use-overlay 훅 정의

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import { CardInputFormStep } from '@/steps/card-register'
 import { CardListStep } from './steps/card-list'
 import { CardRegisterCompleteStep } from '@/steps/card-register-complete'
 import { Flex } from '@/components'
+import { OverlayContextProvider } from '@/hooks'
 
 const 카드_목록 = 'card-list'
 const 카드_입력_폼 = 'card-input-form'
@@ -13,26 +14,28 @@ function App() {
   const [Funnel, step, setStep] = useFunnel(카드_입력_폼)
 
   return (
-    <CardInputContextProvider>
-      <Flex justifyContent="center" backgroundColor="gray500">
-        <Flex width="375px" height="100%" backgroundColor="white">
-          <Funnel.Root step={step}>
-            <Funnel.Step name={카드_목록}>
-              <CardListStep onClickRegister={() => setStep(카드_입력_폼)} />
-            </Funnel.Step>
-            <Funnel.Step name={카드_입력_폼}>
-              <CardInputFormStep
-                onClickPrev={() => setStep(카드_목록)}
-                onSubmit={() => setStep(카드_등록_완료)}
-              />
-            </Funnel.Step>
-            <Funnel.Step name={카드_등록_완료}>
-              <CardRegisterCompleteStep onClickConfirm={() => setStep(카드_목록)} />
-            </Funnel.Step>
-          </Funnel.Root>
+    <OverlayContextProvider>
+      <CardInputContextProvider>
+        <Flex justifyContent="center" backgroundColor="gray500">
+          <Flex width="375px" height="100%" backgroundColor="white">
+            <Funnel.Root step={step}>
+              <Funnel.Step name={카드_목록}>
+                <CardListStep onClickRegister={() => setStep(카드_입력_폼)} />
+              </Funnel.Step>
+              <Funnel.Step name={카드_입력_폼}>
+                <CardInputFormStep
+                  onClickPrev={() => setStep(카드_목록)}
+                  onSubmit={() => setStep(카드_등록_완료)}
+                />
+              </Funnel.Step>
+              <Funnel.Step name={카드_등록_완료}>
+                <CardRegisterCompleteStep onClickConfirm={() => setStep(카드_목록)} />
+              </Funnel.Step>
+            </Funnel.Root>
+          </Flex>
         </Flex>
-      </Flex>
-    </CardInputContextProvider>
+      </CardInputContextProvider>
+    </OverlayContextProvider>
   )
 }
 
---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useOverlay } from './use-overlay/use-overlay'
+export { OverlayContextProvider } from './use-overlay/overlay-context'
+
+export { useFunnel } from './use-funnel'
+export { useUncontrolledInputState } from './use-uncontrolled-input-state'
