117658c40cbd80078ac6c48b8e505fc84e4d3e53
sonsurim <surim014@naver.com>
2024-03-10 23:28:26 +0900

    refactor: add props on BottomSheet

--- src/components/common/BottomSheet/BottomSheet.tsx
+++ src/components/common/BottomSheet/BottomSheet.tsx
@@ -1,12 +1,27 @@
+import { css } from '@emotion/css'
 import { ReactNode } from 'react'
 
 type BottomSheetProps = {
+  isOpen: boolean
+  onClose: () => void
   children: ReactNode
 }
 
-export const BottomSheet = ({ children }: BottomSheetProps) => {
+export const BottomSheet = ({
+  isOpen,
+  onClose,
+  children
+}: BottomSheetProps) => {
+  const bottomSheetStyle = css`
+    display: ${isOpen ? 'flex' : 'none'};
+  `
+
+  const handleClose = () => {
+    onClose()
+  }
+
   return (
-    <div className="modal-dimmed">
+    <div className={`modal-dimmed ${bottomSheetStyle}`} onClick={handleClose}>
       <div className="modal">{children}</div>
     </div>
   )
