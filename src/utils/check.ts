43a2f7acb7d04156739f91db6832c6e9d8d06ba0
jiyeon <ejy1105@gmail.com>
2024-03-10 21:02:37 +0900

    fix: text와 함께 number도 필수props로 수정

--- src/utils/check.ts
+++ src/utils/check.ts
@@ -1,5 +1,4 @@
-export const checkAllMasking = (text: string, number?: number) => {
-  if (!number) return
+export const checkAllMasking = (text: string, number: number) => {
   let maskingVal = ''
   const regx = new RegExp(`[0-9\*]{1,${number}}`, 'g')
   if (!text.match(regx)) return
