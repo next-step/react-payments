ab9b1fca8669511d79769e324899450fbbea37b6
gaeun <knospe1@naver.com>
2024-03-10 16:16:20 +0900

    test: add Card stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,7 @@
 import type { Preview } from "@storybook/react";
 import "../src/styles/input.css";
 import "../src/styles/button.css";
+import "../src/styles/card.css";
 
 const preview: Preview = {
   parameters: {
