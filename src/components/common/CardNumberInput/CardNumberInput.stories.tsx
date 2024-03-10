438b0b7f1c82609394b169ee5feadfb35a567685
sonsurim <surim014@naver.com>
2024-03-11 00:10:51 +0900

    refactor: rename of card number input component to index

--- src/components/common/CardNumberInput/CardNumberInput.stories.tsx
+++ src/components/common/CardNumberInput/CardNumberInput.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { CardNumberInput } from './CardNumberInput'
+import { CardNumberInput } from './index'
 import { useState } from 'react'
 
 const meta: Meta<typeof CardNumberInput> = {
