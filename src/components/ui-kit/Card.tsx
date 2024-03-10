86041a3ed7c928145cc1df55ac514d9ff72d57c0
gaeun <knospe1@naver.com>
2024-03-10 23:51:46 +0900

    fix: fix Card ui

--- src/components/ui-kit/Card.tsx
+++ src/components/ui-kit/Card.tsx
@@ -3,10 +3,49 @@ interface CardProps {
 	children: React.ReactNode;
 }
 
-export default function Card({ variant, children }: CardProps) {
+function CardMain({ variant, children }: CardProps) {
 	return (
 		<div className="card-box">
 			<div className={variant}>{children}</div>
 		</div>
 	);
 }
+
+interface CardTopProps {
+	children?: React.ReactNode;
+}
+
+function CardTop({ children }: CardTopProps) {
+	return <div className="card-top">{children}</div>;
+}
+
+interface CardMiddleProps {
+	children?: React.ReactNode;
+}
+
+function CardMiddle({ children }: CardMiddleProps) {
+	return <div className="card-middle">{children}</div>;
+}
+
+interface CardBottomProps {
+	children?: React.ReactNode;
+}
+
+function CardBottom({ children }: CardBottomProps) {
+	return <div className="card-bottom">{children}</div>;
+}
+
+interface CardTextProps {
+	text: string;
+}
+
+function CardText({ text }: CardTextProps) {
+	return <p className="card-text">{text}</p>;
+}
+
+export const Card = Object.assign(CardMain, {
+	Top: CardTop,
+	Middle: CardMiddle,
+	Bottom: CardBottom,
+	Text: CardText,
+});
--- src/components/ui-kit/index.ts
+++ src/components/ui-kit/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from './Button';
-export { default as Card } from './Card';
+export * from './Card';
 export * from './Input';
