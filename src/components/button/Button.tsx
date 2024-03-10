78bb8300b92914887305a623c4b76d8154ba1080
gaeun <knospe1@naver.com>
2024-03-10 14:35:20 +0900

    feat: add Button

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,11 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: string | React.ReactNode;
+}
+
+export default function Button({ children, onClick }: ButtonProps) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
