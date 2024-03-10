6753d5eec72cbf2cf8fbbbe6383d9d50de0c05db
sonsurim <surim014@naver.com>
2024-03-10 17:09:39 +0900

    refactor: add width prop to input component

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,16 +1,25 @@
+import { css } from '@emotion/css'
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 export type InputProps = ComponentPropsWithoutRef<'input'> & {
   theme?: 'basic' | 'underline'
+  width?: string
 }
 
 export const Input = forwardRef<ElementRef<'input'>, InputProps>(
-  ({ className = '', theme = 'basic', ...props }, forwardedRef) => {
+  (
+    { className = '', theme = 'basic', width = '100%', ...props },
+    forwardedRef
+  ) => {
+    const inputStyle = css`
+      width: ${width};
+    `
+
     return (
       <input
         type="text"
         ref={forwardedRef}
-        className={`input-${theme} ${className}`}
+        className={` ${inputStyle} input-${theme} ${className}`}
         {...props}
       />
     )
