b8a2aeeddfd28291b37629654f18ca76f1a858ed
sonsurim <surim014@naver.com>
2024-03-10 17:09:00 +0900

    refactor: add card box to Card component

--- src/components/Card/SmallCard.tsx
+++ src/components/Card/SmallCard.tsx
@@ -14,24 +14,29 @@ export const SmallCard = forwardRef<ElementRef<'input'>, SmallCardProps>(
     const [month, year] = expiry.split('/')
 
     return (
-      <div className={`small-card ${className}`} ref={forwardedRef} {...props}>
-        <div className="card-top">
-          <span className="card-text">{name}</span>
-        </div>
-        <div className="card-middle">
-          <div className="small-card__chip"></div>
-        </div>
-        <div className="card-bottom">
-          <div className="card-bottom__number">
-            <span className="card-text">
-              {first} {second} •••• ••••
-            </span>
+      <div className="card-box">
+        <div
+          className={`small-card ${className}`}
+          ref={forwardedRef}
+          {...props}>
+          <div className="card-top">
+            <span className="card-text">{name}</span>
           </div>
-          <div className="card-bottom__info">
-            <span className="card-text">{owner}</span>
-            <span className="card-text">
-              {month} / {year}
-            </span>
+          <div className="card-middle">
+            <div className="small-card__chip"></div>
+          </div>
+          <div className="card-bottom">
+            <div className="card-bottom__number">
+              <span className="card-text">
+                {first} {second} •••• ••••
+              </span>
+            </div>
+            <div className="card-bottom__info">
+              <span className="card-text">{owner}</span>
+              <span className="card-text">
+                {month} / {year}
+              </span>
+            </div>
           </div>
         </div>
       </div>
