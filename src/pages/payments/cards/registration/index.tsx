2374ed33a4023cd03d6ea84e210b545b16a7a9f6
saewookkang <pjy9665@kakao.com>
2024-03-10 21:49:10 +0900

    refactor(payments): 스텝 관리 툴 변경(useFunnel -> xstate)

--- src/pages/payments/cards/registration/index.tsx
+++ src/pages/payments/cards/registration/index.tsx
@@ -1,38 +1,63 @@
+import { useMachine } from '@xstate/react'
 import { useRef } from 'react'
+import { setup } from 'xstate'
 
 import { Form } from '@/hooks/form/formContext'
-import { useFunnel } from '@/hooks/useFunnel'
 
 import { formInitialValues } from '../const'
 import { FormType } from '../type'
 import { Step1 } from './components/Step1'
 import { Step1Validate } from './service/validations'
 
+const paymentsStepper = setup({}).createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QAcCGBPAtmAdgF1gGU8xlkwAnAOgGNUKIBaCsKAS1jwtTzYHscjTvTwBiQgFUAQgFkAkgBUA2gAYAuohR9YbXgM0gAHogBMAZgDsVEwBYTADgCMjgJwmVFi3ZMAaEOkR7EwBfUL8cPgg4AzQsXAJiUnIKGO1dfhwDYwRGG0cqIIA2SwBWGxUbEpMLexK-AJySqhKVVsczR3sLFRNHFTKwkFjsfCISMkpaeiYWdk5uPUFhCjxUnUWsxEKqRwszQtsSlzKLXpt7esCQ4L9h+LGkyboGZlYOLh4Mxho+TGQAGzAJDW6X0SCMiDMQWsKg6Jjq-iuoVCQA */
+  id: 'paymentsStepper',
+  initial: 'card-registration-start',
+  states: {
+    'card-registration-start': {
+      on: {
+        NEXT: {
+          target: 'card-registration-complete',
+        },
+      },
+    },
+    'card-registration-complete': {
+      on: {
+        PREV: {
+          target: 'card-registration-start',
+        },
+      },
+    },
+  },
+})
+
 export const AddingCard = () => {
-  const { Funnel, changeStep } = useFunnel<'add-card' | 'complete-card'>('add-card')
+  const [state, send, service] = useMachine(paymentsStepper)
 
   const stepValue = useRef<{ step1: FormType }>({ step1: formInitialValues })
 
+  if (state.value === 'card-registration-start') {
+    return (
+      <Form
+        initialValues={stepValue.current.step1}
+        onSubmit={(values) => {
+          stepValue.current.step1 = values
+
+          send({
+            type: 'NEXT',
+          })
+        }}
+        validate={Step1Validate}
+      >
+        <Step1 />
+      </Form>
+    )
+  }
+
   return (
-    <Funnel>
-      <Funnel.Step name="add-card">
-        <Form
-          initialValues={stepValue.current.step1}
-          onSubmit={(values) => {
-            stepValue.current.step1 = values
-
-            changeStep('complete-card')
-          }}
-          validate={Step1Validate}
-        >
-          <Step1 />
-        </Form>
-      </Funnel.Step>
-
-      <Funnel.Step name="complete-card">
-        <button onClick={() => changeStep('add-card')}> 이전</button>
-        <div>등록 완료 페이지 입니다.</div>
-      </Funnel.Step>
-    </Funnel>
+    <>
+      <button onClick={() => send({ type: 'PREV' })}> 이전</button>
+      <div>등록 완료 페이지 입니다.</div>
+    </>
   )
 }
