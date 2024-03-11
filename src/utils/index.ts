d9923c64c98a2b4437f756312479080000c68169
wogha95 <wogha95@naver.com>
2024-03-11 21:31:02 +0900

    feat: getFormattedMonth 유틸 함수 개선

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,17 +2,24 @@ export function getOnlyNumber(value: string) {
   return value.replace(/\D/, '');
 }
 
-export function getMonthRange(value: string) {
+export function getFormattedMonth(value: string) {
+  const DEFAULT_VALUE_01 = '01';
+  const DEFAULT_VALUE_12 = '12';
+
   if (value.length !== 2) {
     return value;
   }
 
-  const month = parseInt(value);
+  const month = parseInt(value, 10);
+  if (isNaN(month)) {
+    return DEFAULT_VALUE_01;
+  }
+
   if (month < 1) {
-    return '01';
+    return DEFAULT_VALUE_01;
   }
   if (12 < month) {
-    return '12';
+    return DEFAULT_VALUE_12;
   }
 
   return value;
