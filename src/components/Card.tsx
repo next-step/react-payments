9efe9b10d9d894fc4a29c94524199b1d141af383
tjdls111 <tjdls111@naver.com>
2024-03-11 11:58:21 +0900

    feat:카드 추가 완료 페이지 - 카드 별칭

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 
-export interface CardProps {
+export interface CardInputProps {
   cardNumberOne: string
   cardNumberTwo: string
   ownerName: string
   expiredMonth: string
   expiredYear: string
 }
+interface CardProps extends CardInputProps {
+  cardNickname: string
+}
 
 const Card = ({ props }: { props: CardProps }) => {
-  const { cardNumberOne, cardNumberTwo, ownerName, expiredMonth, expiredYear } =
-    props
+  const {
+    cardNickname,
+    cardNumberOne,
+    cardNumberTwo,
+    ownerName,
+    expiredMonth,
+    expiredYear,
+  } = props
 
   return (
     <div className="card-box">
       <div className="big-card">
         <div className="card-top">
-          <span className="card-text__big">클린카드</span>
+          <span className="card-text__big">{cardNickname}</span>
         </div>
         <div className="card-middle">
           <div className="big-card__chip"></div>
--- src/pages/AddCard/FinishAddingCard.tsx
+++ src/pages/AddCard/FinishAddingCard.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import Card, { CardProps } from '../../components/Card.tsx'
+import React, { useState } from 'react'
+import Card, { CardInputProps } from '../../components/Card.tsx'
 import Button from '../../components/Button.tsx'
 import { useNavigate } from 'react-router-dom'
 import Title from '../../components/Title.tsx'
 
-const FinishAddingCard = ({ props }: { props: CardProps }) => {
+const FinishAddingCard = ({ props }: { props: CardInputProps }) => {
+  const [cardNickname, setCardNickname] = useState<string>('')
   const navigate = useNavigate()
   const handleOnClick = () => {
     navigate('/list')
@@ -17,12 +18,13 @@ const FinishAddingCard = ({ props }: { props: CardProps }) => {
           <div className="flex-center">
             <Title className="mb-10">카드등록이 완료되었습니다.</Title>
           </div>
-          <Card props={props} />
+          <Card props={{ ...props, cardNickname }} />
           <div className="input-container flex-center w-100">
             <input
               className="input-underline w-75"
               type="text"
               placeholder="카드의 별칭을 입력해주세요."
+              onChange={(e) => setCardNickname(e.target.value)}
             />
           </div>
           <Button onClick={handleOnClick}>확인</Button>
