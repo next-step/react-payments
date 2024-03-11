135e1ae3944572ca507fe10f70558e9efaa95525
Jay Lee <roof2586@naver.com>
2024-03-11 22:16:31 +0900

    feat: implement credit card entity

---
+++ src/entities/creditcard.ts
@@ -0,0 +1,12 @@
+export interface CreditCard {
+  id: string;
+  date: number;
+  cardNumber: string;
+  expirationMonth: string;
+  expirationYear: string;
+  ownerName: string;
+  cardCompany: string;
+  cardNickname: string;
+  cardPassword: string;
+  verificationCode: string;
+}
