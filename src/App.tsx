8d38cef30bedbdbe4c47225cb80d7c2d4ed324ef
Jay Lee <roof2586@naver.com>
2024-03-11 22:18:09 +0900

    feat: implement route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import CreateCardForm from './components/templates/CreateCardForm';
+import CardEdit from './components/pages/CardEdit';
+import CardForm from './components/pages/CardForm';
+import CardList from './components/pages/CardList';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<CreateCardForm />} />
+        <Route path="/" element={<CardList />} />
+        <Route path="/create" element={<CardForm />} />
+        <Route path="/edit/:id" element={<CardEdit />} />
       </Routes>
     </Router>
   );
