24cc67a0f50f3cf94b1d7c80c53fb2afdfd7df53
osdoonhyun <skw1996@naver.com>
2024-03-12 18:05:13 +0900

    refactor: Form 하위 비즈니스 로직과 결합된 컴포넌트 구조 개선

--- src/pages/Payments/AddCard.tsx
+++ src/pages/Payments/AddCard.tsx
@@ -1,6 +1,9 @@
+import { initialFormData } from '@/constants/form';
+import { Form } from '@/context/Form';
+import { cardValidate } from '@/utils/cardValidations';
 import PageTitle from '@components/@common/PageTitle';
 import Button from '@components/@common/button/molecules/Button';
-import Card from '@components/Card/organisms/CreditCard';
+import CreditCard from '@components/Card/organisms/CreditCard';
 import CardHolderName from '@components/CardForm/molecules/CardHolderName';
 import CardNumber from '@components/CardForm/molecules/CardNumber';
 import ExpirationDate from '@components/CardForm/molecules/ExpirationDate';
@@ -9,25 +12,36 @@ import VerificationCode from '@components/CardForm/molecules/VerificationCode';
 
 type AddCardProps = {
   onPrevious: () => void;
+  onNext: () => void;
 };
 
-export default function AddCard({ onPrevious }: AddCardProps) {
+export default function AddCard({ onPrevious, onNext }: AddCardProps) {
+  const handleSubmit = () => {
+    onNext();
+  };
+
   return (
     <div className='root'>
       <div className='app'>
         <PageTitle onPrevious={onPrevious}>카드 추가</PageTitle>
 
-        {/* Card */}
-        <Card />
+        <Form
+          initialValue={initialFormData}
+          validate={cardValidate}
+          onSubmit={handleSubmit}
+        >
+          {/* Card */}
+          <CreditCard />
 
-        {/* CardForm */}
-        <CardNumber />
-        <ExpirationDate />
-        <CardHolderName />
-        <VerificationCode />
-        <PinNumber />
+          {/* CardForm */}
+          <CardNumber />
+          <ExpirationDate />
+          <CardHolderName />
+          <VerificationCode />
+          <PinNumber />
 
-        <Button>다음</Button>
+          <Button>다음</Button>
+        </Form>
       </div>
     </div>
   );
--- src/pages/Payments/index.tsx
+++ src/pages/Payments/index.tsx
@@ -1,49 +1,25 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import AddCardCompleted from './AddCardCompleted';
 import AddCard from './AddCard';
 import CardList from '../CardList';
-import { Form } from '@/context/Form';
-import { cardValidate } from '@/utils/cardValidations';
-import { initialFormData } from '@/constants/form';
-import { FormType } from '@/type/formType';
 
 export default function Payments() {
   const [step, setStep] = useState('카드_입력');
 
-  const cardValue = useRef({ card: initialFormData });
-
-  const handleSubmit = (values: FormType) => {
-    cardValue.current.card = values;
-
-    onNext();
-  };
-
-  // 다음 단계 로직을 구현하거나 상태를 변경하는 코드 작성
-  const onNext = () => {
-    if (step === '카드_입력') {
-      setStep('카드_입력_완료');
-    } else if (step === '카드_입력_완료') {
-      setStep('카드_목록');
-    }
-  };
-
   return (
     <>
-      <Form
-        initialValue={initialFormData}
-        validate={cardValidate}
-        onSubmit={handleSubmit}
-      >
-        {step === '카드_입력' && (
-          <AddCard onPrevious={() => setStep('카드_목록')} />
-        )}
-        {step === '카드_입력_완료' && (
-          <AddCardCompleted onNext={() => setStep('카드_목록')} />
-        )}
-        {step === '카드_목록' && (
-          <CardList onFirstStep={() => setStep('카드_입력')} />
-        )}
-      </Form>
+      {step === '카드_입력' && (
+        <AddCard
+          onPrevious={() => setStep('카드_목록')}
+          onNext={() => setStep('카드_입력_완료')}
+        />
+      )}
+      {step === '카드_입력_완료' && (
+        <AddCardCompleted onNext={() => setStep('카드_목록')} />
+      )}
+      {step === '카드_목록' && (
+        <CardList onFirstStep={() => setStep('카드_입력')} />
+      )}
     </>
   );
 }
