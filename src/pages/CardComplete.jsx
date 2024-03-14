fbf73fa0e9f7f80502617ff621669d5a77a1b81d
Leeyounwoo <yunwoo.lee@cj.net>
2024-03-14 09:17:10 +0900

    카드 별칭 등록 페이지 - 비제어 컴포넌트로 수정

--- src/pages/CardComplete.jsx
+++ src/pages/CardComplete.jsx
@@ -1,35 +1,44 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import Card from "../components/Card";
 import { CardContext } from "../../providers/CardState/CardStateProvider";
 import { MONTH, YEAR } from "../constants/expirationDate";
-import CardAliasInput from "../components/card-complete/CardAliasInput";
+import Input from "../components/atomic-design-pattern/atom/Input";
 import Button from "../components/atomic-design-pattern/atom/Button";
 
 export default function CardComplete({ onNext }) {
   const { cardState } = useContext(CardContext);
-  const { cardNumber, expirationDate, cardOwnerName, alias } = cardState;
+  const { cardNumber, expirationDate, cardOwnerName } = cardState;
+  const aliasInputRef = useRef(null);
+
+  const onSubmitCardComplete = (event) => {
+    event.preventDefault();
+    const alias = aliasInputRef.current.value || "카드사 이름";
+    console.log(alias);
+  };
 
   return (
-    <div className="root">
-      <div className="app flex-column-center">
-        <div className="flex-center">
-          <h2 className="page-title mb-10">카드등록이 완료되었습니다.</h2>
-        </div>
-        <Card
-          alias={alias}
-          cardNumber={cardNumber}
-          expirationDateMM={expirationDate[MONTH]}
-          expirationDateYY={expirationDate[YEAR]}
-          cardOwnerName={cardOwnerName}
-        />
-        <CardAliasInput />
-
-        <div className="button-box">
-          <Button variant="link" onClick={onNext}>
-            다음
-          </Button>
-        </div>
+    <form onSubmit={onSubmitCardComplete} className="flex-column-center">
+      <div className="flex-center">
+        <h2 className="page-title mb-10">카드등록이 완료되었습니다.</h2>
       </div>
-    </div>
+      <Card
+        cardNumber={cardNumber}
+        expirationDateMM={expirationDate[MONTH]}
+        expirationDateYY={expirationDate[YEAR]}
+        cardOwnerName={cardOwnerName}
+      />
+      <Input
+        className="input-underline w-75"
+        placeholder="카드 별칭 (선택)"
+        maxLength={10}
+        ref={aliasInputRef}
+      />
+
+      <div className="button-box">
+        <Button variant="link" type="submit">
+          다음
+        </Button>
+      </div>
+    </form>
   );
 }
