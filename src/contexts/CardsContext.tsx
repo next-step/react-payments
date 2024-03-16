cc2c5974af0e097788dd5dc7072d0ff379b1813f
gaeun <knospe1@naver.com>
2024-03-16 23:43:39 +0900

    fix: fix CardsContext

--- src/contexts/CardsContext.tsx
+++ src/contexts/CardsContext.tsx
@@ -1,38 +1,44 @@
 import { PaymentCardType } from '@types';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
-export const CardsContext = createContext<PaymentCardType[]>([]);
+export const CardsContext = createContext<
+	| [PaymentCardType[], React.Dispatch<React.SetStateAction<PaymentCardType[]>>]
+	| undefined
+>(undefined);
 
 interface CardProviderProps {
 	children: React.ReactNode;
 }
 
-const getCards = () => [
-	{
-		id: '1',
-		cardNumber: '1234123412341234',
-		cardExpiredDate: '1228',
-		cardHolderName: 'NEW',
-		cardSecurityCode: '123',
-		cardPassword: '12',
-		createdAt: new Date('2024-01-01'),
-		cardAlias: '삼성카드',
-	},
-	{
-		id: '2',
-		cardNumber: '1234123412341234',
-		cardExpiredDate: '1124',
-		cardHolderName: 'GAEUN KIM',
-		cardSecurityCode: '123',
-		cardPassword: '12',
-		createdAt: new Date('2023-01-01'),
-		cardAlias: '현대카드',
-	},
-];
+const getCards: () => PaymentCardType[] = () => {
+	return [
+		{
+			id: '1',
+			cardNumber: '1234123412341234',
+			cardExpiredDate: '1228',
+			cardHolderName: 'NEW',
+			cardSecurityCode: '123',
+			cardPassword: '12',
+			createdAt: new Date('2024-01-01'),
+			cardAlias: '삼성카드',
+		},
+		{
+			id: '2',
+			cardNumber: '1234123412341234',
+			cardExpiredDate: '1124',
+			cardHolderName: 'GAEUN KIM',
+			cardSecurityCode: '123',
+			cardPassword: '12',
+			createdAt: new Date('2023-01-01'),
+			cardAlias: '현대카드',
+		},
+	];
+};
 
 export function CardsProvider({ children }: CardProviderProps) {
-	const cards = getCards();
+	const state = useState(getCards);
+
 	return (
-		<CardsContext.Provider value={cards}>{children}</CardsContext.Provider>
+		<CardsContext.Provider value={state}>{children}</CardsContext.Provider>
 	);
 }
--- src/hooks/useCards.ts
+++ src/hooks/useCards.ts
@@ -2,5 +2,9 @@ import { CardsContext } from '@contexts';
 import { useContext } from 'react';
 
 export default function useCards() {
-	return useContext(CardsContext);
+	const value = useContext(CardsContext);
+	if (value === undefined) {
+		throw new Error('useMyContext should be used within MyContext.Provider');
+	}
+	return value[0];
 }
