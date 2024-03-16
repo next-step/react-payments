c30f4cdf72a5a792b0ed1178bb1566b1eb446aca
bytrustu <bytrustu@gmail.com>
2024-03-16 22:23:14 +0900

    feat: 카드 클릭시 완료페이지 전환 및 카드 삭제 기능 추가

---
+++ src/card/pages/CardListPage.tsx
@@ -0,0 +1,71 @@
+import { CardDisplay, CardPageIndex, CardState, useCard } from '@/card';
+import { AppLayout, Typography, VStack, useFunnel, Button, HStack } from '@/shared';
+
+export const CardListPage = () => {
+  const { ownerCards, setCard, resetCurrentCard, removeCardFromOwner } = useCard();
+  const { goToNext, goToIndex } = useFunnel();
+
+  const moveCardAddPage = () => {
+    resetCurrentCard();
+    goToNext();
+  };
+
+  const moveCardCompletePage = (card: CardState) => {
+    setCard(card);
+    goToIndex(CardPageIndex.CardCompletePage);
+  };
+
+  const onRemoveCardClick = (card: CardState) => {
+    if (window.confirm('정말 삭제하시겠습니까?')) {
+      removeCardFromOwner(card);
+    }
+  };
+
+  const ownerCardsDescending = ownerCards.sort((a, b) => b.createdTimestamp - a.createdTimestamp);
+
+  return (
+    <>
+      <AppLayout.Header>
+        <Typography as="h1" variant="headline" padding="10px 0" margin="0">
+          보유 카드
+        </Typography>
+      </AppLayout.Header>
+      <AppLayout.Body>
+        <VStack width="100%" spacing="20px" padding="20px 0">
+          {ownerCardsDescending.map((ownerCard, index) => (
+            <HStack
+              key={`card-list-${ownerCard.cardNumber}-${index}`}
+              width="100%"
+              justifyContent="center"
+              alignItems="center"
+              spacing="10px"
+            >
+              <VStack spacing="10px" alignItems="center" justifyContent="center">
+                <CardDisplay size="small" {...ownerCard} onClick={() => moveCardCompletePage(ownerCard)} />
+                <Typography variant="body" textAlign="center">
+                  {ownerCard.description}
+                </Typography>
+              </VStack>
+              <Button
+                variant="ghost"
+                colorScheme="gray"
+                width="auto"
+                padding="0"
+                margin="0 0 20px 10px"
+                backgroundColor="unset"
+                onClick={() => onRemoveCardClick(ownerCard)}
+              >
+                삭제
+              </Button>
+            </HStack>
+          ))}
+        </VStack>
+      </AppLayout.Body>
+      <AppLayout.Footer height="300px">
+        <VStack paddingTop="20px" alignItems="center">
+          <CardDisplay.Add onClick={moveCardAddPage} />
+        </VStack>
+      </AppLayout.Footer>
+    </>
+  );
+};
