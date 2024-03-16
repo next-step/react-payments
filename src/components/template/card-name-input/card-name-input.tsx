4ff0277e3449920b68a4aa896d6db633b074bb94
sxungchxn <yyaang3@g.skku.edu>
2024-03-16 20:13:50 +0900

    feat: card-name-input에 ref 연결

--- src/components/template/card-name-input/card-name-input.tsx
+++ src/components/template/card-name-input/card-name-input.tsx
@@ -1,5 +1,7 @@
 import { BaseInput, BaseInputProps } from '@/components/organism/base-input'
 import { useUncontrolledInputState } from '@/hooks/use-uncontrolled-input-state'
+import { PolymorphicRef } from '@/types'
+import { forwardRef } from 'react'
 
 export interface CardNameInputProps
   extends Omit<BaseInputProps, 'value' | 'defaultValue' | 'onChange'> {
@@ -8,24 +10,27 @@ export interface CardNameInputProps
   onChange?: (value: string) => void
 }
 
-export const CardNameInput = (props: CardNameInputProps) => {
-  const { value, defaultValue, onChange, maxLength = 30 } = props
-  const [_value, handleChangeInput] = useUncontrolledInputState({
-    value,
-    finalValue: '',
-    defaultValue,
-    onChange,
-  })
+export const CardNameInput = forwardRef(
+  (props: CardNameInputProps, ref: PolymorphicRef<'input'>) => {
+    const { value, defaultValue, onChange, maxLength = 30 } = props
+    const [_value, handleChangeInput] = useUncontrolledInputState({
+      value,
+      finalValue: '',
+      defaultValue,
+      onChange,
+    })
 
-  const inputHelperText = `${_value.length} / ${maxLength}`
+    const inputHelperText = `${_value.length} / ${maxLength}`
 
-  return (
-    <BaseInput
-      {...props}
-      maxLength={maxLength}
-      value={_value}
-      onChange={e => handleChangeInput(e.target.value)}
-      helperText={inputHelperText}
-    />
-  )
-}
+    return (
+      <BaseInput
+        {...props}
+        ref={ref}
+        maxLength={maxLength}
+        value={_value}
+        onChange={e => handleChangeInput(e.target.value)}
+        helperText={inputHelperText}
+      />
+    )
+  },
+)
