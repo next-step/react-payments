3261e66a5ded90de5b39eb383676c685294cec20
gaeun <knospe1@naver.com>
2024-03-16 23:18:21 +0900

    feat: add Header in CardsPage

--- src/pages/cards/CardsPage.tsx
+++ src/pages/cards/CardsPage.tsx
@@ -1,11 +1,15 @@
 import { AddCardButton } from '@pages/cards/components';
 import Cards from './components/Cards';
+import { Header } from '@components/layout';
 
 export default function CardsPage() {
 	return (
-		<div className="flex-column-center">
-			<Cards />
-			<AddCardButton />
-		</div>
+		<>
+			<Header title="보유카드" />
+			<div className="flex-column-center">
+				<Cards />
+				<AddCardButton />
+			</div>
+		</>
 	);
 }
