4182b453e67061302c1a7962bd063dc42fd20dc8
ww8007 <wshmin1234@gmail.com>
2024-03-19 03:05:56 +0900

    feat: 카드를 삭제할 수 있게 헤더에 아이콘 추가 및 삭제 비지니스 로직 추가

--- src/Card/List/CardList.tsx
+++ src/Card/List/CardList.tsx
@@ -1,10 +1,15 @@
+import Button from "@/common/ui/Button/Button";
+import IconButton from "@/common/ui/Button/IconButton";
 import Header from "@/common/ui/Header/Header";
 import Text from "@/common/ui/Text/Text";
+import { IconName } from "@/common/ui/assets/Icon";
 import { maskStringAfterIndex } from "@/common/utils";
 import { convertObjectValuesToString } from "@/common/utils/object";
 import { CardFunnelProps } from "@/pages/CardFunnel";
 import styled from "@emotion/styled";
+import { useState } from "react";
 import { CARD_COLOR_MAP } from "../constants/cardCompany";
+import { useManageCardContext } from "../machine/card/useCardContext";
 import { CardInfo } from "../types/card";
 import { CARD_COMPANY } from "../types/cardCompany";
 import Card from "../ui/Card/Card";
@@ -14,10 +19,34 @@ interface CardListProps extends CardFunnelProps {
 	cardList: CardInfo[];
 }
 
+type MODE = "ADD" | "DELETE";
+
+const ICON_MAPPER: Record<MODE, IconName> = {
+	ADD: "setting",
+	DELETE: "close"
+};
+
 const CardList = ({ cardList, onNext }: CardListProps) => {
+	const { send } = useManageCardContext();
+	const [mode, setMode] = useState<MODE>("ADD");
+
+	const onToggleMode = () => {
+		if (mode === "ADD") setMode("DELETE");
+		if (mode === "DELETE") setMode("ADD");
+	};
+
 	return (
 		<>
-			<Header title='보유카드' />
+			<Header
+				title='보유카드'
+				rightButton={
+					<IconButton
+						name={ICON_MAPPER[mode]}
+						size='m'
+						onClick={onToggleMode}
+					/>
+				}
+			/>
 			<Wrapper>
 				{cardList.map((card) => {
 					return (
@@ -48,7 +77,26 @@ const CardList = ({ cardList, onNext }: CardListProps) => {
 									</Card.BottomInfo>
 								</Card.Bottom>
 							</Card>
-							<Text.Span>{card.cardName}</Text.Span>
+							{mode === "ADD" && (
+								<TextWrapper>
+									<Text.Span fontSize={15}>{card.cardName}</Text.Span>
+								</TextWrapper>
+							)}
+							{mode === "DELETE" && (
+								<Button
+									onClick={() => {
+										send({ type: "DELETE_CARD", value: card.id });
+									}}
+									buttonColor='main'
+									color='white'
+									height={2}
+									width={65}
+								>
+									<Text.Span fontSize={15} color='white'>
+										삭제
+									</Text.Span>
+								</Button>
+							)}
 						</CardWrapper>
 					);
 				})}
@@ -66,6 +114,13 @@ const Wrapper = styled.div`
 	row-gap: 20px;
 	overflow-y: scroll;
 	max-height: 610px;
+`;
+
+const TextWrapper = styled.div`
+	height: 2rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 `;
 
 const CardWrapper = styled.div`
