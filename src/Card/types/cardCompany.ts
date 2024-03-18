b478ddfda0f0560b67b85147a4d99370d98a50ca
ww8007 <wshmin1234@gmail.com>
2024-03-19 02:00:48 +0900

    feat: 카드 회사에 대한 타입과 CardCompanyInfo 타입 추가

---
+++ src/Card/types/cardCompany.ts
@@ -0,0 +1,14 @@
+export type CARD_COMPANY =
+	| "레냥"
+	| "블냥"
+	| "초냥"
+	| "자냥"
+	| "로냥"
+	| "골냥"
+	| "깜냥"
+	| "신냥";
+
+export interface CardCompanyInfo {
+	bankName: CARD_COMPANY;
+	color: string;
+}
