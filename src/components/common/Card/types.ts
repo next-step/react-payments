9e40cb11479c7033ba7ea12d2e67ba5df7d8b8a7
sonsurim <surim014@naver.com>
2024-03-19 22:39:48 +0900

    refactor: Improved type accuracy

--- src/components/common/Card/types.ts
+++ src/components/common/Card/types.ts
@@ -1,2 +1,8 @@
+type oneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+type zeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+type MM = `0${oneToNine}` | `1${0 | 1 | 2}`
+type YY = `${zeroToNine}${zeroToNine}`
+
 export type CardNumber = `${number} ${number} ${number} ${number}`
-export type CardExpiry = `${number}/${number}`
+export type CardExpiry = `${MM}/${YY}`
