d7365edbdfd0d50e6e07b4b8de05eee1907cc7c6
euan-lee <maplestory1419@naver.com>
2024-03-19 10:29:36 +0900

    feat:card 주인 이름 구현 컴포넌트

--- src/Components/RegisterForm/RegisterFormElements/CardOwner.tsx
+++ src/Components/RegisterForm/RegisterFormElements/CardOwner.tsx
@@ -1,19 +1,19 @@
-import useOwnerName from "../../../Hooks/useOwnerName";
+import useOwnerName from "../../../Hooks/Input/useOwnerName";
 
 const CardOwner = () => {
   const { ownerName, handleNameChange } = useOwnerName();
 
   return (
     <div className="input-container">
-      <span className="input-title">
-        카드 소유자 이름(선택) {ownerName.length}/30
-      </span>
-
+      <div className="input-length">
+        <span className="input-title">카드 소유자 이름(선택)</span>
+        <span className="input-title">{ownerName?.length}/30</span>
+      </div>
       <input
         type="text"
         className="input-basic"
         placeholder="카드에 표시된 이름과 동일하게 입력하세요."
-        value={ownerName}
+        value={ownerName || ""}
         onChange={handleNameChange}
       />
     </div>
---
+++ src/Hooks/Input/useOwnerName.tsx
@@ -0,0 +1,20 @@
+import { useCardInfo } from "../../Context/CardProvider";
+import { ChangeEvent } from "react";
+import { Validation } from "../../Util/Validation";
+const useOwnerName = () => {
+  const { state, dispatch } = useCardInfo();
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (Validation.OwnerName(event.target.value)) {
+      dispatch({
+        type: "SET_OWNER_NAME",
+        payload: { key: "cardOwnerName", value: event.target.value },
+      });
+    }
+  };
+  return {
+    ownerName: state.cardOwnerName,
+    handleNameChange,
+  };
+};
+
+export default useOwnerName;
