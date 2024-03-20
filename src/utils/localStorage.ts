752db653594774de1998839837f9f1e3e7ad5f61
ingdaeho <ingdaeho@swingby.us>
2024-03-20 15:30:44 +0900

    refactor: getLocalStorageItem null 타입 추가

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,6 +1,6 @@
 export const CARD_STORAGE_KEY = 'cards';
 
-export const getLocalStorageItem = <T>({ key }: { key: string }): T => {
+export const getLocalStorageItem = <T>({ key }: { key: string }): T | null => {
   const storedData = localStorage.getItem(key);
 
   return JSON.parse(storedData || 'null');
