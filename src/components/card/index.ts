26550627b6fa1c7091858e67bdc4f74b89f10173
jungjinbeom <insunok0715@naver.com>
2024-03-20 22:24:16 +0900

    eslint 적용

--- src/components/card/index.ts
+++ src/components/card/index.ts
@@ -1,2 +1,2 @@
-export {default as Card} from './Card';
-export {default as EmptyCard} from './EmptyCard';
+export { default as Card } from './Card';
+export { default as EmptyCard } from './EmptyCard';
