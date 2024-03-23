6edd16c5e3f5a133b342b5620ae74040a5d1c52d
sonsurim <surim014@naver.com>
2024-03-23 23:07:41 +0900

    feat: implement the CompletedAddCardStep

--- src/components/PaymentModal/CompletedAddCardStep/index.tsx
+++ src/components/PaymentModal/CompletedAddCardStep/index.tsx
@@ -1,42 +1,59 @@
+import { Card, Input } from '@/components/common'
+import { CardNumber, MM, YY } from '@/components/common/Card/types'
+import { PaymentContext } from '@/contexts/PaymentContext/PaymentProvider'
+import { CardDetail } from '@/contexts/PaymentContext/types'
+import { usePaymentStep } from '@/pages/hooks/usePaymentStep'
+import { ChangeEventHandler, useContext } from 'react'
+
 export const CompletedAddCardStep = () => {
+  const [, setStep] = usePaymentStep()
+  const { card, resetCardDetail, updateCardDetail, updateCardList } =
+    useContext(PaymentContext)
+
+  const handleChangeCardName: ChangeEventHandler<HTMLInputElement> = e => {
+    const { name, value } = e.target
+
+    updateCardDetail({
+      name: name as KeyOf<CardDetail>,
+      value
+    })
+  }
+
+  const handleClickNextButton = () => {
+    updateCardList(card)
+    resetCardDetail()
+    setStep('card list')
+  }
+
   return (
     <div>
-      <h2>4️⃣ 카드 추가 완료</h2>
       <div className="root">
         <div className="app flex-column-center">
           <div className="flex-center">
             <h2 className="page-title mb-10">카드등록이 완료되었습니다.</h2>
           </div>
-          <div className="card-box">
-            <div className="big-card">
-              <div className="card-top">
-                <span className="card-text__big">클린카드</span>
-              </div>
-              <div className="card-middle">
-                <div className="big-card__chip"></div>
-              </div>
-              <div className="card-bottom">
-                <div className="card-bottom__number">
-                  <span className="card-text__big">
-                    1111 - 2222 - oooo - oooo
-                  </span>
-                </div>
-                <div className="card-bottom__info">
-                  <span className="card-text__big">YUJO</span>
-                  <span className="card-text__big">12 / 23</span>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Card.Big
+            number={card.number as CardNumber[]}
+            expiry={card.expiry as [MM, YY]}
+            owner={card.owner}
+            name={card.name}
+          />
           <div className="input-container flex-center w-100">
-            <input
-              className="input-underline w-75"
-              type="text"
+            <Input
+              name="name"
+              theme="underline"
               placeholder="카드의 별칭을 입력해주세요."
+              onChange={handleChangeCardName}
             />
           </div>
           <div className="button-box mt-50">
-            <span className="button-text">다음</span>
+            <span
+              role="button"
+              className="button-text"
+              tabIndex={0}
+              onClick={handleClickNextButton}>
+              다음
+            </span>
           </div>
         </div>
       </div>
