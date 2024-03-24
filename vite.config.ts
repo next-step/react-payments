c30bb87b7bfdb92e516a79f366c556e01917333a
Yangseungchan <uvzone@naver.com>
2024-03-24 17:02:46 +0900

    fix: lib config, build config 분리

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,12 @@
 /// <reference types="vitest" />
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
-import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    dts({
-      include: ['src'],
-      exclude: [
-        'node_modules',
-        'dist',
-        '**/*.stories.tsx',
-        '**/*.test.ts*',
-        './src/vite-env.d.ts',
-        './src/main.tsx',
-      ],
-    }),
-    vanillaExtractPlugin(),
-  ],
+  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
   test: {
     globals: true,
     setupFiles: ['./src/tests/setup-tests.ts'],
@@ -34,25 +17,5 @@ export default defineConfig({
         resources: 'usable',
       },
     },
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'sxungchxn-react-payments',
-      fileName: 'index',
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDom',
-        },
-      },
-    },
-  },
-  resolve: {
-    dedupe: ['react', 'react-dom'],
   },
 })
