cdfe6e5a8c9f3f216efd0558f8f33cf27a087fbd
정수현 <78977003+sasha1107@users.noreply.github.com>
2024-03-27 01:35:02 +0900

    [클린코드 리액트 3기 정수현] 페이먼츠 미션 Step3 (#153)
    * Created project with vite
    
    * chore: .gitignore 수정 및 storybook-static 삭제
    
    - .gitignore에 storybook-static 디렉토리 추가
    
    * feat: 라우터 추가
    
    * 폴더 구조 변경
    
    * chore: add storybook deploy url in commit comment
    
    * feat: example 페이지 임시 분리
    
    * chore: vanilla extract css setting
    
    * feat: Header 컴포넌트 추가
    
    * feat: Form 컴포넌트 추가
    
    * feat: cn 유틸 추가
    
    - className 병합하는 유틸 함수
    
    * feat: Input 컴포넌트 구현
    
    * feat: Card 컴포넌트 구현
    
    * feat: Example page 임시 분리
    
    * feat: List page 구현
    
    * feat: 카드 추가 페이지 구현 중
    
    * chore: svgr 셋팅
    
    * style: layout 설정
    
    - maxWidth 모바일 크기로 설정
    
    * feat: Button 컴포넌트 구현
    
    * style: 스타일 수정
    
    * feat: useFunnel 커스텀 훅 구현
    
    * feat: useFunnel로 step 분리
    
    * refactor: 코드 리뷰 반영
    
    * fix: storybook deploy event 변경
    
    * Update chromatic.yml
    
    * refactor: cn util parameter type 변경
    
    * refactor: form-item label 연결
    
    * refactor: Card 컴포넌트 이동
    
    * refactor: card.css.ts 로직 제거
    
    * chore: install xstate
    
    * refactor: xstate machine으로 Stepper 리팩토링
    
    * feat: React Context -> xstate context
    
    * remove: 사용하지 않는 코드 삭제
    
    * feat: 카드 추가 기능 구현
    
    * style: 스타일 수정
    
    * fix: 새로운 카드 추가 시 최신 순 저장
    
    * feat: 카드 별칭 빈 입력값인 경우, 카드사 이름이 별칭으로 저장
    
    * style: 스타일 수정
    
    * feat: update, delete 구현
    
    * refactor: util 폴더 구조 변경
    
    * refactor: props 구조분해할당 위치 통일
    
    * refactor: inline css 제거
    
    * refactor: stepper 로직 간소화 및 context 파일 위치 변경
    
    * refactor: 불필요한 useMemo 제거
    
    * rename: 폴더명 수정
    
    * feat: id값 uuid로 변경
    
    * refactor: list key값 id로 변경
    
    * refactor: 카드 정보 입력폼 xstate로 구현
    
    * fix: input component 수정
    
    * refactor: 리뷰 반영

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,26 +1,32 @@
 import * as styles from './index.css';
 import { cn } from '@/utils';
+import { forwardRef } from 'react';
 import type { ComponentProps } from 'react';
-
 interface InputProps extends ComponentProps<'input'> {
   type?: 'default' | 'underline';
   htmpType?: ComponentProps<'input'>['type'];
   block?: boolean;
 }
 
-const Input = ({
-  className,
-  htmpType = 'text',
-  type = 'default',
-  block = false,
-  ...rest
-}: InputProps) => {
-  const classNames = cn([styles.inputElement, className]);
-  return (
-    <div className={cn([styles.inputBox[type], styles.inputBlock[`${block}`]])}>
-      <input {...rest} className={classNames} type={htmpType} />
-    </div>
-  );
-};
-
+const Input = forwardRef(
+  (
+    {
+      className,
+      htmpType = 'text',
+      type = 'default',
+      block = false,
+      ...rest
+    }: InputProps,
+    ref: React.ForwardedRef<HTMLInputElement> | undefined
+  ) => {
+    const classNames = cn([styles.inputElement, className]);
+    return (
+      <div
+        className={cn([styles.inputBox[type], styles.inputBlock[`${block}`]])}
+      >
+        <input {...rest} ref={ref} className={classNames} type={htmpType} />
+      </div>
+    );
+  }
+);
 export default Input;
